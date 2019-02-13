2c57e4af4837eafa6a202a82a5724d41d74d6836
Cody A. Ray <cody@confluent.io>
2019-02-13 15:41:31 -0600

    add comment

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,11 @@
 // +build tools
 package tools 
 
+// This version controls our third-party tools, as per https://github.com/golang/go/issues/25922
+//
+// If you try doing this with "go get" in Makefile, it updates your go.mod/go.sum, creating dirty
+// state that causes goreleaser to fail.
+
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
