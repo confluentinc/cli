f6cc486ee05306e3a703bb049afb53c71621e167
Cody A. Ray <cody@confluent.io>
2019-02-26 15:27:08 -0600

    convert to hclog

--- log/logger.go
+++ log/logger.go
@@ -1,16 +1,17 @@
 package log
 
 import (
+	"fmt"
 	"io"
 	"os"
 
 	"github.com/confluentinc/ccloud-sdk-go"
-	"github.com/sirupsen/logrus"
+	"github.com/hashicorp/go-hclog"
 )
 
 // Logger is the standard logger for the Confluent SDK.
 type Logger struct {
-	l *logrus.Logger
+	l hclog.Logger
 }
 
 var _ ccloud.Logger = (*Logger)(nil)
@@ -37,78 +38,90 @@ const (
 
 // New create and configures a new Logger.
 func New() *Logger {
-	logger := &Logger{l: logrus.New()}
-	logger.l.Formatter = &logrus.TextFormatter{FullTimestamp: true, DisableLevelTruncation: true}
-	logger.SetLevel(WARN)
-	logger.SetOutput(os.Stdout)
-	return logger
+	return &Logger{l: hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Warn,
+		Output:     os.Stderr,
+	})}
 }
 
 func (l *Logger) Debug(args ...interface{}) {
-	l.l.Debug(args...)
+	if l.l.IsDebug() {
+		l.l.Debug(fmt.Sprint(args))
+	}
 }
 
 func (l *Logger) Debugf(format string, args ...interface{}) {
-	l.l.Debugf(format, args...)
+	if l.l.IsDebug() {
+		l.l.Debug(fmt.Sprintf(format, args...))
+	}
 }
 
 
 func (l *Logger) Info(args ...interface{}) {
-	l.l.Info(args...)
+	if l.l.IsInfo() {
+		l.l.Info(fmt.Sprint(args...))
+	}
 }
 
 func (l *Logger) Infof(format string, args ...interface{}) {
-	l.l.Infof(format, args...)
+	if l.l.IsInfo() {
+		l.l.Info(fmt.Sprintf(format, args...))
+	}
 }
 
 func (l *Logger) Warn(args ...interface{}) {
-	l.l.Warn(args...)
+	if l.l.IsWarn() {
+		l.l.Warn(fmt.Sprint(args...))
+	}
 }
 
 func (l *Logger) Warnf(format string, args ...interface{}) {
-	l.l.Warnf(format, args...)
+	if l.l.IsWarn() {
+		l.l.Warn(fmt.Sprintf(format, args...))
+	}
 }
 
 func (l *Logger) Error(args ...interface{}) {
-	l.l.Error(args...)
+	if l.l.IsError() {
+		l.l.Error(fmt.Sprint(args...))
+	}
 }
 
 func (l *Logger) Errorf(format string, args ...interface{}) {
-	l.l.Errorf(format, args...)
+	if l.l.IsError() {
+		l.l.Error(fmt.Sprintf(format, args...))
+	}
 }
 
 // Log logs a "msg" and key-value pairs.
 // Example: Log("msg", "hello", "key1", "val1", "key2", "val2")
 func (l *Logger) Log(args ...interface{}) {
-	var msg interface{}
-	m := make(map[string]interface{})
-	for i := 0; i < len(args); i += 2 {
-		k := args[i].(string)
-		v := args[i+1]
-		if k == "msg" {
-			msg = v
-		} else {
-			m[k] = v
+	if l.l.IsDebug() {
+		if args[0] != "msg" {
+			l.l.Debug(`unexpected logging call, first key should be "msg": ` + fmt.Sprint(args))
 		}
+		l.l.Debug(fmt.Sprint(args[1]), args[2:]...)
 	}
-	l.l.WithFields(logrus.Fields(m)).Debug(msg)
 }
 
 func (l *Logger) SetLevel(level Level) {
 	switch level {
 	case ERROR:
-		l.l.SetLevel(logrus.ErrorLevel)
+		l.l.SetLevel(hclog.Error)
 	case WARN:
-		l.l.SetLevel(logrus.WarnLevel)
+		l.l.SetLevel(hclog.Warn)
 	case INFO:
-		l.l.SetLevel(logrus.InfoLevel)
+		l.l.SetLevel(hclog.Info)
 	case DEBUG:
-		l.l.SetLevel(logrus.DebugLevel)
+		l.l.SetLevel(hclog.Debug)
 	case TRACE:
-		l.l.SetLevel(logrus.TraceLevel)
+		l.l.SetLevel(hclog.Trace)
 	}
 }
 
 func (l *Logger) SetOutput(out io.Writer) {
-	l.l.SetOutput(out)
+	l.l =  hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Warn,
+		Output:     out,
+	})
 }
