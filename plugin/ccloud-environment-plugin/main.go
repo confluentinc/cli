79481659ec9ace29ed1e412f7d030f6626766d81
DABH <DABH@users.noreply.github.com>
2019-03-19 11:54:04 -0700

    wrap errors per feedback

--- plugin/ccloud-environment-plugin/main.go
+++ plugin/ccloud-environment-plugin/main.go
@@ -83,29 +83,29 @@ type Account struct {
 func (c *Account) Create(ctx context.Context, account *orgv1.Account) (*orgv1.Account, error) {
 	c.Logger.Log("msg", "Environment.Create()")
 	ret, err := c.Client.Account.Create(ctx, account)
-	return ret, err
+	return ret, shared.ConvertAPIError(err)
 }
 
 func (c *Account) Update(ctx context.Context, account *orgv1.Account) error {
 	c.Logger.Log("msg", "Environment.Update()")
 	err := c.Client.Account.Update(ctx, account)
-	return err
+	return shared.ConvertAPIError(err)
 }
 
 func (c *Account) Delete(ctx context.Context, account *orgv1.Account) error {
 	c.Logger.Log("msg", "Environment.Delete()")
 	err := c.Client.Account.Delete(ctx, account)
-	return err
+	return shared.ConvertAPIError(err)
 }
 
 func (c *Account) Get(ctx context.Context, account *orgv1.Account) (*orgv1.Account, error) {
 	c.Logger.Log("msg", "Environment.Get()")
 	ret, err := c.Client.Account.Get(ctx, account)
-	return ret, err
+	return ret, shared.ConvertAPIError(err)
 }
 
 func (c *Account) List(ctx context.Context, account *orgv1.Account) ([]*orgv1.Account, error) {
 	c.Logger.Log("msg", "Environment.List()")
 	ret, err := c.Client.Account.List(ctx, account)
-	return ret, err
+	return ret, shared.ConvertAPIError(err)
 }
