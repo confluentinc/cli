6d284f31977f12eee0e7ac9cbafc19693f8e93ab
Cody A. Ray <cody@confluent.io>
2019-04-29 13:27:08 -0500

    add one more test case

--- test/kafka_test.go
+++ test/kafka_test.go
@@ -6,11 +6,13 @@ func (s *CLITestSuite) TestKafkaCommands() {
 		// Show what commands are available
 		{args: "kafka cluster --help", fixture: "kafka-cluster-help.golden"},
 		// This is hidden from help, but what if you call it anyway?
-		{args: "kafka cluster create", useKafka: "bob", fixture: "kafka1.golden", wantErrCode: 1},
+		{args: "kafka cluster create", fixture: "kafka1.golden", wantErrCode: 1},
 		// This is hidden from help, but what if you call it anyway... with args?
 		{args: "kafka cluster create my-new-cluster --cloud aws --region us-east-1", useKafka: "bob", fixture: "kafka2.golden", wantErrCode: 1},
 		// This is hidden from help, but what if you call it anyway?
-		{args: "kafka cluster delete", useKafka: "bob", fixture: "kafka3.golden", wantErrCode: 1},
+		{args: "kafka cluster delete", fixture: "kafka3.golden", wantErrCode: 1},
+		// This is hidden from help, but what if you call it anyway... with args?
+		{args: "kafka cluster delete lkc-abc123", fixture: "kafka4.golden", wantErrCode: 1},
 	}
 	resetConfiguration(s.T())
 	for _, tt := range tests {
