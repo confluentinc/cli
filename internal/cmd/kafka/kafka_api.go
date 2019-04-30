caa3385ed19823e694f2e199515f87f573386986
Cody A. Ray <cody@confluent.io>
2019-04-29 21:42:01 -0500

    fix kafka acl --operation options list to be stable sorted

--- internal/cmd/kafka/kafka_api.go
+++ internal/cmd/kafka/kafka_api.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
@@ -145,5 +146,6 @@ OUTER:
 		ops = append(ops, strings.ToLower(v))
 	}
 
+	sort.Strings(ops)
 	return strings.Join(ops, ", ")
 }
