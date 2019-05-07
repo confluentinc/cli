8714d3a0cdf616e767cba6c45d4b3828be94e940
DABH <DABH@users.noreply.github.com>
2019-05-06 22:33:24 -0700

    pr feedback

--- internal/cmd/apikey/command.go
+++ internal/cmd/apikey/command.go
@@ -158,12 +158,7 @@ func (c *command) list(cmd *cobra.Command, args []string) error {
 func (c *command) update(cmd *cobra.Command, args []string) error {
 	apiKey := args[0]
 
-	apiKeys, err := c.client.List(context.Background(), &authv1.ApiKey{AccountId: c.config.Auth.Account.Id})
-	if err != nil {
-		return errors.HandleCommon(err, cmd)
-	}
-
-	_, key, err := getAPIKeyByKey(apiKeys, apiKey)
+	key, err := c.client.Get(context.Background(), &authv1.ApiKey{Key: apiKey, AccountId: c.config.Auth.Account.Id})
 	if err != nil {
 		return errors.HandleCommon(err, cmd)
 	}
@@ -173,7 +168,9 @@ func (c *command) update(cmd *cobra.Command, args []string) error {
 		return errors.HandleCommon(err, cmd)
 	}
 
-	key.Description = description
+	if cmd.Flags().Changed("description") {
+		key.Description = description
+	}
 
 	err = c.client.Update(context.Background(), key)
 	if err != nil {
@@ -196,9 +193,6 @@ func (c *command) create(cmd *cobra.Command, args []string) error {
 	userId, err := cmd.Flags().GetInt32("service-account-id")
 	if err != nil {
 		return errors.HandleCommon(err, cmd)
-	}
-	if userId == 0 {
-		userId = c.config.Auth.User.Id
 	}
 
 	description, err := cmd.Flags().GetString("description")
@@ -229,27 +223,6 @@ func (c *command) create(cmd *cobra.Command, args []string) error {
 	}
 
 	return nil
-}
-
-func getAPIKeyByKey(apiKeys []*authv1.ApiKey, apiKey string) (int32, *authv1.ApiKey, error) {
-	var id int32
-	var key *authv1.ApiKey
-	for _, _key := range apiKeys {
-		if _key.Key == apiKey {
-			id = _key.Id
-			key = _key
-			break
-		}
-	}
-
-	if id == 0 {
-		return id, nil, fmt.Errorf(" Invalid Key")
-	}
-
-	key.Modified = nil
-	key.Created = nil
-
-	return id, key, nil
 }
 
 func (c *command) delete(cmd *cobra.Command, args []string) error {
