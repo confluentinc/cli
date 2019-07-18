7fc34a38b6d3b1d77f9d0ea181f8eb93cd3cd523
Cyrus Vafadari <cyrus@confluent.io>
2019-07-18 10:17:42 -0700

    WIP

--- internal/cmd/schema-registry/command_schema_test.go
+++ internal/cmd/schema-registry/command_schema_test.go
@@ -1,3 +1,95 @@
 package schema_registry
 
-//todo
+import (
+	"context"
+	"fmt"
+	"github.com/confluentinc/ccloud-sdk-go/mock"
+	kafkav1 "github.com/confluentinc/ccloudapis/kafka/v1"
+	orgv1 "github.com/confluentinc/ccloudapis/org/v1"
+	srv1 "github.com/confluentinc/ccloudapis/schemaregistry/v1"
+	cmd2 "github.com/confluentinc/cli/internal/pkg/cmd"
+	"github.com/confluentinc/cli/internal/pkg/config"
+	"github.com/confluentinc/cli/internal/pkg/log"
+
+	cliMock "github.com/confluentinc/cli/mock"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type SchemaTestSuite struct {
+	suite.Suite
+	conf         *config.Config
+	kafkaCluster *kafkav1.KafkaCluster
+	srCluster    *srv1.SchemaRegistryCluster
+	srMock       *mock.SchemaRegistry
+}
+
+func (suite *SchemaTestSuite) SetupSuite() {
+	suite.conf = config.New()
+	suite.conf.Logger = log.New()
+	suite.conf.AuthURL = "http://test"
+	suite.conf.Auth = &config.AuthConfig{
+		User:    new(orgv1.User),
+		Account: &orgv1.Account{Id: "testAccount"},
+	}
+	user := suite.conf.Auth
+	name := fmt.Sprintf("login-%s-%s", user.User.Email, suite.conf.AuthURL)
+
+	suite.conf.Platforms[name] = &config.Platform{
+		Server: suite.conf.AuthURL,
+	}
+
+	suite.conf.Credentials[name] = &config.Credential{
+		Username: user.User.Email,
+	}
+
+	suite.conf.Contexts[name] = &config.Context{
+		Platform:      name,
+		Credential:    name,
+		Kafka:         kafkaClusterID,
+		KafkaClusters: map[string]*config.KafkaClusterConfig{kafkaClusterID: {}},
+	}
+
+	suite.conf.CurrentContext = name
+
+	suite.kafkaCluster = &kafkav1.KafkaCluster{
+		Id:         kafkaClusterID,
+		Enterprise: true,
+	}
+
+	suite.srCluster = &srv1.SchemaRegistryCluster{
+		Id: srClusterID,
+	}
+}
+
+func (suite *SchemaTestSuite) SetupTest() {
+	suite.srMock = &mock.SchemaRegistry{
+		CreateSchemaRegistryClusterFunc: func(ctx context.Context, clusterConfig *srv1.SchemaRegistryClusterConfig) (*srv1.SchemaRegistryCluster, error) {
+			return suite.srCluster, nil
+		},
+		GetSchemaRegistryClusterFunc: func(ctx context.Context, clusterConfig *srv1.SchemaRegistryCluster) (*srv1.SchemaRegistryCluster, error) {
+			return nil, nil
+		},
+	}
+}
+
+func (suite *SchemaTestSuite) newCMD() *cobra.Command {
+	cmd := New(&cliMock.Commander{}, suite.conf, suite.srMock, &cmd2.ConfigHelper{})
+	return cmd
+}
+
+func (suite *SchemaTestSuite) TestDescribeById() {
+	cmd := suite.newCMD()
+	cmd.SetArgs(append([]string{"schema", "describe", "1337"}))
+
+	err := cmd.Execute()
+	req := require.New(suite.T())
+	req.Nil(err)
+	req.True(suite.srMock.CreateSchemaRegistryClusterCalled())
+}
+
+func TestSchemaSuite(t *testing.T) {
+	suite.Run(t, new(SchemaTestSuite))
+}
