4113aaf162d62b7087b251fd6976c174ff58cdc3
DABH <DABH@users.noreply.github.com>
2019-08-12 23:32:13 -0700

    clean up / pr feedback

--- internal/pkg/auth-server/auth-server.go
+++ internal/pkg/auth-server/auth-server.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -26,9 +27,7 @@ var (
 	Auth0Identifier  = "https://confluent.auth0.com/api/v2/"
 	Auth0State       = "ConfluentCLI"
 
-	server = &http.Server{}
-	wg     = &sync.WaitGroup{}
-	bgErr  = *new(error)
+	bgErr = *new(error)
 )
 
 /*
@@ -36,6 +35,8 @@ AuthServer is an HTTP server embedded in the CLI to serve callback requests for 
 The server runs in a goroutine / in the background.
 */
 type AuthServer struct {
+	server                  *http.Server
+	wg                      *sync.WaitGroup
 	CodeVerifier            string
 	CodeChallenge           string
 	Auth0AuthenticationCode string
@@ -48,7 +49,7 @@ func (s *AuthServer) GenerateCodes() error {
 
 	_, err := rand.Read(randomBytes)
 	if err != nil {
-		return errors.Wrap(err, "Unable to generate random bytes for code verifier")
+		return errors.Wrap(err, "unable to generate random bytes for code verifier")
 	}
 
 	s.CodeVerifier = base64.RawURLEncoding.EncodeToString(randomBytes)
@@ -56,7 +57,7 @@ func (s *AuthServer) GenerateCodes() error {
 	hasher := sha256.New()
 	_, err = hasher.Write([]byte(s.CodeVerifier))
 	if err != nil {
-		return errors.Wrap(err, "Unable to compute hash for code challenge")
+		return errors.Wrap(err, "unable to compute hash for code challenge")
 	}
 	s.CodeChallenge = base64.RawURLEncoding.EncodeToString(hasher.Sum(nil))
 
@@ -81,14 +82,20 @@ func (s *AuthServer) Start(env string) error {
 	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 26635}) // confl
 
 	if err != nil {
-		return errors.Wrap(err, "Unable to start HTTP server")
+		return errors.Wrap(err, "unable to start HTTP server")
 	}
 
-	wg.Add(1)
+	s.wg = &sync.WaitGroup{}
+	s.server = &http.Server{}
+
+	s.wg.Add(1)
 	go func() {
-		serverErr := server.Serve(listener)
-		if serverErr != nil {
-			fmt.Println(serverErr.Error())
+		serverErr := s.server.Serve(listener)
+		// Serve returns ErrServerClosed when the server is gracefully, intentionally closed:
+		// https://go.googlesource.com/go/+/master/src/net/http/server.go#2854
+		// So don't surface that error to the user.
+		if serverErr != nil && serverErr.Error() != "http: Server closed" {
+			fmt.Fprintf(os.Stderr, "CLI HTTP auth server encountered error while running: %s\n", serverErr.Error())
 		}
 	}()
 
@@ -98,36 +105,36 @@ func (s *AuthServer) Start(env string) error {
 // GetAuthorizationCode takes the code verifier/challenge and gets an authorization code from Auth0
 func (s *AuthServer) GetAuthorizationCode(auth0ConnectionName string) error {
 	url := "https://" + Auth0Domain + "/authorize?" +
-		"response_type=code&" +
-		"code_challenge=" + s.CodeChallenge + "&" +
-		"code_challenge_method=S256&" +
-		"client_id=" + Auth0ClientID + "&" +
-		"redirect_uri=" + Auth0CallbackURL + "&" +
-		"scope=email%20openid&" +
-		"audience=" + Auth0Identifier + "&" +
-		"state=" + Auth0State
+		"response_type=code" +
+		"&code_challenge=" + s.CodeChallenge +
+		"&code_challenge_method=S256" +
+		"&client_id=" + Auth0ClientID +
+		"&redirect_uri=" + Auth0CallbackURL +
+		"&scope=email%20openid" +
+		"&audience=" + Auth0Identifier +
+		"&state=" + Auth0State
 	if auth0ConnectionName != "" {
 		url += "&connection=" + auth0ConnectionName
 	}
 
 	err := browser.OpenURL(url)
 	if err != nil {
-		return errors.Wrap(err, "Unable to open web browser for authorization")
+		return errors.Wrap(err, "unable to open web browser for authorization")
 	}
 
 	// Wait until flow is finished / callback is called (or timeout...)
 	go func() {
 		time.Sleep(30 * time.Second)
-		bgErr = errors.New("Timed out while waiting for browser authentication to occur.  Please try logging in again.")
-		server.Close()
-		wg.Done()
+		bgErr = errors.New("timed out while waiting for browser authentication to occur; please try logging in again")
+		s.server.Close()
+		s.wg.Done()
 	}()
-	wg.Wait()
+	s.wg.Wait()
 
 	defer func() {
-		serverErr := server.Shutdown(context.Background())
+		serverErr := s.server.Shutdown(context.Background())
 		if serverErr != nil {
-			fmt.Println(serverErr.Error())
+			fmt.Fprintf(os.Stderr, "CLI HTTP auth server encountered error while shutting down: %s\n", serverErr.Error())
 		}
 	}()
 
@@ -142,25 +149,25 @@ func (s *AuthServer) CallbackHandler(rw http.ResponseWriter, request *http.Reque
 	if ok {
 		s.Auth0AuthenticationCode = codes[0]
 	} else {
-		bgErr = errors.New("Authentication callback URL did not contain code parameter in query string.  Login will fail.")
+		bgErr = errors.New("authentication callback URL did not contain code parameter in query string, login will fail")
 	}
 
-	wg.Done()
+	s.wg.Done()
 }
 
 // GetAuth0Token exchanges the obtained Auth0 authorization code for an auth/ID token from Auth0
 func (s *AuthServer) GetAuth0Token() error {
 	url := "https://" + Auth0Domain + "/oauth/token"
-	payload := strings.NewReader("grant_type=authorization_code&" +
-		"client_id=" + Auth0ClientID + "&" +
-		"code_verifier=" + s.CodeVerifier + "&" +
-		"code=" + s.Auth0AuthenticationCode + "&" +
-		"redirect_uri=" + Auth0CallbackURL)
+	payload := strings.NewReader("grant_type=authorization_code" +
+		"&client_id=" + Auth0ClientID +
+		"&code_verifier=" + s.CodeVerifier +
+		"&code=" + s.Auth0AuthenticationCode +
+		"&redirect_uri=" + Auth0CallbackURL)
 	req, _ := http.NewRequest("POST", url, payload)
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return errors.Wrap(err, "Failed to perform GetAuth0Token request")
+		return errors.Wrap(err, "failed to get oauth token")
 	}
 
 	defer res.Body.Close()
@@ -169,14 +176,14 @@ func (s *AuthServer) GetAuth0Token() error {
 	var data map[string]interface{}
 	err = json.Unmarshal([]byte(responseBody), &data)
 	if err != nil {
-		return errors.Wrap(err, "Failed to unmarshal response body in GetAuth0Token request")
+		return errors.Wrap(err, "failed to unmarshal response body in oauth token request")
 	}
 
 	token, ok := data["id_token"]
 	if ok {
 		s.Auth0IDToken = token.(string)
 	} else {
-		return errors.New("GetAuth0Token response body did not contain id_token field")
+		return errors.New("oauth token response body did not contain id_token field")
 	}
 
 	return nil
