09946178c3fef42cc689ef9f1ae90e907e7bf6e9
Prathibha <Prathibha.673@gmail.com>
2019-08-20 17:09:35 -0700

    Add keystore mocks

--- internal/cmd/apikey/apikey_test.go
+++ internal/cmd/apikey/apikey_test.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/spf13/cobra"
-	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 	ccsdkmock "github.com/confluentinc/ccloud-sdk-go/mock"
 	authv1 "github.com/confluentinc/ccloudapis/auth/v1"
 	orgv1 "github.com/confluentinc/ccloudapis/org/v1"
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
-	"github.com/confluentinc/cli/internal/pkg/log"
 	"github.com/confluentinc/cli/internal/pkg/config"
 	"github.com/confluentinc/cli/internal/pkg/keystore"
+	"github.com/confluentinc/cli/internal/pkg/log"
+	"github.com/confluentinc/cli/internal/pkg/mock"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	kafkav1 "github.com/confluentinc/ccloudapis/kafka/v1"
 
@@ -30,11 +31,12 @@ const (
 
 type APITestSuite struct {
 	suite.Suite
-	conf         *config.Config
-	apiMock      *ccsdkmock.APIKey
-	keystore     keystore.KeyStore
-	kafkaCluster *kafkav1.KafkaCluster
-	srCluster    *srv1.SchemaRegistryCluster
+	conf             *config.Config
+	apiMock          *ccsdkmock.APIKey
+	keystore         keystore.KeyStore
+	kafkaCluster     *kafkav1.KafkaCluster
+	srCluster        *srv1.SchemaRegistryCluster
+	srMothershipMock *ccsdkmock.SchemaRegistry
 }
 
 func (suite *APITestSuite) SetupSuite() {
@@ -65,11 +67,13 @@ func (suite *APITestSuite) SetupSuite() {
 
 	suite.conf.CurrentContext = name
 
+	srCluster, _ := suite.conf.SchemaRegistryCluster()
+	srCluster.SrCredentials = &config.APIKeyPair{Key: "abracadabra", Secret: "opensesame"}
+
 	suite.kafkaCluster = &kafkav1.KafkaCluster{
 		Id:         kafkaClusterID,
 		Enterprise: true,
 	}
-
 	suite.srCluster = &srv1.SchemaRegistryCluster{
 		Id: srClusterID,
 	}
@@ -77,7 +81,38 @@ func (suite *APITestSuite) SetupSuite() {
 
 //Require
 func (suite *APITestSuite) SetupTest() {
+	suite.srMothershipMock = &ccsdkmock.SchemaRegistry{
+		CreateSchemaRegistryClusterFunc: func(ctx context.Context, clusterConfig *srv1.SchemaRegistryClusterConfig) (*srv1.SchemaRegistryCluster, error) {
+			return suite.srCluster, nil
+		},
+		GetSchemaRegistryClusterFunc: func(ctx context.Context, clusterConfig *srv1.SchemaRegistryCluster) (*srv1.SchemaRegistryCluster, error) {
+			return nil, nil
+		},
+		GetSchemaRegistryClustersFunc: func(arg0 context.Context, arg1 *srv1.SchemaRegistryCluster) (clusters []*srv1.SchemaRegistryCluster, e error) {
+			return []*srv1.SchemaRegistryCluster{suite.srCluster},nil
+		},
+	}
+	suite.keystore = &mock.KeyStore{
+		HasAPIKeyFunc: func(key, clusterID, environment string) (b bool, e error) {
+			return true, nil
+		},
+		StoreAPIKeyFunc: func(key *authv1.ApiKey, clusterID, environment string) error {
+			return nil
+		},
+		DeleteAPIKeyFunc: func(key string) error {
+			return nil
+		},
+	}
 	suite.apiMock = &ccsdkmock.APIKey{
+		GetFunc: func(ctx context.Context, apiKey *authv1.ApiKey) (key *authv1.ApiKey, e error) {
+			return &authv1.ApiKey{
+				Key:    "abrcadabra",
+				Secret: "opensesame",
+			}, nil
+		},
+		UpdateFunc: func(ctx context.Context, apiKey *authv1.ApiKey) error {
+			return nil
+		},
 		CreateFunc: func(ctx context.Context, apiKey *authv1.ApiKey) (*authv1.ApiKey, error) {
 			return &authv1.ApiKey{
 				Key:    "abrcadabra",
@@ -100,7 +135,7 @@ func (suite *APITestSuite) SetupTest() {
 }
 
 func (suite *APITestSuite) newCMD() *cobra.Command {
-	cmd := New(&cliMock.Commander{}, suite.conf, nil, &pcmd.ConfigHelper{}, suite.keystore)
+	cmd := New(&cliMock.Commander{}, suite.conf, suite.apiMock, &pcmd.ConfigHelper{Config: suite.conf}, suite.keystore)
 	return cmd
 }
 
@@ -121,7 +156,7 @@ func (suite *APITestSuite) TestListSrApiKey() {
 	err := cmd.Execute()
 	req := require.New(suite.T())
 	req.Nil(err)
-	//req.True(suite.apiMock.ListCalled())
+	req.True(suite.apiMock.ListCalled())
 }
 
 func (suite *APITestSuite) TestDeleteApiKey() {
@@ -131,7 +166,7 @@ func (suite *APITestSuite) TestDeleteApiKey() {
 	err := cmd.Execute()
 	req := require.New(suite.T())
 	req.Nil(err)
-	//req.True(suite.apiMock.DeleteCalled())
+	req.True(suite.apiMock.DeleteCalled())
 }
 
 func TestApiTestSuite(t *testing.T) {
