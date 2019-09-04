c645dc0a9a806796fa143fbbe1a8ed7445e3dc25
Prathibha <prathibha@confluent.io>
2019-09-04 16:59:58 -0700

    WIP integration test

--- test/schema-registry_test.go
+++ test/schema-registry_test.go
@@ -5,14 +5,6 @@ func (s *CLITestSuite) TestSrCommands() {
 	tests := []CLITest{
 		// Show what commands are available
 		{args: "schema-registry --help", fixture: "schema-registry-help.golden"},
-		// This is hidden from help, but what if you call it anyway?
-		{args: "schema-registry cluster create", fixture: "sr1.golden", wantErrCode: 1},
-		// This is hidden from help, but what if you call it anyway... with args?
-		{args: "schema-registry cluster enable --cloud aws --geo us", useKafka: "bob", fixture: "kafka2.golden", wantErrCode: 1},
-		// This is hidden from help, but what if you call it anyway?
-		{args: "kafka cluster delete", fixture: "kafka3.golden", wantErrCode: 1},
-		// This is hidden from help, but what if you call it anyway... with args?
-		{args: "kafka cluster delete lkc-abc123", fixture: "kafka4.golden", wantErrCode: 1},
 	}
 	resetConfiguration(s.T(), "ccloud")
 	for _, tt := range tests {
