e32403c68128131004cf0a8fdafbb807654ff80b
Liz Bennett <lbennett@confluent.io>
2019-11-14 09:44:23 -0800

    Fix configs for staging and cpd environments in SSO auth flows

--- internal/pkg/sso/auth_state.go
+++ internal/pkg/sso/auth_state.go
@@ -13,15 +13,17 @@ import (
 )
 
 var (
-	ssoProviderDomain               = "login.confluent.io"
-	ssoProviderDomainDevel          = "login.confluent-dev.io"
-	ssoProviderClientID             = "hPbGZM8G55HSaUsaaieiiAprnJaEc9rH"
-	ssoProviderClientIDDevel        = "XKlqgOEo39iyonTl3Yv3IHWIXGKDP3fA"
-	ssoProviderCallbackLocalURL     = "http://127.0.0.1:26635/cli_callback"
-	ssoProviderCallbackCCloudURL    = "https://confluent.cloud/cli_callback" // used in the --no-browser sso flow
-	ssoProviderCallbackCCloudDevURL = "https://devel.cpdev.cloud/cli_callback"
-	ssoProviderIdentifier           = "https://confluent.auth0.com/api/v2/"
-	ssoProviderIdentifierDevel      = "https://confluent-dev.auth0.com/api/v2/"
+	ssoProviderDomain                = "login.confluent.io"
+	ssoProviderDomainDevel           = "login.confluent-dev.io"
+	ssoProviderClientID              = "hPbGZM8G55HSaUsaaieiiAprnJaEc9rH"
+	ssoProviderClientIDDevel         = "XKlqgOEo39iyonTl3Yv3IHWIXGKDP3fA"
+	ssoProviderCallbackEndpoint      = "/cli_callback"
+	ssoProviderCallbackLocalURL      = "http://127.0.0.1:26635" + ssoProviderCallbackEndpoint
+	ssoProviderCallbackCCloudURL     = "https://confluent.cloud" + ssoProviderCallbackEndpoint // used in the --no-browser sso flow
+	ssoProviderCallbackCCloudDevURL  = "https://devel.cpdev.cloud" + ssoProviderCallbackEndpoint
+	ssoProviderCallbackCCloudStagURL = "https://stag.cpdev.cloud" + ssoProviderCallbackEndpoint
+	ssoProviderIdentifier            = "https://confluent.auth0.com/api/v2/"
+	ssoProviderIdentifierDevel       = "https://confluent-dev.auth0.com/api/v2/"
 )
 
 /*
@@ -45,7 +47,10 @@ type authState struct {
 // auth0 server / SSO integration.
 func newState(config *config.Config) (*authState, error) {
 	env := "prod"
-	if strings.Contains(config.AuthURL, "devel.cpdev.cloud") || strings.Contains(config.AuthURL, "priv.cpdev.cloud") {
+	if strings.Contains(config.AuthURL, "priv.cpdev.cloud") {
+		env = "cpd"
+	}
+	if strings.Contains(config.AuthURL, "devel.cpdev.cloud") {
 		env = "devel"
 	}
 	if strings.Contains(config.AuthURL, "stag.cpdev.cloud") {
@@ -53,32 +58,39 @@ func newState(config *config.Config) (*authState, error) {
 	}
 
 	state := &authState{}
+	switch env {
+	case "cpd":
+    	state.SSOProviderCallbackUrl = config.AuthURL + ssoProviderCallbackEndpoint // callback to the cpd cluster url that was passed in
+		state.SSOProviderHost = "https://" + ssoProviderDomainDevel // only one Auth0 account for cpd, dev and stag
+		state.SSOProviderClientID = ssoProviderClientIDDevel
+		state.SSOProviderIdentifier = ssoProviderIdentifierDevel
+	case "devel":
+	    state.SSOProviderCallbackUrl = ssoProviderCallbackCCloudDevURL
+		state.SSOProviderHost = "https://" + ssoProviderDomainDevel
+		state.SSOProviderClientID = ssoProviderClientIDDevel
+		state.SSOProviderIdentifier = ssoProviderIdentifierDevel
+	case "stag":
+	    state.SSOProviderCallbackUrl = ssoProviderCallbackCCloudStagURL
+		state.SSOProviderHost = "https://" + ssoProviderDomainDevel
+		state.SSOProviderClientID = ssoProviderClientIDDevel
+		state.SSOProviderIdentifier = ssoProviderIdentifierDevel
+	case "prod":
+	    state.SSOProviderCallbackUrl = ssoProviderCallbackCCloudURL
+		state.SSOProviderHost = "https://" + ssoProviderDomain
+		state.SSOProviderClientID = ssoProviderClientID
+		state.SSOProviderIdentifier = ssoProviderIdentifier
+	}
+
+	if !config.NoBrowser {
+		// if we're not using the no browser flow, the callback will always be localhost regardless of environment
+		state.SSOProviderCallbackUrl = ssoProviderCallbackLocalURL
+	}
+
 	err := state.generateCodes()
 	if err != nil {
 		return nil, err
 	}
 
-	if env == "devel" || env == "stag" {
-		state.SSOProviderHost = "https://" + ssoProviderDomainDevel
-		state.SSOProviderClientID = ssoProviderClientIDDevel
-		state.SSOProviderIdentifier = ssoProviderIdentifierDevel
-
-		if config.NoBrowser {
-			state.SSOProviderCallbackUrl = ssoProviderCallbackCCloudDevURL
-		} else {
-			state.SSOProviderCallbackUrl = ssoProviderCallbackLocalURL
-		}
-	} else {
-		state.SSOProviderHost = "https://" + ssoProviderDomain
-		state.SSOProviderClientID = ssoProviderClientID
-		state.SSOProviderIdentifier = ssoProviderIdentifier
-
-		if config.NoBrowser {
-			state.SSOProviderCallbackUrl = ssoProviderCallbackCCloudURL
-		} else {
-			state.SSOProviderCallbackUrl = ssoProviderCallbackLocalURL
-		}
-	}
 	return state, nil
 }
 
