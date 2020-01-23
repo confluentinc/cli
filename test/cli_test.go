a40b98f00651e2643533c6253245b463709ae69c
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-01-23 15:13:47 -0800

    fixed and refacto iam role commands, refactor iam test, started on tests for iam role

--- test/cli_test.go
+++ test/cli_test.go
@@ -25,16 +25,15 @@ import (
 	"time"
 
 	"github.com/chromedp/chromedp"
-
-	"github.com/confluentinc/bincover"
 	"github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/confluentinc/bincover"
 	"github.com/confluentinc/ccloud-sdk-go"
 	authv1 "github.com/confluentinc/ccloudapis/auth/v1"
-	corev1 "github.com/confluentinc/ccloudapis/core/v1"
 	connectv1 "github.com/confluentinc/ccloudapis/connect/v1"
+	corev1 "github.com/confluentinc/ccloudapis/core/v1"
 	kafkav1 "github.com/confluentinc/ccloudapis/kafka/v1"
 	ksqlv1 "github.com/confluentinc/ccloudapis/ksql/v1"
 	orgv1 "github.com/confluentinc/ccloudapis/org/v1"
@@ -172,97 +171,6 @@ func (s *CLITestSuite) Test_Confluent_Help() {
 			{args: "--help", fixture: "confluent-help-flag.golden"},
 			{args: "version", fixture: "confluent-version.golden", regex: true},
 		}
-	}
-	for _, tt := range tests {
-		kafkaAPIURL := serveKafkaAPI(s.T()).URL
-		s.runConfluentTest(tt, serveMds(s.T(), kafkaAPIURL).URL)
-	}
-}
-
-func (s *CLITestSuite) Test_Confluent_Iam_Rolebinding_List() {
-	tests := []CLITest{
-		{
-			name:        "confluent iam rolebinding list, no principal nor role",
-			args:        "iam rolebinding list --kafka-cluster-id CID",
-			fixture:     "confluent-iam-rolebinding-list-no-principal-nor-role.golden",
-			login:       "default",
-			wantErrCode: 1,
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal User:frodo",
-			fixture: "confluent-iam-rolebinding-list-user.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal User:frodo --role DeveloperRead",
-			fixture: "confluent-iam-rolebinding-list-user-and-role-with-multiple-resources-from-one-group.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal User:frodo --role DeveloperWrite",
-			fixture: "confluent-iam-rolebinding-list-user-and-role-with-resources-from-multiple-groups.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal User:frodo --role SecurityAdmin",
-			fixture: "confluent-iam-rolebinding-list-user-and-role-with-cluster-resource.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal User:frodo --role SystemAdmin",
-			fixture: "confluent-iam-rolebinding-list-user-and-role-with-no-matches.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal Group:hobbits --role DeveloperRead",
-			fixture: "confluent-iam-rolebinding-list-group-and-role-with-multiple-resources.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal Group:hobbits --role DeveloperWrite",
-			fixture: "confluent-iam-rolebinding-list-group-and-role-with-one-resource.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --principal Group:hobbits --role SecurityAdmin",
-			fixture: "confluent-iam-rolebinding-list-group-and-role-with-no-matches.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role DeveloperRead",
-			fixture: "confluent-iam-rolebinding-list-role-with-multiple-bindings-to-one-group.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role DeveloperWrite",
-			fixture: "confluent-iam-rolebinding-list-role-with-bindings-to-multiple-groups.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role SecurityAdmin",
-			fixture: "confluent-iam-rolebinding-list-role-on-cluster-bound-to-user.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role SystemAdmin",
-			fixture: "confluent-iam-rolebinding-list-role-with-no-matches.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role DeveloperRead --resource Topic:food",
-			fixture: "confluent-iam-rolebinding-list-role-and-resource-with-exact-match.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role DeveloperRead --resource Topic:shire-parties",
-			fixture: "confluent-iam-rolebinding-list-role-and-resource-with-no-match.golden",
-			login:   "default",
-		},
-		{
-			args:    "iam rolebinding list --kafka-cluster-id CID --role DeveloperWrite --resource Topic:shire-parties",
-			fixture: "confluent-iam-rolebinding-list-role-and-resource-with-prefix-match.golden",
-			login:   "default",
-		},
 	}
 	for _, tt := range tests {
 		kafkaAPIURL := serveKafkaAPI(s.T()).URL
