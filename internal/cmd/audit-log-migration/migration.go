3e869bdb2d49a3f0f22981fd7c93c8defef1b419
Raja Shravan <rajashravan@hotmail.com>
2020-04-21 11:05:57 -0400

    finishes migration module; passing test case

--- internal/cmd/audit-log-migration/migration.go
+++ internal/cmd/audit-log-migration/migration.go
@@ -3,6 +3,8 @@ package audit_log_migration
 import (
   "fmt"
   "encoding/json"
+  "regexp"
+  "strings"
 
   "github.com/confluentinc/mds-sdk-go"
 )
@@ -24,19 +26,26 @@ func AuditLogConfigTranslation(clusterConfigs map[string]string, bootstrapServer
   setDefaultTopic(&newSpec, "confluent-audit-log-events") // TODO: make this a CONST somehwere
 
   // step 4 - combine exlcuded ExcludedPrincipals
-  //combineExludedPrincipals(clusterAuditLogConfigSpecs, &newSpec)
+  combineExludedPrincipals(clusterAuditLogConfigSpecs, &newSpec)
 
+  // step 5 - combine routes and Replace the CRN authority
+  combineRoutes(clusterAuditLogConfigSpecs, &newSpec)
+  replaceCRNAuthorityRoutes(&newSpec, crnAuthority)
 
+  // step 6 - generate routes for default topics
+  generateAlternateDefaultTopicRoutes(clusterAuditLogConfigSpecs, &newSpec, crnAuthority)
 
   printSpec(&newSpec)
-
-  fmt.Println(crnAuthority)
-
   return newSpec
 }
 
 func printSpec(spec *mds.AuditLogConfigSpec) {
-  fmt.Println(spec.Destinations)
+  for k, v := range spec.Routes {
+    fmt.Println(k)
+    if v.Other != nil {
+      fmt.Println(*v.Other.Allowed)
+    }
+  }
 }
 
 func jsonConfigsToAuditLogConfigSpecs(clusterConfigs map[string]string) map[string]*mds.AuditLogConfigSpec {
@@ -44,6 +53,8 @@ func jsonConfigsToAuditLogConfigSpecs(clusterConfigs map[string]string) map[stri
   for k, v := range clusterConfigs {
     var spec mds.AuditLogConfigSpec
     json.Unmarshal([]byte(v), &spec)
+    fmt.Println(v)
+    fmt.Println(spec)
     clusterAuditLogConfigSpecs[k] = &spec
   }
   return clusterAuditLogConfigSpecs
@@ -85,13 +96,130 @@ func setDefaultTopic(newSpec *mds.AuditLogConfigSpec, defaultTopicName string) {
   }
 }
 
-// func combineExludedPrincipals(newSpec *mds.AuditLogConfigSpec, defaultTopicName string) {
-//
-// }
+func combineExludedPrincipals(specs map[string]*mds.AuditLogConfigSpec, newSpec *mds.AuditLogConfigSpec) {
+  var newExcludedPrincipals []string
+
+  for _, spec := range specs {
+    excludedPrincipals := spec.ExcludedPrincipals
+    for _, principal := range excludedPrincipals {
+      if !find(newExcludedPrincipals, principal) {
+        newExcludedPrincipals = append(newExcludedPrincipals, principal)
+      }
+    }
+  }
+
+  newSpec.ExcludedPrincipals = newExcludedPrincipals
+}
+
+func combineRoutes(specs map[string]*mds.AuditLogConfigSpec, newSpec *mds.AuditLogConfigSpec) {
+  newRoutes := make(map[string]mds.AuditLogConfigRouteCategories)
+
+  for clusterId, spec := range specs {
+    routes := spec.Routes
+    for crnPath, route := range routes {
+      newCrnPath := replaceClusterId(crnPath, clusterId)
+      newRoutes[newCrnPath] = route
+    }
+  }
+
+  newSpec.Routes = newRoutes
+}
+
+func replaceCRNAuthorityRoutes(newSpec *mds.AuditLogConfigSpec, newCrnAuthority string) {
+  routes := newSpec.Routes
+  for k, _ := range newSpec.Routes {
+    fmt.Println(k)
+  }
+  for crnPath, routeValue := range routes {
+    // if crn path is already valid, no need to replace!
+    if !crnPathContainsAuthority(crnPath, newCrnAuthority) {
+      newCrnPath := replaceCRNAuthority(crnPath, newCrnAuthority)
+      routes[newCrnPath] = routeValue
+      delete(routes, crnPath)
+    }
+  }
+}
+
+func crnPathContainsAuthority(crnPath, crnAuthority string) bool {
+  re := regexp.MustCompile("^crn://" + crnAuthority + "/.*")
+  return re.Match([]byte(crnPath))
+}
+
+func replaceCRNAuthority(crnPath, newCrnAuthority string) string {
+  re := regexp.MustCompile("crn://([^/]*)/")
+  return string(re.ReplaceAll([]byte(crnPath), []byte("crn://" + newCrnAuthority + "/")))
+}
+
+func replaceClusterId(crnPath, clusterId string) string {
+  const kafkaIdentifier = "kafka=*"
+  if !strings.Contains(crnPath, kafkaIdentifier) {
+    err := fmt.Errorf("%q not present in crnPath %q, cannot insert clusterId", kafkaIdentifier, crnPath)
+  	fmt.Println(err.Error())
+    return crnPath
+  }
+  return strings.Replace(crnPath, kafkaIdentifier, "kafka=" + clusterId, 1)
+}
+
+func generateCrnPath(clusterId, crnAuthority, pathExtension string) string {
+  path := "crn://" + crnAuthority + "/kafka=" + clusterId
+  if pathExtension != "" {
+    path += "/" + pathExtension + "=*"
+  }
+  return path
+}
+
+func generateAlternateDefaultTopicRoutes(specs map[string]*mds.AuditLogConfigSpec, newSpec *mds.AuditLogConfigSpec, crnAuthority string) {
+  for clusterId, spec := range specs {
+    defaultTopic := spec.DefaultTopics.Denied
+    fmt.Println(clusterId)
+    if defaultTopic != "confluent-audit-log-events" {
+      other := mds.AuditLogConfigRouteCategoryTopics{
+        Allowed: &defaultTopic,
+        Denied: &defaultTopic,
+      }
+
+      // // add OTHER block
+      for routeName, route := range newSpec.Routes {
+        if strings.Contains(routeName, "kafka=" + clusterId) {
+
+          if newSpec.Routes[routeName].Other == nil {
+            route.Other = &other
+            newSpec.Routes[routeName] = route
+          }
+        }
+      }
+
+      // add the four new routes to the newSpec, if not already there
+      newRouteConfig := mds.AuditLogConfigRouteCategories{
+        Other: &other,
+      }
+      pathExtensions := []string{"", "topic", "connect", "ksql"}
+      for _, extension := range pathExtensions {
+        routeName := generateCrnPath(clusterId, crnAuthority, extension)
+        if _, ok := newSpec.Routes[routeName]; !ok {
+          newSpec.Routes[routeName] = newRouteConfig
+          fmt.Println("replace")
+          fmt.Println(*newSpec.Routes[routeName].Other.Allowed)
+        }
+      }
+
+
+    }
+  }
+}
 
 func max(x, y int64) int64 {
  if x > y {
    return x
  }
  return y
+}
+
+func find(slice []string, val string) bool {
+    for _, item := range slice {
+        if item == val {
+            return true
+        }
+    }
+    return false
 }
--- internal/cmd/audit-log-migration/migration_test.go
+++ internal/cmd/audit-log-migration/migration_test.go
@@ -2,6 +2,7 @@ package audit_log_migration
 
 import (
   "testing"
+  "encoding/json"
 
 	"github.com/stretchr/testify/require"
 
@@ -13,112 +14,119 @@ func TestAuditLogConfigTranslation(t *testing.T) {
 		clusterConfigs  map[string]string
 		bootstrapServers string
 		crnAuthority  string
+    wantSpecAsString string
 	}{
 		{
 			map[string]string{
-				"cluster123": "{\n    \"destinations\": {\n        \"bootstrap_servers\": [\n            \"audit.example.com:9092\"\n        ],\n        \"topics\": {\n            \"confluent-audit-log-events_payroll\": {\n                \"retention_ms\": 50\n            }\n        }\n    },\n    \"routes\": {\n        \"crn://mds1.example.com/kafka=*/topic=payroll-*\": {\n            \"produce\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            },\n            \"consume\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            },\n            \"other\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            }\n        }\n    },\n    \"excluded_principals\": [\n        \"User:Alice\"\n    ]\n}",
+        "cluster123": "{\n    \"destinations\": {\n        \"bootstrap_servers\": [\n            \"audit.example.com:9092\"\n        ],\n        \"topics\": {\n            \"confluent-audit-log-events_payroll\": {\n                \"retention_ms\": 50\n            },\n            \"confluent-audit-log-events\": {\n                \"retention_ms\": 500\n            }\n        }\n    },\n    \"default_topics\": {\n        \"allowed\": \"confluent-audit-log-events\",\n        \"denied\": \"confluent-audit-log-events\"\n    },\n    \"routes\": {\n        \"crn://mds1.example.com/kafka=*/topic=payroll-*\": {\n            \"produce\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            },\n            \"consume\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            }\n        }\n    },\n    \"excluded_principals\": [\n        \"User:Alice\"\n    ]\n}",
 
-        "clusterABC": "{\n  \"destinations\": {\n      \"bootstrap_servers\": [\n          \"some-server\"\n      ],\n      \"topics\": {\n          \"confluent-audit-log-events_payroll\": {\n              \"retention_ms\": 2592000000\n          },\n          \"confluent-audit-log-events_billing\": {\n              \"retention_ms\": 2592000000\n          }\n      }\n  },\n  \"default_topics\": {\n      \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n      \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n  },\n  \"routes\": {\n      \"crn://mds1.example.com/kafka=*/topic=billing-*\": {\n          \"produce\": {\n              \"allowed\": \"confluent-audit-log-events_billing\",\n              \"denied\": \"confluent-audit-log-events_billing\"\n          },\n          \"consume\": {\n              \"allowed\": \"confluent-audit-log-events_billing\",\n              \"denied\": \"confluent-audit-log-events_billing\"\n          },\n          \"other\": {\n              \"allowed\": \"confluent-audit-log-events_billing\",\n              \"denied\": \"confluent-audit-log-events_billing\"\n          }\n      },\n      \"crn://mds1.example.com/kafka=*/topic=payroll-*\": {\n          \"produce\": {\n              \"allowed\": \"confluent-audit-log-events_payroll\",\n              \"denied\": \"confluent-audit-log-events_payroll\"\n          },\n          \"consume\": {\n              \"allowed\": \"confluent-audit-log-events_payroll\",\n              \"denied\": \"confluent-audit-log-events_payroll\"\n          },\n          \"other\": {\n              \"allowed\": \"confluent-audit-log-events_payroll\",\n              \"denied\": \"confluent-audit-log-events_payroll\"\n          }\n      }\n  },\n  \"excluded_principals\": [\n      \"User:Bob\"\n  ]\n}",
-			},
+        "clusterABC": "{\n  \"destinations\": {\n      \"bootstrap_servers\": [\n          \"some-server\"\n      ],\n      \"topics\": {\n          \"confluent-audit-log-events_payroll\": {\n              \"retention_ms\": 2592000000\n          },\n          \"confluent-audit-log-events_billing\": {\n              \"retention_ms\": 2592000000\n          },\n          \"DIFFERENT-DEFAULT-TOPIC\": {\n              \"retention_ms\": 100\n          }\n      }\n  },\n  \"default_topics\": {\n      \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n      \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n  },\n  \"routes\": {\n      \"crn://mds1.example.com/kafka=*/topic=billing-*\": {\n          \"produce\": {\n              \"allowed\": \"confluent-audit-log-events_billing\",\n              \"denied\": \"confluent-audit-log-events_billing\"\n          },\n          \"consume\": {\n              \"allowed\": \"confluent-audit-log-events_billing\",\n              \"denied\": \"confluent-audit-log-events_billing\"\n          },\n          \"other\": {\n              \"allowed\": \"confluent-audit-log-events_billing\",\n              \"denied\": \"confluent-audit-log-events_billing\"\n          }\n      },\n      \"crn://mds1.example.com/kafka=*/topic=payroll-*\": {\n          \"produce\": {\n              \"allowed\": \"confluent-audit-log-events_payroll\",\n              \"denied\": \"confluent-audit-log-events_payroll\"\n          },\n          \"consume\": {\n              \"allowed\": \"confluent-audit-log-events_payroll\",\n              \"denied\": \"confluent-audit-log-events_payroll\"\n          }\n      }\n  },\n  \"excluded_principals\": [\n      \"User:Bob\"\n  ]\n}",
+      },
       "new_bootstrap",
       "NEW.CRN.AUTHORITY.COM",
+      "{\n    \"destinations\": {\n        \"bootstrap_servers\": [\n            \"new_bootstrap\"\n        ],\n        \"topics\": {\n            \"confluent-audit-log-events\": {\n                \"retention_ms\": 500\n            },\n            \"confluent-audit-log-events_payroll\": {\n                \"retention_ms\": 2592000000\n            },\n            \"confluent-audit-log-events_billing\": {\n                \"retention_ms\": 2592000000\n            },\n            \"DIFFERENT-DEFAULT-TOPIC\": {\n                \"retention_ms\": 100\n            }\n        }\n    },\n    \"default_topics\": {\n        \"allowed\": \"confluent-audit-log-events\",\n        \"denied\": \"confluent-audit-log-events\"\n    },\n    \"routes\": {\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=billing-*\": {\n            \"produce\": {\n                \"allowed\": \"confluent-audit-log-events_billing\",\n                \"denied\": \"confluent-audit-log-events_billing\"\n            },\n            \"consume\": {\n                \"allowed\": \"confluent-audit-log-events_billing\",\n                \"denied\": \"confluent-audit-log-events_billing\"\n            },\n            \"other\": {\n                \"allowed\": \"confluent-audit-log-events_billing\",\n                \"denied\": \"confluent-audit-log-events_billing\"\n            }\n        },\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=cluster123/topic=payroll-*\": {\n            \"produce\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            },\n            \"consume\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            }\n        },\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=payroll-*\": {\n            \"produce\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            },\n            \"consume\": {\n                \"allowed\": \"confluent-audit-log-events_payroll\",\n                \"denied\": \"confluent-audit-log-events_payroll\"\n            },\n            \"other\": {\n                \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n                \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n            }\n        },\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC\": {\n            \"other\": {\n                \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n                \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n            }\n        },\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=*\": {\n            \"other\": {\n                \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n                \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n            }\n        },\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/connect=*\": {\n            \"other\": {\n                \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n                \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n            }\n        },\n        \"crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/ksql=*\": {\n            \"other\": {\n                \"allowed\": \"DIFFERENT-DEFAULT-TOPIC\",\n                \"denied\": \"DIFFERENT-DEFAULT-TOPIC\"\n            }\n        }\n    },\n    \"excluded_principals\": [\n        \"User:Alice\",\n        \"User:Bob\"\n    ]\n}",
 		},
 	}
 	for _, c := range testCases {
-    billing_topic := "confluent-audit-log-events_billing"
-    payroll_topic := "confluent-audit-log-events_payroll"
-    different_default_topic := "DIFFERENT-DEFAULT-TOPIC"
-    want := mds.AuditLogConfigSpec{
-      Destinations: mds.AuditLogConfigDestinations{
-        BootstrapServers: []string{"new_bootstrap"},
-        Topics: map[string]mds.AuditLogConfigDestinationConfig {
-          "confluent-audit-log-events": mds.AuditLogConfigDestinationConfig{
-            RetentionMs: 7776000000,
-          },
-          "confluent-audit-log-events_payroll": mds.AuditLogConfigDestinationConfig{
-            RetentionMs: 2592000000,
-          },
-          "confluent-audit-log-events_billing": mds.AuditLogConfigDestinationConfig{
-            RetentionMs: 2592000000,
-          },
-        },
-      },
-      ExcludedPrincipals: []string{"User:Alice","User:Bob"},
-      DefaultTopics: mds.AuditLogConfigDefaultTopics{
-        Allowed: "confluent-audit-log-events",
-        Denied: "confluent-audit-log-events",
-      },
-      Routes: map[string]mds.AuditLogConfigRouteCategories{
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=billing-*": mds.AuditLogConfigRouteCategories {
-          Produce: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &billing_topic,
-            Denied: &billing_topic,
-          },
-          Consume: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &billing_topic,
-            Denied: &billing_topic,
-          },
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &billing_topic,
-            Denied: &billing_topic,
-          },
-        },
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=cluster123/topic=payroll-*": mds.AuditLogConfigRouteCategories {
-          Produce: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &payroll_topic,
-            Denied: &payroll_topic,
-          },
-          Consume: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &payroll_topic,
-            Denied: &payroll_topic,
-          },
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &payroll_topic,
-            Denied: &payroll_topic,
-          },
-        },
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=payroll-*": mds.AuditLogConfigRouteCategories {
-          Produce: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &payroll_topic,
-            Denied: &payroll_topic,
-          },
-          Consume: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &payroll_topic,
-            Denied: &payroll_topic,
-          },
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &payroll_topic,
-            Denied: &payroll_topic,
-          },
-        },
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC": mds.AuditLogConfigRouteCategories {
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &different_default_topic,
-            Denied: &different_default_topic,
-          },
-        },
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=*": mds.AuditLogConfigRouteCategories {
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &different_default_topic,
-            Denied: &different_default_topic,
-          },
-        },
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/connect=*": mds.AuditLogConfigRouteCategories {
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &different_default_topic,
-            Denied: &different_default_topic,
-          },
-        },
-        "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/ksql=*": mds.AuditLogConfigRouteCategories {
-          Other: &mds.AuditLogConfigRouteCategoryTopics{
-            Allowed: &different_default_topic,
-            Denied: &different_default_topic,
-          },
-        },
-      },
-    }
+    // billing_topic := "confluent-audit-log-events_billing"
+    // payroll_topic := "confluent-audit-log-events_payroll"
+    // different_default_topic := "DIFFERENT-DEFAULT-TOPIC"
+
+    // want := mds.AuditLogConfigSpec{
+    //   Destinations: mds.AuditLogConfigDestinations{
+    //     BootstrapServers: []string{"new_bootstrap"},
+    //     Topics: map[string]mds.AuditLogConfigDestinationConfig {
+    //       "confluent-audit-log-events": mds.AuditLogConfigDestinationConfig{
+    //         RetentionMs: 7776000000,
+    //       },
+    //       "confluent-audit-log-events_payroll": mds.AuditLogConfigDestinationConfig{
+    //         RetentionMs: 2592000000,
+    //       },
+    //       "confluent-audit-log-events_billing": mds.AuditLogConfigDestinationConfig{
+    //         RetentionMs: 2592000000,
+    //       },
+    //     },
+    //   },
+    //   DefaultTopics: mds.AuditLogConfigDefaultTopics{
+    //     Allowed: "confluent-audit-log-events",
+    //     Denied: "confluent-audit-log-events",
+    //   },
+    //   Routes: map[string]mds.AuditLogConfigRouteCategories{
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=billing-*": mds.AuditLogConfigRouteCategories {
+    //       Produce: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &billing_topic,
+    //         Denied: &billing_topic,
+    //       },
+    //       Consume: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &billing_topic,
+    //         Denied: &billing_topic,
+    //       },
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &billing_topic,
+    //         Denied: &billing_topic,
+    //       },
+    //     },
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=cluster123/topic=payroll-*": mds.AuditLogConfigRouteCategories {
+    //       Produce: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &payroll_topic,
+    //         Denied: &payroll_topic,
+    //       },
+    //       Consume: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &payroll_topic,
+    //         Denied: &payroll_topic,
+    //       },
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &payroll_topic,
+    //         Denied: &payroll_topic,
+    //       },
+    //     },
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=payroll-*": mds.AuditLogConfigRouteCategories {
+    //       Produce: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &payroll_topic,
+    //         Denied: &payroll_topic,
+    //       },
+    //       Consume: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &payroll_topic,
+    //         Denied: &payroll_topic,
+    //       },
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &different_default_topic,
+    //         Denied: &different_default_topic,
+    //       },
+    //     },
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC": mds.AuditLogConfigRouteCategories {
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &different_default_topic,
+    //         Denied: &different_default_topic,
+    //       },
+    //     },
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/topic=*": mds.AuditLogConfigRouteCategories {
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &different_default_topic,
+    //         Denied: &different_default_topic,
+    //       },
+    //     },
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/connect=*": mds.AuditLogConfigRouteCategories {
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &different_default_topic,
+    //         Denied: &different_default_topic,
+    //       },
+    //     },
+    //     "crn://NEW.CRN.AUTHORITY.COM/kafka=clusterABC/ksql=*": mds.AuditLogConfigRouteCategories {
+    //       Other: &mds.AuditLogConfigRouteCategoryTopics{
+    //         Allowed: &different_default_topic,
+    //         Denied: &different_default_topic,
+    //       },
+    //     },
+    //   },
+    //   ExcludedPrincipals: []string{"User:Alice","User:Bob"},
+    // }
+    var want mds.AuditLogConfigSpec
+    json.Unmarshal([]byte(c.wantSpecAsString), &want)
+
+
     got := AuditLogConfigTranslation(c.clusterConfigs, c.bootstrapServers, c.crnAuthority)
-    //require.Equal(t, want, got)
+    require.Equal(t, want, got)
     require.NotNil(t, want)
     require.NotNil(t, got)
 	}
