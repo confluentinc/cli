2bbfd54c6698c81621cc70a066bd3570cb6c7960
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-04-24 14:15:10 -0700

    New release notes for v1.1.0

---
+++ internal/pkg/update/update_msg.go
@@ -0,0 +1,28 @@
+
+package update
+
+var (
+	ReleaseNotesMsg = `
+Updates are available for %s from (current: %s, latest: %s).
+
+=============================================================
+CCLOUD CLI v1.1.0 Release Notes
+=============================================================
+
+New Features
+----------------------------
+- FEAT CCLOUD
+- FEAT BOTH
+
+
+Bug Fixes
+----------------------------
+- BUG CCLOUD
+- BUG BOTH
+
+To install them, please run:
+$ %s update
+
+
+`
+)
