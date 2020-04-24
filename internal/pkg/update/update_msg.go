29cfde6df5cf1f829df93a1e97a817cd32eb11c8
DABH <DABH@users.noreply.github.com>
2020-04-24 16:17:28 -0700

    New release notes for v1.1.0

---
+++ internal/pkg/update/update_msg.go
@@ -0,0 +1,31 @@
+
+package update
+
+var (
+	ReleaseNotesMsg = `
+Updates are available for %s from (current: %s, latest: %s).
+
+=============================================================
+CCLOUD CLI v1.1.0 Release Notes
+=============================================================
+
+New Features
+----------------------------
+- pro
+- xyz
+- abc
+- 123
+
+
+Bug Fixes
+----------------------------
+- asdf
+- bug1
+- bug2
+
+To install them, please run:
+$ %s update
+
+
+`
+)
