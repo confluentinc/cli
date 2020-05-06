378ee9c60ec6076070df938dc88a1f23dece47dd
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-05-06 14:12:18 -0700

    fixed typo

--- internal/pkg/errors/msg.go
+++ internal/pkg/errors/msg.go
@@ -5,5 +5,5 @@ var (
 	ConfigUnspecifiedPlatformError = "Context \"%s\" has a corrupted platform. To fix, please remove the config file, and run `login` or `init`."
 	ConfigUnspecifiedCredentialError = "Context \"%s\" has corrupted credentials. To fix, please remove the config file, and run `login` or `init`."
 
-	APIKeyCommandResourceTypeNotImplementedErrorMsg = "Command not yet available for non Kafka cluster resource."
+	APIKeyCommandResourceTypeNotImplementedErrorMsg = "Command not yet available for non -Kafka cluster resources."
 )
