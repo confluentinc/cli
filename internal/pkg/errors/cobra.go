39f7bce6c9bf71f75b336f37093e80cd76ab5ae2
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-05-27 14:13:52 -0700

    clean up old error handling

--- internal/pkg/errors/cobra.go
+++ internal/pkg/errors/cobra.go
@@ -2,13 +2,8 @@ package errors
 
 import (
 	"fmt"
-	"github.com/hashicorp/go-multierror"
 	"github.com/spf13/cobra"
 	"io"
-
-	corev1 "github.com/confluentinc/cc-structs/kafka/core/v1"
-	"github.com/confluentinc/ccloud-sdk-go"
-	"github.com/confluentinc/mds-sdk-go"
 )
 
 var (
@@ -21,21 +16,6 @@ func HandleCommon(err error, cmd *cobra.Command) error {
 	}
 	var cliError CLIDefinedError
 	switch e := err.(type) {
-
-	case mds.GenericOpenAPIError:
-		cmd.SilenceUsage = true
-		return fmt.Errorf(e.Error() + ": " + string(e.Body()))
-	case *corev1.Error:
-		var result error
-		result = multierror.Append(result, e)
-		for name, msg := range e.GetNestedErrors() {
-			result = multierror.Append(result, fmt.Errorf("%s: %s", name, msg))
-		}
-		cmd.SilenceUsage = true
-		return result
-	case *ccloud.InvalidTokenError:
-		cmd.SilenceUsage = true
-		return fmt.Errorf(CorruptedAuthTokenErrorMsg)
 	case CLIDefinedError:
 		cliError = e
 	default:
