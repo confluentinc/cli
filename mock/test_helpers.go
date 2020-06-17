697e9f2b97e5d17fb2e40419dcf353fe46580078
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-06-17 14:48:20 -0700

    fix mock helper

--- mock/test_helpers.go
+++ mock/test_helpers.go
@@ -1,7 +1,6 @@
 package mock
 
 import (
-	v3 "github.com/confluentinc/cli/internal/pkg/config/v3"
 	"github.com/spf13/cobra"
 
 	"github.com/confluentinc/cli/internal/pkg/analytics"
@@ -9,7 +8,6 @@ import (
 
 func NewDummyAnalyticsMock() *AnalyticsClient {
 	return &AnalyticsClient{
-		SetCLIConfigFunc:         func(cfg *v3.Config) {},
 		SetStartTimeFunc:         func() {},
 		TrackCommandFunc:         func(cmd *cobra.Command, args []string) {},
 		SendCommandAnalyticsFunc: func(cmd *cobra.Command, args []string, cmdExecutionError error) error { return nil },
