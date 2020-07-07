74dd63552bd8c21b81c01f742c6bbe6283453d15
Brian Strauch <bstrauch@confluent.io>
2020-07-07 15:26:47 -0600

    change to rst format

--- internal/pkg/examples/examples.go
+++ internal/pkg/examples/examples.go
@@ -13,8 +13,9 @@ type Example struct {
 func BuildExampleString(examples ...Example) string {
 	str := strings.Builder{}
 	for _, e := range examples {
-		str.WriteString(e.Desc + "\n")
-		str.WriteString(fmt.Sprintf("```%s```\n", e.Code))
+		str.WriteString(e.Desc + "\n\n")
+		str.WriteString("::\n\n")
+		str.WriteString(fmt.Sprintf("%s\n\n", e.Code))
 	}
 	return str.String()
 }
