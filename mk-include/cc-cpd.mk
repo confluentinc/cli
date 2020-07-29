5fd9510e83a83f8f3f2fd08e69898cfb9f1097fb
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-07-29 11:15:17 -0700

    separating make targets

--- mk-include/cc-cpd.mk
+++ mk-include/cc-cpd.mk
@@ -154,6 +154,22 @@ _run-cc-system-tests:
 	#source $(CC_SYSTEM_TEST_ENV_SECRETS) && TEST_REPORT_FILE="$(BUILD_DIR)/ci-gating/TEST-report.xml" TESTS_TO_RUN='$(TESTS_TO_RUN)' $(MAKE) -C $(CC_SYSTEM_TEST_CHECKOUT_DIR) test
 	source $(CC_SYSTEM_TEST_ENV_SECRETS) && TEST_REPORT_FILE="$(BUILD_DIR)/ci-gating/TEST-report.xml" $(MAKE) -C $(CC_SYSTEM_TEST_CHECKOUT_DIR) test
 
+.PHONY: cpd-debug-and-err
+cpd-debug-and-err:
+	$(CPD_PATH) debug --id `kubectl config current-context`; exit 1
+
+.PHONY: cpd-priv-testenv
+cpd-priv-testenv:
+	@echo "## Exporting CPD environment bash profile."
+	set -o pipefail && $(CPD_PATH) priv testenv --id `kubectl config current-context` > $(CC_SYSTEM_TEST_ENV_SECRETS)
+	
+.PHONY: system-test-init-env
+system-test-init-env:
+	source $(CC_SYSTEM_TEST_ENV_SECRETS) && $(MAKE) -C $(CC_SYSTEM_TEST_CHECKOUT_DIR) init-env
+
+.PHONY: run-system-tests
+run-system-tests:
+	source $(CC_SYSTEM_TEST_ENV_SECRETS) && TEST_REPORT_FILE="$(BUILD_DIR)/ci-gating/TEST-report.xml" $(MAKE) -C $(CC_SYSTEM_TEST_CHECKOUT_DIR) test
 
 .PHONY: clean-cc-system-tests
 ## Clean up .cc-system-tests folder
