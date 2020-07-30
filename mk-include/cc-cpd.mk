1507a9978dbc0d8356664e0627cace8d7c0a02d1
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-07-30 11:43:52 -0700

    fix accidentally change when trying to inject more creds and remove echo test

--- mk-include/cc-cpd.mk
+++ mk-include/cc-cpd.mk
@@ -49,7 +49,7 @@ ifeq ($(CI),true)
 	echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 	curl https://www.mongodb.org/static/pgp/server-3.4.asc | sudo apt-key add -
 	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-	sudo apt-gext update
+	sudo apt-get update
 	sudo apt-get install -y google-cloud-sdk kubectl
 	gcloud config set project cloud-private-dev
 	gcloud config set account semaphore@cloud-private-dev.iam.gserviceaccount.com
