f4be863c4155808fba78e9f1745e5657c1f90bb9
Agam Brahma <abrahma@confluent.io>
2020-08-10 10:11:49 -0700

    Review comments round #1.

--- internal/cmd/kafka/command_link.go
+++ internal/cmd/kafka/command_link.go
@@ -5,6 +5,7 @@ import (
 	linkv1 "github.com/confluentinc/cc-structs/kafka/clusterlink/v1"
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/errors"
+	"github.com/confluentinc/cli/internal/pkg/examples"
 	"github.com/confluentinc/cli/internal/pkg/output"
 	"github.com/spf13/cobra"
 )
@@ -36,7 +37,7 @@ func NewLinkCommand(prerunner pcmd.PreRunner) *cobra.Command {
 		&cobra.Command{
 			Use:    "link",
 			Hidden: true,
-			Short:  "Manage inter-cluster link.",
+			Short:  "Manages inter-cluster links.",
 		},
 		prerunner)
 	cmd := &linkCommand{
@@ -52,15 +53,16 @@ func (c *linkCommand) init() {
 
 	listCmd := &cobra.Command{
 		Use: "list",
-		Short: "List previously created cluster-link.",
-		Example: `
-List every link.
-
-::
-
-        ccloud kafka link list`,
+		Short: "List previously created cluster links.",
+		Example: examples.BuildExampleString(
+			examples.Example{
+				Text: "List every link",
+				Code: "ccloud kafka link list",
+			},
+		),
 		RunE: c.list,
-		Args: cobra.NoArgs}
+		Args: cobra.NoArgs
+	}
 	listCmd.Flags().StringP(output.FlagName, output.ShortHandFlag, output.DefaultValue, output.Usage)
 	listCmd.Flags().SortFlags = false
 	c.AddCommand(listCmd)
@@ -68,31 +70,30 @@ List every link.
 	// Note: this is subject to change as we iterate on options for how to specify a source cluster.
 	createCmd := &cobra.Command{
 		Use: "create <link-name>",
-		Short: "Create a new cluster-link.",
-		Example: `
-Create a cluster-link, using supplied source url and properties.
-
-::
-
-        ccloud kafka link create MyLink --source myhost:1234
-        ccloud kafka link create MyLink --source myhost:1234 --config "key1=val1,key2=val2"`,
+		Short: "Create a new cluster link.",
+		Example: examples.BuildExampleString(
+			examples.Example{
+				Text: "Create a cluster link, using supplied source url and properties.",
+				Code: "ccloud kafka link create my_link --source myhost:1234\nccloud kafka link create my_link --source myhost:1234 --config \"key1=val1,key2=val2\"",
+			},
+		),
 		RunE: c.create,
 		Args: cobra.ExactArgs(1),
 	}
-	createCmd.Flags().String(sourceBootstrapServersFlagName, "", "Bootstrap-servers address for source cluster.")
+	createCmd.Flags().String(sourceBootstrapServersFlagName, "", "Bootstrap-server address for source cluster.")
 	createCmd.Flags().String(sourceConfigFlagName, "", "Additional comma-separated properties for source cluster.")
 	createCmd.Flags().SortFlags = false
 	c.AddCommand(createCmd)
 
 	deleteCmd := &cobra.Command{
 		Use: "delete <link-name>",
-		Short: "Delete a previously created cluster-link.",
-		Example: `
-Deletes a cluster-link.
-
-::
-
-        ccloud kafka link delete Mylink`,
+		Short: "Delete a previously created cluster link.",
+		Example: examples.BuildExampleString(
+			examples.Example{
+				Text: "Deletes a cluster link.",
+				Code: "ccloud kafka link delete my_link",
+			},
+		),
 		RunE: c.delete,
 		Args: cobra.ExactArgs(1),
 	}
@@ -100,13 +101,13 @@ Deletes a cluster-link.
 
 	describeCmd := &cobra.Command{
 		Use: "describe <link-name>",
-		Short: "Describes a previously created cluster-link.",
-		Example: `
-Describes a cluster-link.
-
-::
-
-        ccloud kafka link describe MyLink`,
+		Short: "Describes a previously created cluster link.",
+		Example: examples.BuildExampleString(
+			examples.Example{
+				Text: "Describes a cluster link.",
+				Code: "ccloud kafka link describe my_link",
+			},
+		),
         RunE: c.describe,
         Args: cobra.ExactArgs(1),
 	}
@@ -115,22 +116,22 @@ Describes a cluster-link.
 	c.AddCommand(describeCmd)
 
 	// Note: this can change as we decide how to present this modification interface (allowing multiple properties, allowing override and delete, etc).
-	alterCmd := &cobra.Command{
-		Use: "alter <link-name>",
-		Short: "Alters a property for a previously created cluster-link.",
-		Example: `
-Alters a property for a cluster-link.
-
-::
-
-        ccloud kafka link alter MyLink --key retention.ms --value 123456890`,
-		RunE: c.alter,
+	updateCmd := &cobra.Command{
+		Use: "update <link-name>",
+		Short: "Updates a property for a previously created cluster link.",
+		Example: examples.BuildExampleString(
+			examples.Example{
+				Text: "Updates a property for a cluster link.",
+				Code: "ccloud kafka link update my_link --key retention.ms --value 123456890",
+			},
+		),
+		RunE: c.update,
 		Args: cobra.ExactArgs(1),
 	}
-	alterCmd.Flags().String(propertyKeyFlagName, "", "Property key.")
-	alterCmd.Flags().String(propertyValueFlagName, "", "Property value.")
-	alterCmd.Flags().SortFlags = false
-	c.AddCommand(alterCmd)
+	updateCmd.Flags().String(propertyKeyFlagName, "", "Property key.")
+	updateCmd.Flags().String(propertyValueFlagName, "", "Property value.")
+	updateCmd.Flags().SortFlags = false
+	c.AddCommand(updateCmd)
 }
 
 func (c *linkCommand) list(cmd *cobra.Command, args []string) error {
@@ -232,7 +233,7 @@ func (c *linkCommand) describe(cmd *cobra.Command, args []string) error {
 	return outputWriter.Out()
 }
 
-func (c *linkCommand) alter(cmd *cobra.Command, args []string) error {
+func (c *linkCommand) update(cmd *cobra.Command, args []string) error {
 	cluster, err := pcmd.KafkaCluster(cmd, c.Context)
 	if err != nil {
 		return errors.HandleCommon(err, cmd)
