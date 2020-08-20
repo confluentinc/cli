247e5ca1e09a5b190197c485a4e273e92b818148
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-08-19 17:35:04 -0700

    docker login issue attempt

--- mk-include/cc-docker.mk
+++ mk-include/cc-docker.mk
@@ -3,6 +3,7 @@
 docker-login:
 ifeq ($(DOCKER_USER)$(DOCKER_APIKEY),$(_empty))
 	echo "EMPTY"
+	echo $(DOCKER_USER)
 	@jq -e '.auths."confluent-docker.jfrog.io"' $(HOME)/.docker/config.json 2>&1 >/dev/null ||\
 		(echo "confluent-docker.jfrog.io not logged in, Username and Password not found in environment, prompting for login:" && \
 		 docker login confluent-docker.jfrog.io)
