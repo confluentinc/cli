3facb2dd9c07cf430287a3d74b38e65d754d2899
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-08-19 17:00:57 -0700

    docker login testing

--- mk-include/cc-docker.mk
+++ mk-include/cc-docker.mk
@@ -2,10 +2,12 @@
 ## Login to docker Artifactory
 docker-login:
 ifeq ($(DOCKER_USER)$(DOCKER_APIKEY),$(_empty))
+	echo "EMPTY"
 	@jq -e '.auths."confluent-docker.jfrog.io"' $(HOME)/.docker/config.json 2>&1 >/dev/null ||\
 		(echo "confluent-docker.jfrog.io not logged in, Username and Password not found in environment, prompting for login:" && \
 		 docker login confluent-docker.jfrog.io)
 else
+	echo "NAH EMPTY"
 	@jq -e '.auths."confluent-docker.jfrog.io"' $(HOME)/.docker/config.json 2>&1 >/dev/null ||\
 		docker login confluent-docker.jfrog.io --username $(DOCKER_USER) --password $(DOCKER_APIKEY)
 endif
