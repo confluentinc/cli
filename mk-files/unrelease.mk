2c5295950871e0e82c21ba0f044dbb76447156f1
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-09-30 16:16:30 -0700

    update unrelease process

--- mk-files/unrelease.mk
+++ mk-files/unrelease.mk
@@ -1,8 +1,18 @@
-UNRELEASE_TARGET ?= $(S3_BUCKET_PATH)
+.PHONY: unrelease-prod
+unrelease-prod:
+	make reset-tag-and-commit
+	make restore-latest-archives
+	make delete-archives-and-binaries
+	make delete-release-notes
 
-.PHONY: unrelease
-unrelease: unrelease-warn
-	make unrelease-s3
+.PHONY: unrelease-stag
+unrelease-stag:
+	make reset-tag-and-commit
+	make clean-staging-folder
+	make delete-release-notes
+
+.PHONY: reset-tag-and-commit
+reset-tag-and-commit: unrelease-warn
 	git checkout master
 	git pull
 	git diff-index --quiet HEAD # ensures git status is clean
@@ -10,7 +20,6 @@ unrelease: unrelease-warn
 	git push --delete origin v$(CLEAN_VERSION) # delete remote tag
 	git reset --hard HEAD~1 # warning: assumes "chore" version bump was last commit
 	git push origin HEAD --force
-	make restore-latest-archives
 
 .PHONY: unrelease-warn
 unrelease-warn:
@@ -21,38 +30,22 @@ unrelease-warn:
 	@echo -n "Warning: Ensure a git version bump (new commit and new tag) has occurred before continuing, else you will remove the prior version. Continue? (y/n): "
 	@read line; if [ $$line = "n" ] || [ $$line = "N" ]; then echo aborting; exit 1; fi
 
-.PHONY: unrelease-s3
-unrelease-s3:
-	@echo "If you are going to reattempt the release again without the need to edit the release notes, there is no need to delete the release notes from S3."
+.PHONY: delete-archives-and-binaries
+delete-archives-and-binaries:
+	$(caasenv-authenticate); \
+	$(call delete-release-folder,binaries); \
+	$(call delete-release-folder,archives)
+
+.PHONY: delete-release-notes
+delete-release-notes:
 	@echo -n "Do you want to delete the release notes from S3? (y/n): "
-	@read line; if [ $$line = "y" ] || [ $$line = "Y" ]; then make delete-binaries-archives-and-release-notes; else make delete-binaries-and-archives; fi
-
-.PHONY: delete-binaries-and-archives
-delete-binaries-and-archives:
+	@read line; if [ $$line = "n" ] || [ $$line = "N" ]; then echo aborting; exit 1; fi
 	$(caasenv-authenticate); \
-	$(delete-binaries); \
-	$(delete-archives)
+	$(call delete-release-folder,release-notes)
 
-.PHONY: delete-binaries-archives-and-release-notes
-delete-binaries-archives-and-release-notes:
-	$(caasenv-authenticate); \
-	$(delete-binaries); \
-	$(delete-archives); \
-	$(delete-release-notes)
-
-define delete-binaries
-	aws s3 rm $(UNRELEASE_TARGET)/ccloud-cli/binaries/$(CLEAN_VERSION) --recursive; \
-	aws s3 rm $(UNRELEASE_TARGET)/confluent-cli/binaries/$(CLEAN_VERSION) --recursive
-endef
-
-define delete-archives
-	aws s3 rm $(UNRELEASE_TARGET)/ccloud-cli/archives/$(CLEAN_VERSION) --recursive; \
-	aws s3 rm $(UNRELEASE_TARGET)/confluent-cli/archives/$(CLEAN_VERSION) --recursive
-endef
-
-define delete-release-notes
-	aws s3 rm $(UNRELEASE_TARGET)/ccloud-cli/release-notes/$(CLEAN_VERSION) --recursive; \
-	aws s3 rm $(UNRELEASE_TARGET)/confluent-cli/release-notes/$(CLEAN_VERSION) --recursive
+define delete-release-folder
+	aws s3 rm $(S3_BUCKET_PATH)/ccloud-cli/$1/$(CLEAN_VERSION) --recursive; \
+	aws s3 rm $(S3_BUCKET_PATH)/confluent-cli/$1/$(CLEAN_VERSION) --recursive
 endef
 
 .PHONY: restore-latest-archives
@@ -65,3 +58,8 @@ restore-latest-archives: restore-latest-archives-warn
 restore-latest-archives-warn:
 	@echo -n "Warning: Overriding archives in the latest folder with archives from version v$(CLEAN_VERSION). Continue? (y/n): "
 	@read line; if [ $$line = "n" ] || [ $$line = "N" ]; then echo aborting; exit 1; fi
+
+.PHONY: clean-staging-folder
+clean-staging-folder:
+	$(caasenv-authenticate); \
+	aws s3 rm $(S3_STAG_PATH) --recursive
