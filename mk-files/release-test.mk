9ec621fa9585d4c43156f64382bbf3e29312f2c9
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-10-01 16:30:30 -0700

    clean up release-test.mk

--- mk-files/release-test.mk
+++ mk-files/release-test.mk
@@ -1,16 +1,17 @@
-VERIFY_RELEASE_TARGET ?= $(S3_BUCKET_PATH)
-VERIFY_ARCHIVES_FOLDER_OVERRIDE ?=
+VERIFY_BIN_FOLDER ?= $(S3_BUCKET_PATH)
+OVERRIDE_S3_FOLDER ?=
+ARCHIVES_VERSION ?=
 
+# if ARCHIVES_VERSION is empty, archives latest folder will be tested
 .PHONY: test-installers
 test-installers:
 	@echo Running packaging/installer tests
-	@ # if ${ARCHIVES_VERSION_TO_TEST} is empty, archives latest folder will be tested
-	@bash test-installers.sh ${ARCHIVES_VERSION_TO_TEST}
+	@bash test-installers.sh $(ARCHIVES_VERSION)
 
 .PHONY: verify-stag
 verify-stag:
-	VERIFY_ARCHIVES_FOLDER_TARGET=$(S3_STAG_FOLDER_NAME) make verify-archive-installers
-	VERIFY_RELEASE_TARGET=$(S3_STAG_PATH) make verify-binary-files
+	OVERRIDE_S3_FOLDER=$(S3_STAG_FOLDER_NAME) make verify-archive-installers
+	VERIFY_BIN_FOLDER=$(S3_STAG_PATH) make verify-binary-files
 
 .PHONY: verify-prod
 verify-prod:
@@ -19,15 +20,14 @@ verify-prod:
 
 .PHONY: verify-archive-installers
 verify-archive-installers:
-	make test-installers OVERRIDE_S3_FOLDER=$(VERIFY_ARCHIVES_FOLDER_TARGET)
-	make test-installers ARCHIVES_VERSION_TO_TEST=v$(CLEAN_VERSION) OVERRIDE_S3_FOLDER=$(VERIFY_ARCHIVES_FOLDER_TARGET)
+	OVERRIDE_S3_FOLDER=$(OVERRIDE_S3_FOLDER) make test-installers 
+	ARCHIVES_VERSION=v$(CLEAN_VERSION) OVERRIDE_S3_FOLDER=$(OVERRIDE_S3_FOLDER) make test-installers 
 	@echo "*** ARCHIVES VERIFICATION PASSED!!! ***"
 
 # check that the expected binaries are present and have --acl public-read
 .PHONY: verify-binary-files
 verify-binary-files:
 	$(eval TEMP_DIR=$(shell mktemp -d))
-	echo $(TEMP_DIR)
 	@$(caasenv-authenticate) && \
 	for binary in ccloud confluent; do \
 		for os in linux darwin windows; do \
@@ -39,7 +39,7 @@ verify-binary-files:
 				if [ "$${os}" = "windows" ] ; then \
 					suffix=".exe"; \
 				fi ; \
-				FILE=$(VERIFY_RELEASE_TARGET)/$${binary}-cli/binaries/$(CLEAN_VERSION)/$${binary}_$(CLEAN_VERSION)_$${os}_$${arch}$${suffix}; \
+				FILE=$(VERIFY_BIN_FOLDER)/$${binary}-cli/binaries/$(CLEAN_VERSION)/$${binary}_$(CLEAN_VERSION)_$${os}_$${arch}$${suffix}; \
 				echo "Checking binary: $${FILE}"; \
 				aws s3 cp $$FILE $(TEMP_DIR) || { rm -rf $(TEMP_DIR) && exit 1; }; \
 			done; \
@@ -47,3 +47,4 @@ verify-binary-files:
 	done
 	rm -rf $(TEMP_DIR)	
 	@echo "*** BINARIES VERIFICATION PASSED!!! ***"
+
