84cfadaaf24466d597fa957ef144ba9d61436645
Alex Cheng <acheng@confluent.io>
2020-10-15 12:10:58 -0500

    CIAM-397 --current-env arg instead of --environment current

--- internal/cmd/iam/command_rolebinding_test.go
+++ internal/cmd/iam/command_rolebinding_test.go
@@ -136,6 +136,11 @@ var roleBindingListTests = []roleBindingTest{
 		roleName: "EnvironmentAdmin",
 		scope:    mdsv2alpha1.Scope{Path: []string{"organization=" + v3.MockOrgResourceId, "environment=env-123"}},
 	},
+	{
+		args:     []string{"--role", "EnvironmentAdmin", "--current-env"},
+		roleName: "EnvironmentAdmin",
+		scope:    mdsv2alpha1.Scope{Path: []string{"organization=" + v3.MockOrgResourceId, "environment=" + v3.MockEnvironmentId}},
+	},
 }
 
 func (suite *RoleBindingTestSuite) TestRoleBindingsList() {
@@ -187,7 +192,13 @@ var roleBindingCreateDeleteTests = []roleBindingTest{
 		err:  notfoundError,
 	},
 	{
-		args:      []string{"--principal", "User:" + v3.MockUserResourceId, "--role", "EnvironmentAdmin", "--environment", "current"},
+		args:      []string{"--principal", "User:" + v3.MockUserResourceId, "--role", "EnvironmentAdmin", "--environment", "env-123"},
+		principal: "User:" + v3.MockUserResourceId,
+		roleName:  "EnvironmentAdmin",
+		scope:     mdsv2alpha1.Scope{Path: []string{"organization=" + v3.MockOrgResourceId, "environment=env-123"}},
+	},
+	{
+		args:      []string{"--principal", "User:" + v3.MockUserResourceId, "--role", "EnvironmentAdmin", "--current-env"},
 		principal: "User:" + v3.MockUserResourceId,
 		roleName:  "EnvironmentAdmin",
 		scope:     mdsv2alpha1.Scope{Path: []string{"organization=" + v3.MockOrgResourceId, "environment=" + v3.MockEnvironmentId}},
