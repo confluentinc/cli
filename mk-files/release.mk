c89fc1332bd42e51ffa15e6ee718e7a42f80ba7a
DABH <DABH@users.noreply.github.com>
2020-10-18 22:03:31 -0700

    WIP

--- mk-files/release.mk
+++ mk-files/release.mk
@@ -37,6 +37,12 @@ define copy-stag-content-to-prod
 	done
 endef
 
+.PHONY: gorelease-alpine
+gorelease:
+	GO111MODULE=off go get -u github.com/inconshreveable/mousetrap && \
+	GO111MODULE=on GOPRIVATE=github.com/confluentinc GONOSUMDB=github.com/confluentinc,github.com/golangci/go-misc VERSION=$(VERSION) HOSTNAME="$(HOSTNAME)" S3FOLDER=$(S3_STAG_FOLDER_NAME)/ccloud-cli goreleaser release --rm-dist -f .goreleaser-ccloud-alpine.yml && \
+	GO111MODULE=on GOPRIVATE=github.com/confluentinc GONOSUMDB=github.com/confluentinc,github.com/golangci/go-misc VERSION=$(VERSION) HOSTNAME="$(HOSTNAME)" S3FOLDER=$(S3_STAG_FOLDER_NAME)/confluent-cli goreleaser release --rm-dist -f .goreleaser-confluent-alpine.yml
+
 .PHONY: gorelease
 gorelease:
 	$(eval token := $(shell (grep github.com ~/.netrc -A 2 | grep password || grep github.com ~/.netrc -A 2 | grep login) | head -1 | awk -F' ' '{ print $$2 }'))
