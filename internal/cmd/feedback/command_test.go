7d3313beb2e36a01b557e480f2234e8b8885e3ce
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2020-10-29 11:12:31 -0700

    fix cmd package import name

--- internal/cmd/feedback/command_test.go
+++ internal/cmd/feedback/command_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 
-	"github.com/confluentinc/cli/internal/pkg/cmd"
+	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/config"
 	v3 "github.com/confluentinc/cli/internal/pkg/config/v3"
 	"github.com/confluentinc/cli/internal/pkg/errors"
@@ -15,22 +15,22 @@ import (
 )
 
 func TestFeedback(t *testing.T) {
-	command := cmd.BuildRootCommand()
+	command := pcmd.BuildRootCommand()
 	command.AddCommand(mockFeedbackCommand("This feedback tool is great!"))
 
 	req := require.New(t)
-	out, err := cmd.ExecuteCommand(command, "feedback")
+	out, err := pcmd.ExecuteCommand(command, "feedback")
 	req.NoError(err)
 	req.Contains(out, "Enter feedback: ")
 	req.Contains(out, errors.ThanksForFeedbackMsg)
 }
 
 func TestFeedbackEmptyMessage(t *testing.T) {
-	command := cmd.BuildRootCommand()
+	command := pcmd.BuildRootCommand()
 	command.AddCommand(mockFeedbackCommand(""))
 
 	req := require.New(t)
-	out, err := cmd.ExecuteCommand(command, "feedback")
+	out, err := pcmd.ExecuteCommand(command, "feedback")
 	req.NoError(err)
 	req.Contains(out, "Enter feedback: ")
 }
