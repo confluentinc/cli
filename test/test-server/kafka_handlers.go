470210900e6273bd473433d3adf9452bef545bea
Miles Todzo <mtodzo@confluent.io>
2020-12-26 20:07:58 -0800

    topic create delete describe and list tests

--- test/test-server/kafka_handlers.go
+++ test/test-server/kafka_handlers.go
@@ -2,6 +2,7 @@ package test_server
 
 import (
 	"encoding/json"
+	utilv1 "github.com/confluentinc/cc-structs/kafka/util/v1"
 	"io"
 	"net/http"
 	"testing"
@@ -122,3 +123,138 @@ func (k *KafkaRouter) HandleKafkaTopicMirrorStop(t *testing.T) func(w http.Respo
 		}
 	}
 }
+// Handler for: "/2.0/kafka/{cluster}/topics"
+func (k *KafkaRouter) HandleKafkaListCreateTopic(t *testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET": //list call
+			k.HandleKafkaListTopic(t)(w, r)
+		case "PUT": //create call
+			k.HandleKafkaCreateTopic(t)(w, r)
+		}
+	}
+}
+// Handler for: GET "/2.0/kafka/{cluster}/topics"
+func (k *KafkaRouter) HandleKafkaListTopic(t *testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		cluster := vars["cluster"]
+		var listTopicReply schedv1.ListTopicReply
+		switch cluster {
+		case "lkc-topics":
+			listTopicReply = schedv1.ListTopicReply{
+				Topics: []*schedv1.TopicDescription{
+					{
+						Name: "topic1",
+					},
+					{
+						Name: "topic2",
+					},
+				},
+			}
+		case "lkc-no-topics":
+			listTopicReply = schedv1.ListTopicReply{Topics: []*schedv1.TopicDescription{}}
+		}
+		topicReply, err := json.Marshal(listTopicReply.Topics)
+		require.NoError(t, err)
+		_, err = io.WriteString(w, string(topicReply))
+		require.NoError(t, err)
+	}
+}
+// Handler for: POST "/2.0/kafka/{cluster}/topics"
+func (k *KafkaRouter) HandleKafkaCreateTopic(t *testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		cluster := vars["cluster"]
+		spec := &schedv1.TopicSpecification{}//CreateTopicRequest{}
+		err := utilv1.UnmarshalJSON(r.Body, spec)
+		require.NoError(t, err)
+		topicName := spec.Name
+		var createTopicReply *schedv1.CreateTopicReply
+		switch {
+		case cluster == "lkc-create-topic" && topicName == "dupTopic":
+			w.WriteHeader(http.StatusBadRequest)
+			createTopicReply = &schedv1.CreateTopicReply{Error: &schedv1.KafkaAPIError{Message: "topic already exists"}}
+		default:
+			createTopicReply = &schedv1.CreateTopicReply{}
+		}
+		topicReply, err := json.Marshal(createTopicReply)
+		require.NoError(t, err)
+		_, err = io.WriteString(w, string(topicReply))
+		require.NoError(t, err)
+	}
+}
+// Handler for: "/2.0/kafka/{cluster}/topics/{topic}"
+func (k *KafkaRouter) HandleKafkaDescribeDeleteTopic(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET": //describe call
+			k.HandleKafkaDescribeTopic(t)(w, r)
+		case "DELETE": //delete call
+			k.HandleKafkaDeleteTopic(t)(w, r)
+		}
+	}
+}
+// Handler for: GET "/2.0/kafka/{cluster}/topics/{topic}"
+func (k *KafkaRouter) HandleKafkaDescribeTopic(t *testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		cluster := vars["cluster"]
+		topic := vars["topic"]
+		var describeTopicReply *schedv1.DescribeTopicReply
+		switch {
+		case cluster == "lkc-describe-topic" && topic == "topic1":
+			describeTopicReply = &schedv1.DescribeTopicReply{Topic: &schedv1.TopicDescription{
+				Name: "topic1",
+				Partitions: []*schedv1.TopicPartitionInfo{{Partition: 1, Leader: &schedv1.KafkaNode{Id: 1}, Replicas: []*schedv1.KafkaNode{{Id: 1}}}},
+			}}
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			describeTopicReply = &schedv1.DescribeTopicReply{Error: &schedv1.KafkaAPIError{Message: "topic not found"}}
+		}
+		topicReply, err := json.Marshal(describeTopicReply.Topic)
+		require.NoError(t, err)
+		_, err = io.WriteString(w, string(topicReply))
+		require.NoError(t, err)
+	}
+}
+// Handler for: DELETE "/2.0/kafka/{cluster}/topics/{topic}"
+func (k *KafkaRouter) HandleKafkaDeleteTopic(t *testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		cluster := vars["cluster"]
+		topic := vars["topic"]
+		var deleteTopicReply *schedv1.DeleteTopicReply
+		switch {
+		case cluster == "lkc-delete-topic" && topic == "topic1":
+			deleteTopicReply = &schedv1.DeleteTopicReply{}
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			deleteTopicReply = &schedv1.DeleteTopicReply{Error: &schedv1.KafkaAPIError{Message: "topic not found"}}
+		}
+		topicReply, err := json.Marshal(deleteTopicReply)
+		require.NoError(t, err)
+		_, err = io.WriteString(w, string(topicReply))
+		require.NoError(t, err)
+	}
+}
+//Handler for: "/2.0/kafka/{cluster}/topics/{topic}/config"
+func (k *KafkaRouter) HandleKafkaTopicListConfig(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var listTopicConfigReply schedv1.ListTopicConfigReply
+		if r.Method == "GET" { //part of describe call
+			listTopicConfigReply = schedv1.ListTopicConfigReply{TopicConfig: &schedv1.TopicConfig{Entries: []*schedv1.TopicConfigEntry{}}}
+		}
+		// POSTs are part of update calls
+		vars := mux.Vars(r)
+		topic := vars["topic"]
+		switch topic {
+
+		}
+		topicReply, err := json.Marshal(listTopicConfigReply)
+		require.NoError(t, err)
+		_, err = io.WriteString(w, string(topicReply))
+		require.NoError(t, err)
+	}
+}
+
