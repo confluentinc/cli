ecb38d0c9d29a515bb9d91ad20752474ea6241d9
Miles Todzo <mtodzo@confluent.io>
2020-12-27 20:41:08 -0800

    add failed regex test case

--- test/payment_test.go
+++ test/payment_test.go
@@ -27,6 +27,11 @@ func (s *CLITestSuite) TestPaymentUpdate() {
 			preCmdFuncs: []bincover.PreCmdFunc{stdinPipeFunc(strings.NewReader("4242424242424242\n12/70\n999\nBrian Strauch\n"))},
 			fixture:     "admin/payment-update-success.golden",
 		},
+		{
+			args:        "admin payment update", //testing with CVC failing regex check on first attempt
+			preCmdFuncs: []bincover.PreCmdFunc{stdinPipeFunc(strings.NewReader("4242424242424242\n12/70\n99\n999\nBrian Strauch\n"))},
+			fixture:     "admin/payment-update-bad-cvc.golden",
+		},
 	}
 	for _, test := range tests {
 		test.login = "default"
