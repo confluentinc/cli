62e3e337d647765b8ec8d5af786a8cecd3b07a60
Miles Todzo <mtodzo@confluent.io>
2021-01-05 14:49:15 -0800

    fixed how file values are compared

--- test/secret_test.go
+++ test/secret_test.go
@@ -52,13 +52,30 @@ func checkMasterKeySecretsFile(file *os.File, keys []string) func(t *testing.T){
 
 
 var (
-	testPropertyEntry = []string{"testProperty = test"}
+	testPropertyEntries        = []string{"testProperty = test", "testAdd = add"}
+	expectedConfigPropertyKeys = []string{"config.providers", "config.providers.securepass.class"}
+	expectedSecretPropertyKeys = []string{"_metadata.symmetric_key.0.created_at", "_metadata.symmetric_key.0.envvar", "_metadata.symmetric_key.0.length",
+		"_metadata.symmetric_key.0.iterations", "_metadata.symmetric_key.0.salt", "_metadata.symmetric_key.0.enc"}
+
+	expectedConfigPropertyValues = map[string]string {
+		"config.providers" : "securepass",
+		"config.providers.securepass.class" : "io.confluent.kafka.security.config.provider.SecurePassConfigProvider",
+	}
+	expectedSecretPropertyValues = map[string]string {
+		"_metadata.symmetric_key.0.created_at" : "",
+		"_metadata.symmetric_key.0.envvar" : "CONFLUENT_SECURITY_MASTER_KEY",
+		"_metadata.symmetric_key.0.length" : "32",
+		"_metadata.symmetric_key.0.iterations" : "1000",
+		"_metadata.symmetric_key.0.salt" : "",
+		"_metadata.symmetric_key.0.enc" : "",
+		"secrets.properties/testProperty" : `ENC[AES/CBC/PKCS5Padding,data:phUW0EYbYpjIV9fbjH37/w==,iv:Ab2ZBOcofe3tOC/DKfYG2w==,type:str]`,
+	}
 )
 
 func (s *CLITestSuite) TestSecretFile() {
 	config, err := ioutil.TempFile("", "config.*.properties")
 	require.NoError(s.T(), err)
-	err = ioutil.WriteFile(config.Name(), []byte(testPropertyEntry[0]), 0644)
+	err = ioutil.WriteFile(config.Name(), []byte(testPropertyEntries[0]), 0644)
 	require.NoError(s.T(), err)
 
 	secrets, err := ioutil.TempFile("", "secrets.*.properties")
@@ -71,33 +88,30 @@ func (s *CLITestSuite) TestSecretFile() {
 	defer os.Remove(secrets.Name())
 	defer os.Remove(output.Name())
 
-	expectedPropertiesForConfig := map[string]string {
-		"testProperty" : `${securepass:`+secrets.Name()+`:`+filepath.Base(config.Name())+`/testProperty}`,
-		"config.providers" : "securepass",
-		"config.providers.securepass.class" : "io.confluent.kafka.security.config.provider.SecurePassConfigProvider",
-	}
-	expectedPropertiesForSecrets := map[string]string {
-		"_metadata.symmetric_key.0.created_at" : "2021-01-04 15:55:08.792508 -0800 PST m=+0.035750432",
-		"_metadata.symmetric_key.0.envvar" : "CONFLUENT_SECURITY_MASTER_KEY",
-		"_metadata.symmetric_key.0.length" : "32",
-		"_metadata.symmetric_key.0.iterations" : "1000",
-		"_metadata.symmetric_key.0.salt" : "Hlj1KM4mbIy6LoRwKniYtB5e+yQ3CoUucqh70NGYUfw=",
-		"_metadata.symmetric_key.0.enc" : `ENC[AES/CBC/PKCS5Padding,data:YUNiJYhbo0XXKGV3S9oH8OcVMLQH6Rc6uiDvUWO3OWKRJYSJT8dUO3wdsFVqkNpM,iv:/oct96zlQcuy3sgupLE+yQ==,type:str]`,
-		"secrets.properties/testProperty" : `ENC[AES/CBC/PKCS5Padding,data:phUW0EYbYpjIV9fbjH37/w==,iv:Ab2ZBOcofe3tOC/DKfYG2w==,type:str]`,
-	}
+	encryptTestConfigPropertyKeys := append([]string{"testProperty"}, expectedConfigPropertyKeys...)
+	encryptTestConfigPropertyValues := expectedConfigPropertyValues
+	encryptTestConfigPropertyValues["testProperty"] = `${securepass:`+secrets.Name()+`:`+filepath.Base(config.Name())+`/testProperty}`
+
+
 
 	tests := []CLITest{
 		{
 			name:	"secret file encrypt",
 			env:	[]string{fmt.Sprintf("%s=H0TKtEk7kxBHqxyXtQx0WJaiqsOlaQ89yXZLguEa2yI=", secret.ConfluentKeyEnvVar)},
 			args: "secret file encrypt --config-file="+config.Name()+" --local-secrets-file="+ secrets.Name()+" --remote-secrets-file="+ secrets.Name()+" --config testProperty",
-			wantFunc: checkEncryptedPropertyValues(config, expectedPropertiesForConfig, secrets, expectedPropertiesForSecrets),
+			wantFunc: checkEncryptedPropertyValues(config, encryptTestConfigPropertyKeys, encryptTestConfigPropertyValues, secrets, expectedSecretPropertyKeys, expectedSecretPropertyValues),
 		},
 		{
 			name:	"secret file decrypt",
 			env:	[]string{fmt.Sprintf("%s=H0TKtEk7kxBHqxyXtQx0WJaiqsOlaQ89yXZLguEa2yI=", secret.ConfluentKeyEnvVar)},
 			args: "secret file decrypt --config-file="+config.Name()+" --local-secrets-file="+ secrets.Name()+" --output-file="+ output.Name()+" --config testProperty",
-			wantFunc: checkDecryptedPropertyValues(output, testPropertyEntry),
+			wantFunc: checkDecryptedPropertyValues(output, testPropertyEntries[:1]),
+		},
+		{
+			name:	"secret file add",
+			env:	[]string{fmt.Sprintf("%s=H0TKtEk7kxBHqxyXtQx0WJaiqsOlaQ89yXZLguEa2yI=", secret.ConfluentKeyEnvVar)},
+			args: "secret file add --config-file="+config.Name()+" --local-secrets-file="+ secrets.Name()+" --remote-secrets-file="+ secrets.Name()+" --config testAdd",
+			wantFunc: checkDecryptedPropertyValues(output, testPropertyEntries),
 		},
 	}
 	for _, test := range tests {
@@ -122,29 +136,35 @@ func checkDecryptedPropertyValues(output *os.File, entry []string) func(t *testi
 	}
 }
 
-func checkEncryptedPropertyValues(config *os.File, expectedPropertiesForConfig map[string]string, secrets *os.File, expectedPropertiesForSecrets map[string]string) func(t *testing.T) {
+func checkEncryptedPropertyValues(config *os.File, configPropertyKeys []string, expectedConfigPropertyValues map[string]string, secrets *os.File, secretPropertyKeys []string, expectedSecretPropertyValues map[string]string) func(t *testing.T) {
 	return func(t *testing.T) {
 		configFile, err := os.Open(config.Name())
 		require.NoError(t, err)
 		defer configFile.Close()
-		scanner := bufio.NewScanner(configFile)
-		for scanner.Scan() {
-			line := scanner.Text()
-			if equal := strings.Index(line, "="); equal >= 0 {
-				require.NotEmpty(t, expectedPropertiesForConfig[line[0:equal-1]])
-				require.Equal(t, expectedPropertiesForConfig[line[0:equal-1]], line[equal+2:])
-			}
+		configScanner := bufio.NewScanner(configFile)
+
+
+		for _, entry := range configPropertyKeys {
+			require.True(t, configScanner.Scan())
+			line := configScanner.Text()
+			equal := strings.Index(line, "=")
+			require.Equal(t, entry, line[0:equal-1])
+			require.Equal(t, expectedConfigPropertyValues[entry], line[equal+2:])
 		}
 
 		secretsFile, err := os.Open(secrets.Name())
 		require.NoError(t, err)
 		defer secretsFile.Close()
 		secretsScanner := bufio.NewScanner(secretsFile)
-		for secretsScanner.Scan() {
-			line := scanner.Text()
-			if equal := strings.Index(line, "="); equal >= 0 {
-				require.NotEmpty(t, expectedPropertiesForSecrets[line[0:equal-1]])
-				require.Equal(t, expectedPropertiesForSecrets[line[0:equal-1]], line[equal+2:])
+
+		for _, entry := range secretPropertyKeys {
+			require.True(t, secretsScanner.Scan())
+			line := secretsScanner.Text()
+			equal := strings.Index(line, "=")
+			require.Equal(t, entry, line[0:equal-1])
+			require.Equal(t, entry, line[0:equal-1])
+			if expectedSecretPropertyValues[entry] != "" {	// empty strings indicate entry values that are non-deterministic
+				require.Equal(t, expectedSecretPropertyValues[entry], line[equal+2:])
 			}
 		}
 	}
