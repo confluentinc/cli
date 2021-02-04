c62296ef869dbb8bf68f5358e9de0a76d1e3d4d2
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2021-02-03 17:12:13 -0800

    remove moved test

--- internal/cmd/shell/command_test.go
+++ internal/cmd/shell/command_test.go
@@ -1,18 +1,14 @@
 package shell
 
 import (
-	"fmt"
 	"testing"
 
 	goprompt "github.com/c-bata/go-prompt"
-	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	v3 "github.com/confluentinc/cli/internal/pkg/config/v3"
 	"github.com/confluentinc/cli/internal/pkg/log"
-	"github.com/confluentinc/cli/internal/pkg/shell/completer"
-	cliMock "github.com/confluentinc/cli/mock"
 )
 
 const (
@@ -59,52 +55,4 @@ func Test_prefixState(t *testing.T) {
 			require.Equal(t, tt.wantColor, color)
 		})
 	}
-}
-
-func TestArgumentParsing(t *testing.T) {
-	tests := []struct {
-		name      string
-		flagValue string
-	}{
-		{
-			name:      "basic flag value",
-			flagValue: "hi",
-		},
-		{
-			name:      "flag value with space in between",
-			flagValue: "hi hi",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			commandCalled := false
-			shellCmd := newTestShellCommandWithExpectedFlag(t, tt.flagValue, &commandCalled)
-			shellCmd.SetArgs([]string{"ccloud", "api", "--description", tt.flagValue})
-			err := shellCmd.Execute()
-			require.NoError(t, err)
-			require.True(t, commandCalled)
-		})
-	}
-}
-
-func newTestShellCommandWithExpectedFlag(t *testing.T, expectedFlag string, commandCalled *bool) *cobra.Command {
-	cli := &cobra.Command{
-		Use: "ccloud",
-	}
-	apiCommand := &cobra.Command{
-		Use: "api",
-		Run: func(cmd *cobra.Command, args []string) {
-			description, err := cmd.Flags().GetString("description")
-			require.NoError(t, err)
-			require.Equal(t, expectedFlag, description)
-			*commandCalled = true
-			fmt.Println("CALLED")
-		},
-	}
-	apiCommand.Flags().String("description", "", "Description of API key.")
-	cli.AddCommand(apiCommand)
-	config := v3.AuthenticatedCloudConfigMock()
-	prerunner := &pcmd.PreRun{}
-	shellCompleter := completer.NewShellCompleter(cli)
-	return NewShellCmd(cli, prerunner, "ccloud", config, nil, shellCompleter, log.New(), cliMock.NewDummyAnalyticsMock(), nil)
 }
