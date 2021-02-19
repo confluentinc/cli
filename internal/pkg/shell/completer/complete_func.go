a3f055856b9a3c37ec3d39287945592d68f5a896
csreesangkom <csreesangkom@chris.sreesangkom-MBP15>
2021-02-19 10:28:47 -0800

    add complete_func.go file

---
+++ internal/pkg/shell/completer/complete_func.go
@@ -0,0 +1,30 @@
+package completer
+
+import (
+	"github.com/c-bata/go-prompt"
+	"github.com/spf13/cobra"
+
+	"github.com/confluentinc/ccloud-sdk-go"
+	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
+	pkafka "github.com/confluentinc/cli/internal/pkg/kafka"
+)
+
+func ClusterFlagServerCompleterFunc(cmd *cobra.Command, client *ccloud.Client, environmentId string) func() []prompt.Suggest {
+	return func() []prompt.Suggest {
+		var suggestions []prompt.Suggest
+		if !pcmd.CanCompleteCommand(cmd) {
+			return suggestions
+		}
+		clusters, err := pkafka.ListKafkaClusters(client, environmentId)
+		if err != nil {
+			return suggestions
+		}
+		for _, cluster := range clusters {
+			suggestions = append(suggestions, prompt.Suggest{
+				Text:        cluster.Id,
+				Description: cluster.Name,
+			})
+		}
+		return suggestions
+	}
+}
