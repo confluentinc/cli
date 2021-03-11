84f85c672c50971b8a06ebbafeacba86dcffd2d3
Miles Todzo <mtodzo@confluent.io>
2021-03-10 20:06:29 -0800

    added json and yaml support for topic update

--- internal/cmd/kafka/command_topic_onprem.go
+++ internal/cmd/kafka/command_topic_onprem.go
@@ -329,6 +329,12 @@ func (topicCmd *topicCommand) deleteTopic(cmd *cobra.Command, args []string) err
 func (topicCmd *topicCommand) updateTopicConfig(cmd *cobra.Command, args []string) error {
 	// Parse Argument
 	topicName := args[0]
+	format, err := cmd.Flags().GetString(output.FlagName)
+	if err != nil {
+		return err
+	} else if output.IsValidFormatString(format) == false { // catch format flag
+		return output.NewInvalidOutputFormatFlagError(format)
+	}
 	restClient, restContext, err := topicCmd.initKafkaRest(cmd)
 	if err != nil {
 		return err
@@ -364,22 +370,29 @@ func (topicCmd *topicCommand) updateTopicConfig(cmd *cobra.Command, args []strin
 	if err != nil {
 		return kafkaRestError(restClient.GetConfig().BasePath, err, resp)
 	}
-	// no errors (config update successful)
-	utils.Printf(cmd, errors.UpdateTopicConfigMsg, topicName)
-	// Print Updated Configs
-	tableLabels := []string{"Name", "Value"}
-	tableEntries := make([][]string, len(configs))
-	for i, config := range configs {
-		tableEntries[i] = printer.ToRow(
-			&struct {
-				Name  string
-				Value string
-			}{Name: config.Name, Value: *config.Value}, []string{"Name", "Value"})
+	if format == output.Human.String() {
+		// no errors (config update successful)
+		utils.Printf(cmd, errors.UpdateTopicConfigMsg, topicName)
+		// Print Updated Configs
+		tableLabels := []string{"Name", "Value"}
+		tableEntries := make([][]string, len(configs))
+		for i, config := range configs {
+			tableEntries[i] = printer.ToRow(
+				&struct {
+					Name  string
+					Value string
+				}{Name: config.Name, Value: *config.Value}, []string{"Name", "Value"})
+		}
+		sort.Slice(tableEntries, func(i int, j int) bool {
+			return tableEntries[i][0] < tableEntries[j][0]
+		})
+		printer.RenderCollectionTable(tableEntries, tableLabels)
+	} else { //json or yaml
+		err = output.StructuredOutput(format, configs)
+		if err != nil {
+			return err
+		}
 	}
-	sort.Slice(tableEntries, func(i int, j int) bool {
-		return tableEntries[i][0] < tableEntries[j][0]
-	})
-	printer.RenderCollectionTable(tableEntries, tableLabels)
 	return nil
 }
 
@@ -394,7 +407,7 @@ func (topicCmd *topicCommand) updateTopicConfig(cmd *cobra.Command, args []strin
 //--client-cert-path string   Path to client cert to be verified by Confluent REST Proxy, include for mTLS authentication.
 //--client-key-path string    Path to client private key, include for mTLS authentication.
 //--no-auth                   Include if requests should be made without authentication headers, and user will not be prompted for credentials.
-//-o, --output string             Specify the output format as "human", "json", or "yaml". (default "human")
+//-o, --output string         Specify the output format as "human", "json", or "yaml". (default "human")
 //--context string            CLI Context name.
 func (topicCmd *topicCommand) describeTopic(cmd *cobra.Command, args []string) error {
 	// Parse Args
