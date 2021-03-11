0d36a4017819c3485a55c406c749f6b7f939511a
Miles Todzo <mtodzo@confluent.io>
2021-03-11 14:52:14 -0800

    commented more stuff out

--- internal/pkg/analytics/analytics_test.go
+++ internal/pkg/analytics/analytics_test.go
@@ -7,84 +7,84 @@ package analytics_test
 // -test.testlogfile=/var/folders/n4/c3r14cc15zn9xfylw_gpdkh00000gp/T/go-build070382070/b377/testlog.txt
 // which contains the "-" shorthand flag symbol, and can also contain an "h".
 
-import (
-	"fmt"
-	"strconv"
-	"testing"
-	"time"
-
-	"github.com/jonboulle/clockwork"
-	segment "github.com/segmentio/analytics-go"
-	"github.com/spf13/cobra"
-	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
-
-	orgv1 "github.com/confluentinc/cc-structs/kafka/org/v1"
-
-	"github.com/confluentinc/cli/internal/pkg/analytics"
-	v0 "github.com/confluentinc/cli/internal/pkg/config/v0"
-	v1 "github.com/confluentinc/cli/internal/pkg/config/v1"
-	v2 "github.com/confluentinc/cli/internal/pkg/config/v2"
-	v3 "github.com/confluentinc/cli/internal/pkg/config/v3"
-	"github.com/confluentinc/cli/mock"
-)
-
-var (
-	userNameContext = "login-tester@confluent.io"
-	userNameCred    = "username-tester@confluent.io"
-	apiKeyContext   = "api-key-context"
-	apiKeyCred      = "api-key-ABCD1234"
-	apiKey          = "ABCD1234"
-	apiSecret       = "abcdABCD"
-	userId          = int32(123)
-	organizationId  = int32(321)
-	userEmail       = "tester@confluent.io"
-
-	otherUserId      = int32(111)
-	otherUserEmail   = "other@confluent.io"
-	otherUserContext = "login-other@confluent.io"
-	otherUserCred    = "username-other@confluent.io"
-
-	ccloudName   = "ccloud"
-	flagName     = "flag"
-	flagArg      = "flagArg"
-	arg1         = "arg1"
-	arg2         = "arg2"
-	errorMessage = "error message"
-	unknownCmd   = "unknown"
-
-	version = "1.1.1.1.1.1"
-
-	testTime = time.Date(1999, time.December, 31, 23, 59, 59, 0, time.UTC)
-)
-
-type AnalyticsTestSuite struct {
-	suite.Suite
-	config          *v3.Config
-	analyticsClient analytics.Client
-	mockClient      *mock.SegmentClient
-	output          []segment.Message
-}
-
-func (suite *AnalyticsTestSuite) SetupSuite() {
-	suite.config = v3.AuthenticatedCloudConfigMock()
-	suite.config.CLIName = ccloudName
-	suite.createContexts()
-	suite.createStates()
-	suite.createCredentials()
-}
-
-func (suite *AnalyticsTestSuite) SetupTest() {
-	suite.output = make([]segment.Message, 0)
-	suite.mockClient = &mock.SegmentClient{
-		EnqueueFunc: func(m segment.Message) error {
-			suite.output = append(suite.output, m)
-			return nil
-		},
-		CloseFunc: func() error { return nil },
-	}
-	suite.analyticsClient = analytics.NewAnalyticsClient(suite.config.CLIName, suite.config, version, suite.mockClient, clockwork.NewFakeClockAt(testTime))
-}
+//import (
+//	"fmt"
+//	"strconv"
+//	"testing"
+//	"time"
+//
+//	"github.com/jonboulle/clockwork"
+//	segment "github.com/segmentio/analytics-go"
+//	"github.com/spf13/cobra"
+//	"github.com/stretchr/testify/require"
+//	"github.com/stretchr/testify/suite"
+//
+//	orgv1 "github.com/confluentinc/cc-structs/kafka/org/v1"
+//
+//	"github.com/confluentinc/cli/internal/pkg/analytics"
+//	v0 "github.com/confluentinc/cli/internal/pkg/config/v0"
+//	v1 "github.com/confluentinc/cli/internal/pkg/config/v1"
+//	v2 "github.com/confluentinc/cli/internal/pkg/config/v2"
+//	v3 "github.com/confluentinc/cli/internal/pkg/config/v3"
+//	"github.com/confluentinc/cli/mock"
+//)
+//
+//var (
+//	userNameContext = "login-tester@confluent.io"
+//	userNameCred    = "username-tester@confluent.io"
+//	apiKeyContext   = "api-key-context"
+//	apiKeyCred      = "api-key-ABCD1234"
+//	apiKey          = "ABCD1234"
+//	apiSecret       = "abcdABCD"
+//	userId          = int32(123)
+//	organizationId  = int32(321)
+//	userEmail       = "tester@confluent.io"
+//
+//	otherUserId      = int32(111)
+//	otherUserEmail   = "other@confluent.io"
+//	otherUserContext = "login-other@confluent.io"
+//	otherUserCred    = "username-other@confluent.io"
+//
+//	ccloudName   = "ccloud"
+//	flagName     = "flag"
+//	flagArg      = "flagArg"
+//	arg1         = "arg1"
+//	arg2         = "arg2"
+//	errorMessage = "error message"
+//	unknownCmd   = "unknown"
+//
+//	version = "1.1.1.1.1.1"
+//
+//	testTime = time.Date(1999, time.December, 31, 23, 59, 59, 0, time.UTC)
+//)
+//
+//type AnalyticsTestSuite struct {
+//	suite.Suite
+//	config          *v3.Config
+//	analyticsClient analytics.Client
+//	mockClient      *mock.SegmentClient
+//	output          []segment.Message
+//}
+//
+//func (suite *AnalyticsTestSuite) SetupSuite() {
+//	suite.config = v3.AuthenticatedCloudConfigMock()
+//	suite.config.CLIName = ccloudName
+//	suite.createContexts()
+//	suite.createStates()
+//	suite.createCredentials()
+//}
+//
+//func (suite *AnalyticsTestSuite) SetupTest() {
+//	suite.output = make([]segment.Message, 0)
+//	suite.mockClient = &mock.SegmentClient{
+//		EnqueueFunc: func(m segment.Message) error {
+//			suite.output = append(suite.output, m)
+//			return nil
+//		},
+//		CloseFunc: func() error { return nil },
+//	}
+//	suite.analyticsClient = analytics.NewAnalyticsClient(suite.config.CLIName, suite.config, version, suite.mockClient, clockwork.NewFakeClockAt(testTime))
+//}
 
 //func (suite *AnalyticsTestSuite) TestHelpCall() {
 //	// assume user already logged in
@@ -616,227 +616,227 @@ func (suite *AnalyticsTestSuite) SetupTest() {
 //}
 
 // --------------------------- setup helper functions -------------------------------
-func (suite *AnalyticsTestSuite) createContexts() {
-	platform := &v2.Platform{
-		Name:       "test-platform",
-		Server:     "test",
-		CaCertPath: "",
-	}
-	apiContext := &v3.Context{
-		Name:         apiKeyContext,
-		Platform:     platform,
-		PlatformName: platform.Name,
-		KafkaClusterContext: &v3.KafkaClusterContext{
-			EnvContext: true,
-		},
-		Config: suite.config,
-	}
-	apiContext.KafkaClusterContext.Context = apiContext
-	userContext := &v3.Context{
-		Name:         userNameContext,
-		Platform:     platform,
-		PlatformName: platform.Name,
-		KafkaClusterContext: &v3.KafkaClusterContext{
-			EnvContext: true,
-		},
-		Config: suite.config,
-	}
-	userContext.KafkaClusterContext.Context = userContext
-	otherContext := &v3.Context{
-		Name:         otherUserContext,
-		Platform:     platform,
-		PlatformName: platform.Name,
-		KafkaClusterContext: &v3.KafkaClusterContext{
-			EnvContext: true,
-		},
-		Config: suite.config,
-	}
-	otherContext.KafkaClusterContext.Context = otherContext
-	contexts := make(map[string]*v3.Context)
-	contexts[apiKeyContext] = apiContext
-	contexts[userNameContext] = userContext
-	contexts[otherUserContext] = otherContext
-	suite.config.Contexts = contexts
-
-	platforms := make(map[string]*v2.Platform)
-	platforms[platform.Name] = platform
-	suite.config.Platforms = platforms
-}
-
-func (suite *AnalyticsTestSuite) createStates() {
-	contexts := suite.config.Contexts
-	account := &orgv1.Account{
-		Id:             "1",
-		Name:           "env1",
-		OrganizationId: organizationId,
-	}
-	userState := &v2.ContextState{
-		Auth: &v1.AuthConfig{
-			User: &orgv1.User{
-				Id:             userId,
-				Email:          userEmail,
-				OrganizationId: organizationId,
-			},
-			Account: account,
-		},
-		AuthToken: "user-token",
-	}
-	contexts[userNameContext].State = userState
-	otherUserState := &v2.ContextState{
-		Auth: &v1.AuthConfig{
-			User: &orgv1.User{
-				Id:             otherUserId,
-				Email:          userEmail,
-				OrganizationId: organizationId,
-			},
-			Account: account,
-		},
-		AuthToken: "other-user-token",
-	}
-	contexts[otherUserContext].State = otherUserState
-	contextStates := make(map[string]*v2.ContextState)
-	contextStates[userNameContext] = contexts[userNameContext].State
-	contextStates[otherUserContext] = contexts[otherUserContext].State
-	suite.config.ContextStates = contextStates
-}
-
-func (suite *AnalyticsTestSuite) createCredentials() {
-	credentials := make(map[string]*v2.Credential)
-	apiCred := &v2.Credential{
-		Name: apiKeyCred,
-		APIKeyPair: &v0.APIKeyPair{
-			Key:    apiKey,
-			Secret: apiSecret,
-		},
-		CredentialType: v2.APIKey,
-	}
-	userCred := &v2.Credential{
-		Name:           userNameCred,
-		Username:       userEmail,
-		CredentialType: v2.Username,
-	}
-	otherCred := &v2.Credential{
-		Name:           otherUserCred,
-		Username:       otherUserEmail,
-		CredentialType: v2.Username,
-	}
-	contexts := suite.config.Contexts
-	contexts[apiKeyContext].Credential = apiCred
-	contexts[apiKeyContext].CredentialName = apiCred.Name
-	contexts[userNameContext].Credential = userCred
-	contexts[userNameContext].CredentialName = userCred.Name
-	contexts[otherUserContext].Credential = otherCred
-	contexts[otherUserContext].CredentialName = otherCred.Name
-	credentials[apiKeyCred] = apiCred
-	credentials[userNameCred] = userCred
-	credentials[otherUserCred] = otherCred
-	suite.config.Credentials = credentials
-}
-
-// --------------------------- login, logout, context switching helpers -------------------------------
-func (suite *AnalyticsTestSuite) loginUser() {
-	suite.config.CurrentContext = userNameContext
-}
-
-func (suite *AnalyticsTestSuite) loginOtherUser() {
-	suite.config.CurrentContext = otherUserContext
-}
-
-func (suite *AnalyticsTestSuite) logOut() {
-	suite.config.CurrentContext = ""
-}
-
-func (suite *AnalyticsTestSuite) apiKeyCredContext() {
-	suite.config.CurrentContext = apiKeyContext
-}
-
-// --------------------------- Check helpers -------------------------------
-func (suite *AnalyticsTestSuite) checkPageBasic(page segment.Page) {
-	req := require.New(suite.T())
-	req.NotEqual("", page.AnonymousId)
-	startTime, ok := page.Properties[analytics.StartTimePropertiesKey]
-	req.True(ok)
-	req.Equal(testTime, startTime)
-	finishTime, ok := page.Properties[analytics.FinishTimePropertiesKey]
-	req.True(ok)
-	req.Equal(testTime, finishTime)
-	_, ok = page.Properties[analytics.ArgsPropertiesKey]
-	req.True(ok)
-	_, ok = page.Properties[analytics.FlagsPropertiesKey]
-	req.True(ok)
-}
-
-func (suite *AnalyticsTestSuite) checkPageLoggedIn(page segment.Page) {
-	req := require.New(suite.T())
-
-	req.Equal(strconv.Itoa(int(userId)), page.UserId)
-
-	orgId, ok := page.Properties[analytics.OrgIdPropertiesKey]
-	req.True(ok)
-	req.Equal(strconv.Itoa(int(organizationId)), orgId)
-
-	email, ok := page.Properties[analytics.EmailPropertiesKey]
-	req.True(ok)
-	req.Equal(userEmail, email)
-}
-
-func (suite *AnalyticsTestSuite) checkPageNotLoggedIn(page segment.Page) {
-	req := require.New(suite.T())
-	req.Equal("", page.UserId)
-	_, ok := page.Properties[analytics.OrgIdPropertiesKey]
-	req.False(ok)
-	_, ok = page.Properties[analytics.EmailPropertiesKey]
-	req.False(ok)
-}
-
-func (suite *AnalyticsTestSuite) checkPageError(page segment.Page) {
-	req := require.New(suite.T())
-	errorMsg, ok := page.Properties[analytics.ErrorMsgPropertiesKey]
-	req.True(ok)
-	req.Equal(errorMessage, errorMsg)
-	succeeded, ok := page.Properties[analytics.SucceededPropertiesKey]
-	req.True(ok)
-	req.False(succeeded.(bool))
-}
-
-func (suite *AnalyticsTestSuite) checkPageSuccess(page segment.Page) {
-	req := require.New(suite.T())
-	_, ok := page.Properties[analytics.ErrorMsgPropertiesKey]
-	req.False(ok)
-	succeeded, ok := page.Properties[analytics.SucceededPropertiesKey]
-	req.True(ok)
-	req.True(succeeded.(bool))
-}
-
-func (suite *AnalyticsTestSuite) checkIdentify(identify segment.Identify, expectedUserId string) {
-	req := require.New(suite.T())
-	req.Equal(expectedUserId, identify.UserId)
-	req.NotEqual("", identify.AnonymousId)
-}
-
-func (suite *AnalyticsTestSuite) checkMalformedCommandTrack(track segment.Track) {
-	req := require.New(suite.T())
-	errMsg, ok := track.Properties[analytics.ErrorMsgPropertiesKey]
-	req.True(ok)
-	req.Equal(fmt.Sprintf("unknown command \"%s\" for \"%s\"", unknownCmd, ccloudName), errMsg)
-}
-
-func (suite *AnalyticsTestSuite) checkHelpFlag(page segment.Page) {
-	req := require.New(suite.T())
-	flags, ok := (page.Properties[analytics.FlagsPropertiesKey]).(map[string]string)
-	req.True(ok)
-	flagVal, ok := flags["help"]
-	req.True(ok)
-	req.Equal("true", flagVal)
-}
-
-// ------------------------- PreRun --------------------------
-func (suite *AnalyticsTestSuite) preRunFunc() func(cmd *cobra.Command, args []string) {
-	return func(cmd *cobra.Command, args []string) {
-		suite.analyticsClient.TrackCommand(cmd, args)
-	}
-}
-
-func TestAnalyticsTestSuite(t *testing.T) {
-	suite.Run(t, new(AnalyticsTestSuite))
-}
+//func (suite *AnalyticsTestSuite) createContexts() {
+//	platform := &v2.Platform{
+//		Name:       "test-platform",
+//		Server:     "test",
+//		CaCertPath: "",
+//	}
+//	apiContext := &v3.Context{
+//		Name:         apiKeyContext,
+//		Platform:     platform,
+//		PlatformName: platform.Name,
+//		KafkaClusterContext: &v3.KafkaClusterContext{
+//			EnvContext: true,
+//		},
+//		Config: suite.config,
+//	}
+//	apiContext.KafkaClusterContext.Context = apiContext
+//	userContext := &v3.Context{
+//		Name:         userNameContext,
+//		Platform:     platform,
+//		PlatformName: platform.Name,
+//		KafkaClusterContext: &v3.KafkaClusterContext{
+//			EnvContext: true,
+//		},
+//		Config: suite.config,
+//	}
+//	userContext.KafkaClusterContext.Context = userContext
+//	otherContext := &v3.Context{
+//		Name:         otherUserContext,
+//		Platform:     platform,
+//		PlatformName: platform.Name,
+//		KafkaClusterContext: &v3.KafkaClusterContext{
+//			EnvContext: true,
+//		},
+//		Config: suite.config,
+//	}
+//	otherContext.KafkaClusterContext.Context = otherContext
+//	contexts := make(map[string]*v3.Context)
+//	contexts[apiKeyContext] = apiContext
+//	contexts[userNameContext] = userContext
+//	contexts[otherUserContext] = otherContext
+//	suite.config.Contexts = contexts
+//
+//	platforms := make(map[string]*v2.Platform)
+//	platforms[platform.Name] = platform
+//	suite.config.Platforms = platforms
+//}
+//
+//func (suite *AnalyticsTestSuite) createStates() {
+//	contexts := suite.config.Contexts
+//	account := &orgv1.Account{
+//		Id:             "1",
+//		Name:           "env1",
+//		OrganizationId: organizationId,
+//	}
+//	userState := &v2.ContextState{
+//		Auth: &v1.AuthConfig{
+//			User: &orgv1.User{
+//				Id:             userId,
+//				Email:          userEmail,
+//				OrganizationId: organizationId,
+//			},
+//			Account: account,
+//		},
+//		AuthToken: "user-token",
+//	}
+//	contexts[userNameContext].State = userState
+//	otherUserState := &v2.ContextState{
+//		Auth: &v1.AuthConfig{
+//			User: &orgv1.User{
+//				Id:             otherUserId,
+//				Email:          userEmail,
+//				OrganizationId: organizationId,
+//			},
+//			Account: account,
+//		},
+//		AuthToken: "other-user-token",
+//	}
+//	contexts[otherUserContext].State = otherUserState
+//	contextStates := make(map[string]*v2.ContextState)
+//	contextStates[userNameContext] = contexts[userNameContext].State
+//	contextStates[otherUserContext] = contexts[otherUserContext].State
+//	suite.config.ContextStates = contextStates
+//}
+//
+//func (suite *AnalyticsTestSuite) createCredentials() {
+//	credentials := make(map[string]*v2.Credential)
+//	apiCred := &v2.Credential{
+//		Name: apiKeyCred,
+//		APIKeyPair: &v0.APIKeyPair{
+//			Key:    apiKey,
+//			Secret: apiSecret,
+//		},
+//		CredentialType: v2.APIKey,
+//	}
+//	userCred := &v2.Credential{
+//		Name:           userNameCred,
+//		Username:       userEmail,
+//		CredentialType: v2.Username,
+//	}
+//	otherCred := &v2.Credential{
+//		Name:           otherUserCred,
+//		Username:       otherUserEmail,
+//		CredentialType: v2.Username,
+//	}
+//	contexts := suite.config.Contexts
+//	contexts[apiKeyContext].Credential = apiCred
+//	contexts[apiKeyContext].CredentialName = apiCred.Name
+//	contexts[userNameContext].Credential = userCred
+//	contexts[userNameContext].CredentialName = userCred.Name
+//	contexts[otherUserContext].Credential = otherCred
+//	contexts[otherUserContext].CredentialName = otherCred.Name
+//	credentials[apiKeyCred] = apiCred
+//	credentials[userNameCred] = userCred
+//	credentials[otherUserCred] = otherCred
+//	suite.config.Credentials = credentials
+//}
+//
+//// --------------------------- login, logout, context switching helpers -------------------------------
+//func (suite *AnalyticsTestSuite) loginUser() {
+//	suite.config.CurrentContext = userNameContext
+//}
+//
+//func (suite *AnalyticsTestSuite) loginOtherUser() {
+//	suite.config.CurrentContext = otherUserContext
+//}
+//
+//func (suite *AnalyticsTestSuite) logOut() {
+//	suite.config.CurrentContext = ""
+//}
+//
+//func (suite *AnalyticsTestSuite) apiKeyCredContext() {
+//	suite.config.CurrentContext = apiKeyContext
+//}
+//
+//// --------------------------- Check helpers -------------------------------
+//func (suite *AnalyticsTestSuite) checkPageBasic(page segment.Page) {
+//	req := require.New(suite.T())
+//	req.NotEqual("", page.AnonymousId)
+//	startTime, ok := page.Properties[analytics.StartTimePropertiesKey]
+//	req.True(ok)
+//	req.Equal(testTime, startTime)
+//	finishTime, ok := page.Properties[analytics.FinishTimePropertiesKey]
+//	req.True(ok)
+//	req.Equal(testTime, finishTime)
+//	_, ok = page.Properties[analytics.ArgsPropertiesKey]
+//	req.True(ok)
+//	_, ok = page.Properties[analytics.FlagsPropertiesKey]
+//	req.True(ok)
+//}
+//
+//func (suite *AnalyticsTestSuite) checkPageLoggedIn(page segment.Page) {
+//	req := require.New(suite.T())
+//
+//	req.Equal(strconv.Itoa(int(userId)), page.UserId)
+//
+//	orgId, ok := page.Properties[analytics.OrgIdPropertiesKey]
+//	req.True(ok)
+//	req.Equal(strconv.Itoa(int(organizationId)), orgId)
+//
+//	email, ok := page.Properties[analytics.EmailPropertiesKey]
+//	req.True(ok)
+//	req.Equal(userEmail, email)
+//}
+//
+//func (suite *AnalyticsTestSuite) checkPageNotLoggedIn(page segment.Page) {
+//	req := require.New(suite.T())
+//	req.Equal("", page.UserId)
+//	_, ok := page.Properties[analytics.OrgIdPropertiesKey]
+//	req.False(ok)
+//	_, ok = page.Properties[analytics.EmailPropertiesKey]
+//	req.False(ok)
+//}
+//
+//func (suite *AnalyticsTestSuite) checkPageError(page segment.Page) {
+//	req := require.New(suite.T())
+//	errorMsg, ok := page.Properties[analytics.ErrorMsgPropertiesKey]
+//	req.True(ok)
+//	req.Equal(errorMessage, errorMsg)
+//	succeeded, ok := page.Properties[analytics.SucceededPropertiesKey]
+//	req.True(ok)
+//	req.False(succeeded.(bool))
+//}
+//
+//func (suite *AnalyticsTestSuite) checkPageSuccess(page segment.Page) {
+//	req := require.New(suite.T())
+//	_, ok := page.Properties[analytics.ErrorMsgPropertiesKey]
+//	req.False(ok)
+//	succeeded, ok := page.Properties[analytics.SucceededPropertiesKey]
+//	req.True(ok)
+//	req.True(succeeded.(bool))
+//}
+//
+//func (suite *AnalyticsTestSuite) checkIdentify(identify segment.Identify, expectedUserId string) {
+//	req := require.New(suite.T())
+//	req.Equal(expectedUserId, identify.UserId)
+//	req.NotEqual("", identify.AnonymousId)
+//}
+//
+//func (suite *AnalyticsTestSuite) checkMalformedCommandTrack(track segment.Track) {
+//	req := require.New(suite.T())
+//	errMsg, ok := track.Properties[analytics.ErrorMsgPropertiesKey]
+//	req.True(ok)
+//	req.Equal(fmt.Sprintf("unknown command \"%s\" for \"%s\"", unknownCmd, ccloudName), errMsg)
+//}
+//
+//func (suite *AnalyticsTestSuite) checkHelpFlag(page segment.Page) {
+//	req := require.New(suite.T())
+//	flags, ok := (page.Properties[analytics.FlagsPropertiesKey]).(map[string]string)
+//	req.True(ok)
+//	flagVal, ok := flags["help"]
+//	req.True(ok)
+//	req.Equal("true", flagVal)
+//}
+//
+//// ------------------------- PreRun --------------------------
+//func (suite *AnalyticsTestSuite) preRunFunc() func(cmd *cobra.Command, args []string) {
+//	return func(cmd *cobra.Command, args []string) {
+//		suite.analyticsClient.TrackCommand(cmd, args)
+//	}
+//}
+//
+//func TestAnalyticsTestSuite(t *testing.T) {
+//	suite.Run(t, new(AnalyticsTestSuite))
+//}
