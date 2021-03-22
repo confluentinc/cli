c2f4e575415ede51c98f3dc8a4a1414aef41c944
DABH <DABH@users.noreply.github.com>
2021-03-22 14:48:20 -0700

    fix

--- mk-files/dockerhub.mk
+++ mk-files/dockerhub.mk
@@ -7,6 +7,5 @@ publish-dockerhub:
 	docker push confluentinc/ccloud-cli:latest
 	docker images | grep "confluentinc/confluent-cli" | awk '{system("docker rmi -f " "'"confluentinc/confluent-cli"'" $2)}'
 	docker build -f ./mk-files/Dockerfile_confluent -t confluentinc/confluent-cli:$(CLEAN_VERSION) -t confluentinc/confluent-cli:latest ./mk-files/
-	docker push --all-tags confluentinc/confluent-cli
 	docker push confluentinc/confluent-cli:$(CLEAN_VERSION)
 	docker push confluentinc/confluent-cli:latest
