4c4c1972a778c3409281480716a3b1af592681ad
Alyssa Huang <51169659+ahuang98@users.noreply.github.com>
2021-04-30 11:38:45 -0700

    CLI-830 CCloud Consumer Group and Consumer Lag support (#820)
    Added consumer lag and basic consumer group ccloud CLI support:
    Consumer group:
    list, describe
    
    Consumer lag:
    summarize, list, get
    
    References
    https://confluentinc.atlassian.net/wiki/spaces/~797050444/pages/1643188895/One+Pager+Exposing+Consumer+Lag
    https://confluentinc.atlassian.net/wiki/spaces/~797050444/pages/2070654288/One+Pager+-+CLI+support+for+Consumer+Group+kafka-rest+APIs
    
    Test&Review
    Added tests to command_test.go and kafka_test.go and confirmed golden file outputs, tested new cc-system-tests locally https://github.com/confluentinc/cc-system-tests/pull/1655 (will merge in after).

--- internal/cmd/kafka/utils.go
+++ internal/cmd/kafka/utils.go
@@ -1,6 +1,11 @@
 package kafka
 
-import "os"
+import (
+	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
+	"github.com/confluentinc/cli/internal/pkg/errors"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 func copyMap(inputMap map[string]string) map[string]string {
 	newMap := make(map[string]string)
@@ -16,4 +21,20 @@ func fileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func getKafkaRestProxyAndLkcId(c *pcmd.AuthenticatedStateFlagCommand, cmd *cobra.Command) (*pcmd.KafkaREST, string, error) {
+	kafkaREST, err := c.AuthenticatedCLICommand.GetKafkaREST()
+	if err != nil {
+		return nil, "", err
+	}
+	if kafkaREST == nil {
+		return nil, "", errors.New(errors.RestProxyNotAvailable)
+	}
+	// Kafka REST is available
+	kafkaClusterConfig, err := c.AuthenticatedCLICommand.Context.GetKafkaClusterForCommand(cmd)
+	if err != nil {
+		return nil, "", err
+	}
+	return kafkaREST, kafkaClusterConfig.ID, nil
 }
