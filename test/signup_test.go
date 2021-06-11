4bf53c1808e085db63948846b1685050ecf70c99
Miles Todzo <33913881+mtodzo@users.noreply.github.com>
2021-06-10 18:23:32 -0700

    CLI-987 removed check email logic in signup flow and persist login to config after signup (#882)
    * removed check email logic and persist login to config
    
    * add suggestion with email already exists errror
    
    * revise error message

--- test/signup_test.go
+++ test/signup_test.go
@@ -10,11 +10,6 @@ func (s *CLITestSuite) TestSignup() {
 	tests := []CLITest{
 		{
 			args:        "signup --url=" + s.TestBackend.GetCloudUrl(),
-			preCmdFuncs: []bincover.PreCmdFunc{stdinPipeFunc(strings.NewReader("already-exists@confluent.io\nMiles\nTodzo\nConfluent\nPa$$word12\ny\ny\ny\n"))},
-			fixture:     "signup/signup-email-exists.golden",
-		},
-		{
-			args:        "signup --url=" + s.TestBackend.GetCloudUrl(),
 			preCmdFuncs: []bincover.PreCmdFunc{stdinPipeFunc(strings.NewReader("test-signup@confluent.io\nMiles\nTodzo\nConfluent\nPa$$word12\nn\ny\nN\nY\nn\ny\n"))},
 			fixture:     "signup/signup-reprompt-on-no-success.golden",
 		},
