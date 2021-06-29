4ebc13dd4b2eb6ee23d37d1d534f7cde7c81002f
Brian Strauch <bstrauch24@gmail.com>
2021-06-29 08:29:19 -0500

    CLI-947: Show all new release notes on update (#903)
    * show all release notes
    
    * fix unit test
    
    * use patch versions to prove semver works

---
+++ internal/cmd/update/command_test.go
@@ -0,0 +1,33 @@
+package update
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/confluentinc/cli/internal/pkg/log"
+	updatemock "github.com/confluentinc/cli/internal/pkg/update/mock"
+	"github.com/confluentinc/cli/internal/pkg/version"
+	"github.com/confluentinc/cli/mock"
+)
+
+func TestGetReleaseNotes_MultipleReleaseNotes(t *testing.T) {
+	client := &updatemock.Client{
+		GetLatestReleaseNotesFunc: func(currentVersion string) (string, []string, error) {
+			notes := []string{
+				"v0.1.0 changes\n",
+				"v1.0.0 changes\n",
+			}
+			return "1.0.0", notes, nil
+		},
+	}
+
+	c := &command{
+		client:          client,
+		logger:          log.New(),
+		analyticsClient: mock.NewDummyAnalyticsMock(),
+		version:         &version.Version{Version: "0.0.0"},
+	}
+
+	require.Equal(t, "v0.1.0 changes\n\nv1.0.0 changes\n", c.getReleaseNotes("1.0.0"))
+}
