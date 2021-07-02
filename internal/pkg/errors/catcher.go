c8f6dfc1896f107bfa444581bcc33bbe43550043
Keahooi Hung <khung@confluent.io>
2021-07-01 17:50:33 -0700

    Cast to string

--- internal/pkg/errors/catcher.go
+++ internal/pkg/errors/catcher.go
@@ -34,9 +34,9 @@ func catchMDSErrors(err error) error {
 	}
 	switch err.(type) {
 	case mds.GenericOpenAPIError:
-		return Errorf(GenericOpenAPIErrorMsg, err.Error(), err.(mds.GenericOpenAPIError).Body())
+		return Errorf(GenericOpenAPIErrorMsg, err.Error(), string(err.(mds.GenericOpenAPIError).Body()))
 	case mdsv2alpha1.GenericOpenAPIError:
-		return Errorf(GenericOpenAPIErrorMsg, err.Error(), err.(mdsv2alpha1.GenericOpenAPIError).Body())
+		return Errorf(GenericOpenAPIErrorMsg, err.Error(), string(err.(mdsv2alpha1.GenericOpenAPIError).Body()))
 	}
 	return err
 }
