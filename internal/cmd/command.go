fc384ec9dec931a25492b5f7feafcc10dacaac98
Brian Strauch <bstrauch@confluent.io>
2021-07-08 15:16:12 -0500

    remove cliname references

--- internal/cmd/command.go
+++ internal/cmd/command.go
@@ -133,7 +133,7 @@ func NewConfluentCommand(cfg *v3.Config, isTest bool, ver *pversion.Version) *co
 	isAPIKeyLogin := isAPIKeyCredential(cfg)
 
 	cli.AddCommand(auditlog.New(cliName, prerunner))
-	cli.AddCommand(completion.New(cli, cliName))
+	cli.AddCommand(completion.New(cli))
 	cli.AddCommand(config.New(cfg.IsCloud(), prerunner, analyticsClient))
 	cli.AddCommand(kafka.New(isAPIKeyLogin, cliName, prerunner, logger.Named("kafka"), ver.ClientID, serverCompleter, analyticsClient))
 	cli.AddCommand(login.New(prerunner, logger, ccloudClientFactory, mdsClientManager, analyticsClient, netrcHandler, loginCredentialsManager, authTokenHandler, isTest).Command)
