ed97b3fc394601af9f4a2d8630ed5a3d8d9eac62
Brian Strauch <bstrauch@confluent.io>
2021-07-22 12:33:58 -0500

    unify update

--- internal/cmd/command.go
+++ internal/cmd/command.go
@@ -137,7 +137,7 @@ func NewConfluentCommand(cfg *v3.Config, isTest bool, ver *pversion.Version) *co
 	cli.AddCommand(logout.New(cliName, prerunner, analyticsClient, netrcHandler).Command)
 	cli.AddCommand(secret.New(flagResolver, secrets.NewPasswordProtectionPlugin(logger)))
 	if !cfg.DisableUpdates {
-		cli.AddCommand(update.New(cliName, logger, ver, updateClient, analyticsClient))
+		cli.AddCommand(update.New(logger, ver, updateClient, analyticsClient))
 	}
 	cli.AddCommand(version.New(prerunner, ver))
 
