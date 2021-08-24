3478dd12b9399076e995c285ca6ba92280ddc963
Miles Todzo <mtodzo@confluent.io>
2021-08-24 18:20:32 -0400

    trailing period for suggestion

--- internal/pkg/errors/error_message.go
+++ internal/pkg/errors/error_message.go
@@ -23,7 +23,7 @@ const (
 	APIKeyNotValidForClusterSuggestions = "Provide the cluster this API key belongs to using the `--resource` flag or the `ccloud kafka cluster use` command."
 	APIKeyNotFoundSuggestions           = "Ensure the API key you are trying to store exists and has not been deleted, or create a new API key via `ccloud api-key create`."
 	ServiceAccountNotFoundErrorMsg      = "Service account \"%s\" not found"
-	ServiceAccountNotFoundSuggestions   = "List service accounts with `ccloud service-account list`"
+	ServiceAccountNotFoundSuggestions   = "List service accounts with `ccloud service-account list`.	"
 
 	// audit-log command
 	EnsureCPSixPlusSuggestions        = "Ensure that you are running against MDS with CP 6.0+."
