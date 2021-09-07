7e380d54cf871d00b281769a7776846bf089da0b
Miles Todzo <mtodzo@confluent.io>
2021-09-07 10:28:57 -0700

    fixed tests

--- internal/cmd/kafka/command_broker_onprem.go
+++ internal/cmd/kafka/command_broker_onprem.go
@@ -201,7 +201,7 @@ func (brokerCmd *brokerCommand) describe(cmd *cobra.Command, args []string) erro
 	return nil
 }
 // fetch per-broker configs or just configName config if specified
-func getIndividualBrokerConfigs(restClient *kafkarestv3.APIClient, restContext context.Context, clusterId, brokerId, configName string) (kafkarestv3.BrokerConfigDataList, error) {
+func getIndividualBrokerConfigs(restClient *kafkarestv3.APIClient, restContext context.Context, clusterId string, brokerId int32, configName string) (kafkarestv3.BrokerConfigDataList, error) {
 	var brokerConfig kafkarestv3.BrokerConfigDataList
 	var resp *http.Response
 	var err error
