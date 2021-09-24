5eee32aaf5617e38c1447bcad5e44ad48960f459
Brian Strauch <bstrauch24@gmail.com>
2021-09-24 13:52:13 -0700

    Hotfix: Extend login timeout to 30s (#1021)
    * extend login timeout to 30s
    
    * fix nil pointer
    
    * adjust timeout for autologin as well

--- internal/cmd/logout/command_test.go
+++ internal/cmd/logout/command_test.go
@@ -176,7 +176,7 @@ func newLoginCmd(auth *sdkMock.Auth, user *sdkMock.User, cliName string, req *re
 	ccloudClientFactory := &cliMock.MockCCloudClientFactory{
 		AnonHTTPClientFactoryFunc: func(baseURL string) *ccloud.Client {
 			req.Equal("https://confluent.cloud", baseURL)
-			return &ccloud.Client{Auth: auth, User: user}
+			return &ccloud.Client{Params: &ccloud.Params{HttpClient: new(http.Client)}, Auth: auth, User: user}
 		},
 		JwtHTTPClientFactoryFunc: func(ctx context.Context, jwt, baseURL string) *ccloud.Client {
 			return &ccloud.Client{Auth: auth, User: user}
