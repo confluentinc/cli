e23654789cf686f666568ad2ba1bcab114573aaf
Jingjing Tian <jtian@confluent.io>
2021-10-22 09:45:58 -0700

    Fix failed test in windows

--- internal/pkg/serdes/jsonserdes.go
+++ internal/pkg/serdes/jsonserdes.go
@@ -17,8 +17,12 @@ type JsonSerializationProvider struct {
 func (jsonProvider *JsonSerializationProvider) LoadSchema(schemaPath string, referencePathMap map[string]string) error {
 	sl := gojsonschema.NewSchemaLoader()
 	for referenceName, referencePath := range referencePathMap {
-		referenceLoader := gojsonschema.NewReferenceLoader("file://" + referencePath)
-		err := sl.AddSchema("/" + referenceName, referenceLoader)
+		refSchema, err := ioutil.ReadFile(referencePath)
+		if err != nil {
+			return err
+		}
+		referenceLoader := gojsonschema.NewStringLoader(string(refSchema))
+		err = sl.AddSchema("/" + referenceName, referenceLoader)
 		if err != nil {
 			return err
 		}
