69519a7aba3b9ece49e9f4d1cfa4aafce0280b23
Brian Strauch <bstrauch@confluent.io>
2021-11-02 13:42:53 -0700

    autocomplete price command

--- go.mod
+++ go.mod
@@ -67,9 +67,9 @@ require (
 	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563 // indirect
 	github.com/segmentio/analytics-go v3.1.0+incompatible
 	github.com/segmentio/backo-go v0.0.0-20160424052352-204274ad699c // indirect
-	github.com/spf13/cobra v1.1.3
+	github.com/spf13/cobra v1.2.1
 	github.com/spf13/pflag v1.0.5
-	github.com/spf13/viper v1.7.1
+	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
 	github.com/stripe/stripe-go v70.15.0+incompatible
 	github.com/tidwall/gjson v1.6.5
@@ -89,7 +89,6 @@ require (
 
 replace (
 	github.com/shurcooL/sanitized_anchor_name => github.com/shurcooL/sanitized_anchor_name v1.0.0
-	github.com/spf13/cobra => github.com/spf13/cobra v1.1.3-0.20210218152603-eb3b6397b1b5
 	github.com/ugorji/go v1.1.4 => github.com/ugorji/go v0.0.0-20190316192920-e2bddce071ad
 	k8s.io/api => k8s.io/api v0.0.0-20190126160459-e86510ea3fe7
 	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20171026124306-e509bb64fe11
--- internal/cmd/kafka/command_region.go
+++ internal/cmd/kafka/command_region.go
@@ -3,6 +3,7 @@ package kafka
 import (
 	"context"
 
+	"github.com/confluentinc/ccloud-sdk-go-v1"
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
@@ -11,7 +12,7 @@ import (
 
 var (
 	regionListFields           = []string{"CloudId", "CloudName", "RegionId", "RegionName"}
-	regionListHumanLabels      = []string{"CloudId", "CloudName", "RegionId", "RegionName"}
+	regionListHumanLabels      = []string{"Cloud ID", "Cloud Name", "Region ID", "Region Name"}
 	regionListStructuredLabels = []string{"cloud_id", "cloud_name", "region_id", "region_name"}
 )
 
@@ -19,66 +20,86 @@ type regionCommand struct {
 	*pcmd.AuthenticatedCLICommand
 }
 
-// NewRegionCommand returns the Cobra command for Kafka region.
-func NewRegionCommand(prerunner pcmd.PreRunner) *cobra.Command {
-	cliCmd := pcmd.NewAuthenticatedCLICommand(
-		&cobra.Command{
-			Use:         "region",
-			Short:       "Manage Confluent Cloud regions.",
-			Long:        "Use this command to manage Confluent Cloud regions.",
-			Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLogin},
-		}, prerunner)
-	cmd := &regionCommand{
-		AuthenticatedCLICommand: cliCmd,
-	}
-	cmd.init()
-	return cmd.Command
+type region struct {
+	CloudId    string
+	CloudName  string
+	RegionId   string
+	RegionName string
 }
 
-func (c *regionCommand) init() {
-	listCmd := &cobra.Command{
+// NewRegionCommand returns the Cobra command for Kafka region.
+func NewRegionCommand(prerunner pcmd.PreRunner) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:         "region",
+		Short:       "Manage Confluent Cloud regions.",
+		Long:        "Use this command to manage Confluent Cloud regions.",
+		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLogin},
+	}
+
+	c := &regionCommand{AuthenticatedCLICommand: pcmd.NewAuthenticatedCLICommand(cmd, prerunner)}
+	c.AddCommand(c.newListCommand())
+	return c.Command
+}
+
+func (c *regionCommand) newListCommand() *cobra.Command {
+	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List cloud provider regions.",
 		Args:  cobra.NoArgs,
-		RunE:  pcmd.NewCLIRunE(c.list),
+		RunE: pcmd.NewCLIRunE(func(cmd *cobra.Command, _ []string) error {
+			cloud, _ := cmd.Flags().GetString("cloud")
+
+			regions, err := ListRegions(c.Client, cloud)
+			if err != nil {
+				return err
+			}
+
+			w, err := output.NewListOutputWriter(cmd, regionListFields, regionListHumanLabels, regionListStructuredLabels)
+			if err != nil {
+				return err
+			}
+
+			for _, region := range regions {
+				w.AddElement(region)
+			}
+
+			return w.Out()
+		}),
 	}
-	listCmd.Flags().String("cloud", "", "The cloud ID to filter by.")
-	listCmd.Flags().StringP(output.FlagName, output.ShortHandFlag, output.DefaultValue, output.Usage)
-	listCmd.Flags().SortFlags = false
-	c.AddCommand(listCmd)
+
+	cmd.Flags().String("cloud", "", "The cloud ID to filter by.")
+	cmd.Flags().StringP(output.FlagName, output.ShortHandFlag, output.DefaultValue, output.Usage)
+	cmd.Flags().SortFlags = false
+
+	return cmd
 }
 
-func (c *regionCommand) list(cmd *cobra.Command, _ []string) error {
-	clouds, err := c.Client.EnvironmentMetadata.Get(context.Background())
+func ListRegions(client *ccloud.Client, cloud string) ([]*region, error) {
+	metadataList, err := client.EnvironmentMetadata.Get(context.Background())
 	if err != nil {
-		return err
+		return nil, err
 	}
-	cloudIdFilter, err := cmd.Flags().GetString("cloud")
-	if err != nil {
-		return err
-	}
-	outputWriter, err := output.NewListOutputWriter(cmd, regionListFields, regionListHumanLabels, regionListStructuredLabels)
-	if err != nil {
-		return err
-	}
-	type regionStruct struct {
-		CloudId    string
-		CloudName  string
-		RegionId   string
-		RegionName string
-	}
-	for _, cloud := range clouds {
-		for _, region := range cloud.Regions {
-			if !region.IsSchedulable || (cloudIdFilter != "" && cloudIdFilter != cloud.Id) {
+
+	var regions []*region
+
+	for _, metadata := range metadataList {
+		if cloud != "" && cloud != metadata.Id {
+			continue
+		}
+
+		for _, r := range metadata.Regions {
+			if !r.IsSchedulable {
 				continue
 			}
-			outputWriter.AddElement(&regionStruct{
-				CloudId:    cloud.Id,
-				CloudName:  cloud.Name,
-				RegionId:   region.Id,
-				RegionName: region.Name,
+
+			regions = append(regions, &region{
+				CloudId:    metadata.Id,
+				CloudName:  metadata.Name,
+				RegionId:   r.Id,
+				RegionName: r.Name,
 			})
 		}
 	}
-	return outputWriter.Out()
+
+	return regions, nil
 }
