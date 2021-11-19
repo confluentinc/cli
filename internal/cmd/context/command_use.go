6c12740e0d155f620389da8d613d063196630738
Brian Strauch <bstrauch@confluent.io>
2021-11-19 12:58:08 -0800

    autocompletion for context (#1083)

--- internal/cmd/context/command_use.go
+++ internal/cmd/context/command_use.go
@@ -8,10 +8,11 @@ import (
 
 func (c *command) newUseCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:   "use [context]",
-		Short: "Use a context.",
-		Args:  cobra.ExactArgs(1),
-		RunE:  pcmd.NewCLIRunE(c.use),
+		Use:               "use <context>",
+		Short:             "Use a context.",
+		Args:              cobra.ExactArgs(1),
+		ValidArgsFunction: pcmd.NewValidArgsFunction(c.validArgs),
+		RunE:              pcmd.NewCLIRunE(c.use),
 	}
 }
 
