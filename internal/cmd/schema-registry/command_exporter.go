e1506649148314c4a147fa6a8a04cf08a347d20e
Jingjing Tian <jtian@confluent.io>
2021-11-30 08:56:49 -0800

    Minor fix

--- internal/cmd/schema-registry/command_exporter.go
+++ internal/cmd/schema-registry/command_exporter.go
@@ -90,10 +90,10 @@ func (c *exporterCommand) init() {
 	}
 	cmd.Flags().StringP(output.FlagName, output.ShortHandFlag, output.DefaultValue, output.Usage)
 	cmd.Flags().StringSlice("subjects", []string{"*"}, "Exporter subjects. Use a comma separated list, or specify the flag multiple times.")
+	cmd.Flags().String("subject-format", "", "Exporter subject rename format. The format string can contain ${subject}, which will be replaced with default subject name.")
 	cmd.Flags().String("context-type", "AUTO", `Exporter context type. One of "AUTO", "CUSTOM" or "NONE".`)
 	cmd.Flags().String("context-name", "", "Exporter context name.")
 	cmd.Flags().String("config-file", "", "Exporter config file.")
-	cmd.Flags().String("subject-format", "", "Exporter subject rename format. The format string can contain ${subject}, which will be replaced with default subject name.")
 
 	_ = cmd.MarkFlagRequired("config-file")
 	c.AddCommand(cmd)
@@ -116,10 +116,10 @@ func (c *exporterCommand) init() {
 	}
 	cmd.Flags().StringP(output.FlagName, output.ShortHandFlag, output.DefaultValue, output.Usage)
 	cmd.Flags().StringSlice("subjects", []string{}, "Exporter subjects. Use a comma separated list, or specify the flag multiple times.")
+	cmd.Flags().String("subject-format", "", "Exporter subject rename format. The format string can contain ${subject}, which will be replaced with default subject name.")
 	cmd.Flags().String("context-type", "", `Exporter context type. One of "AUTO", "CUSTOM" or "NONE".`)
 	cmd.Flags().String("context-name", "", "Exporter context name.")
 	cmd.Flags().String("config-file", "", "Exporter config file.")
-	cmd.Flags().String("subject-format", "", "Exporter subject rename format. The format string can contain ${subject}, which will be replaced with default subject name.")
 
 	c.AddCommand(cmd)
 
