ffd522ce67dd76c0b679f9ef7db0900938156e47
Brian Strauch <bstrauch@confluent.io>
2021-12-02 14:05:22 -0800

    Split `ksql` commands into individual files (#1099)
    * split ksql commands into individual files
    
    * fix naming and tests
    
    * remove prerunner from command struct
    
    * fix integration tests

--- internal/cmd/ksql/command.go
+++ internal/cmd/ksql/command.go
@@ -12,36 +12,31 @@ import (
 
 type command struct {
 	*pcmd.CLICommand
-	prerunner       pcmd.PreRunner
 	serverCompleter completer.ServerSideCompleter
 	analyticsClient analytics.Client
 }
 
-// New returns the default command object for interacting with KSQL.
 func New(cfg *v1.Config, prerunner pcmd.PreRunner, serverCompleter completer.ServerSideCompleter, analyticsClient analytics.Client) *cobra.Command {
-	cliCmd := pcmd.NewCLICommand(
-		&cobra.Command{
-			Use:         "ksql",
-			Short:       "Manage ksqlDB applications.",
-			Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLoginOrOnPremLogin},
-		}, prerunner)
-	cmd := &command{
-		CLICommand:      cliCmd,
-		prerunner:       prerunner,
+	cmd := &cobra.Command{
+		Use:         "ksql",
+		Short:       "Manage ksqlDB applications.",
+		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLoginOrOnPremLogin},
+	}
+
+	c := &command{
+		CLICommand:      pcmd.NewCLICommand(cmd, prerunner),
 		serverCompleter: serverCompleter,
 		analyticsClient: analyticsClient,
 	}
-	cmd.init(cfg)
-	return cmd.Command
-}
 
-func (c *command) init(cfg *v1.Config) {
-	clusterCmd := NewClusterCommand(c.prerunner, c.analyticsClient)
+	appCmd := newAppCommand(prerunner, c.analyticsClient)
 
-	c.AddCommand(clusterCmd.Command)
-	c.AddCommand(NewClusterCommandOnPrem(c.prerunner))
+	c.AddCommand(appCmd.Command)
+	c.AddCommand(NewClusterCommandOnPrem(prerunner))
 
 	if cfg.IsCloudLogin() {
-		c.serverCompleter.AddCommand(clusterCmd)
+		c.serverCompleter.AddCommand(appCmd)
 	}
+
+	return c.Command
 }
