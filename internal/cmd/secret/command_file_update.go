f490d0e727c85590d00e55f7dacbf7d5ea3771ae
Brian Strauch <bstrauch@confluent.io>
2021-12-13 13:06:38 -0800

    cleanup secret, update, and version

---
+++ internal/cmd/secret/command_file_update.go
@@ -0,0 +1,55 @@
+package secret
+
+import (
+	"github.com/spf13/cobra"
+
+	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
+	"github.com/confluentinc/cli/internal/pkg/errors"
+	"github.com/confluentinc/cli/internal/pkg/utils"
+)
+
+func (c *command) newUpdateCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update",
+		Short: "Update the encrypted secrets from the configuration properties file.",
+		Long:  "This command updates the encrypted secrets from the configuration properties file.",
+		Args:  cobra.NoArgs,
+		RunE:  pcmd.NewCLIRunE(c.update),
+	}
+
+	cmd.Flags().String("config-file", "", "Path to the configuration properties file.")
+	cmd.Flags().String("local-secrets-file", "", "Path to the local encrypted configuration properties file.")
+	cmd.Flags().String("remote-secrets-file", "", "Path to the remote encrypted configuration properties file.")
+	cmd.Flags().String("config", "", "List of key/value pairs of configuration properties.")
+
+	_ = cmd.MarkFlagRequired("config-file")
+	_ = cmd.MarkFlagRequired("local-secrets-file")
+	_ = cmd.MarkFlagRequired("remote-secrets-file")
+	_ = cmd.MarkFlagRequired("config")
+
+	return cmd
+}
+
+func (c *command) update(cmd *cobra.Command, _ []string) error {
+	configSource, err := cmd.Flags().GetString("config")
+	if err != nil {
+		return err
+	}
+
+	newConfigs, err := c.getConfigs(configSource, "config properties", "", false)
+	if err != nil {
+		return err
+	}
+
+	configPath, localSecretsPath, remoteSecretsPath, err := c.getConfigFilePath(cmd)
+	if err != nil {
+		return err
+	}
+
+	if err := c.plugin.UpdateEncryptedPasswords(configPath, localSecretsPath, remoteSecretsPath, newConfigs); err != nil {
+		return err
+	}
+
+	utils.ErrPrintln(cmd, errors.UpdateSecretFileMsg)
+	return nil
+}
