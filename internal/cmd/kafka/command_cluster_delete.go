17c73535394a733a015d7077e9cd3a68b9256e82
Brian Strauch <bstrauch@confluent.io>
2021-12-15 20:08:42 -0800

    Autocompletion: `confluent kafka` (#1136)
    * refactor to avoid merge conflicts
    
    * autocomplete kafka cluster
    
    * autocomplete kafka topic and consumer-group

--- internal/cmd/kafka/command_cluster_delete.go
+++ internal/cmd/kafka/command_cluster_delete.go
@@ -15,11 +15,12 @@ import (
 
 func (c *clusterCommand) newDeleteCommand(cfg *v1.Config) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:         "delete <id>",
-		Short:       "Delete a Kafka cluster.",
-		Args:        cobra.ExactArgs(1),
-		RunE:        pcmd.NewCLIRunE(c.delete),
-		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLogin},
+		Use:               "delete <id>",
+		Short:             "Delete a Kafka cluster.",
+		Args:              cobra.ExactArgs(1),
+		ValidArgsFunction: pcmd.NewValidArgsFunction(c.validArgs),
+		RunE:              pcmd.NewCLIRunE(c.delete),
+		Annotations:       map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLogin},
 	}
 
 	pcmd.AddContextFlag(cmd, c.CLICommand)
--- internal/cmd/kafka/command_cluster_update.go
+++ internal/cmd/kafka/command_cluster_update.go
@@ -18,9 +18,10 @@ import (
 
 func (c *clusterCommand) newUpdateCommand(cfg *v1.Config) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update <id>",
-		Short: "Update a Kafka cluster.",
-		Args:  cobra.ExactArgs(1),
+		Use:               "update <id>",
+		Short:             "Update a Kafka cluster.",
+		Args:              cobra.ExactArgs(1),
+		ValidArgsFunction: pcmd.NewValidArgsFunction(c.validArgs),
 		RunE: pcmd.NewCLIRunE(func(cmd *cobra.Command, args []string) error {
 			return c.update(cmd, args, form.NewPrompt(os.Stdin))
 		}),
