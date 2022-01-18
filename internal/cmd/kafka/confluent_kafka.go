10cdb6224151c16f2e0658b5997ce7da82c277fd
MuweiHe <mhe@confluent.io>
2022-01-18 14:31:08 -0800

    delete extra newline

--- internal/cmd/kafka/confluent_kafka.go
+++ internal/cmd/kafka/confluent_kafka.go
@@ -13,7 +13,6 @@ import (
 	"regexp"
 	"time"
 
-
 	pauth "github.com/confluentinc/cli/internal/pkg/auth"
 	configv1 "github.com/confluentinc/cli/internal/pkg/config/v1"
 	"github.com/confluentinc/cli/internal/pkg/errors"
