4cb8b356a6c28abd9e0860a95e19860f8db62908
MuweiHe <mhe@confluent.io>
2022-02-09 16:41:40 -0800

    Update main.go

--- cmd/lint/main.go
+++ cmd/lint/main.go
@@ -30,7 +30,7 @@ var commandRules = []linter.CommandRule{
 	linter.RequireStartWithCapital("Long"),
 
 	// Soft Requirements
-	linter.Filter(linter.RequireLengthBetween("Short", 10, 60), linter.ExcludeCommand("schema-registry")),
+	linter.Filter(linter.RequireLengthBetween("Short", 10, 60), linter.ExcludeCommand("schema-registry compatibility")),
 }
 
 var flagRules = []linter.FlagRule{
