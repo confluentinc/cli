09e390c2a05949aa748d1803d30462c91368cb5b
David Hyde <DABH@users.noreply.github.com>
2022-02-11 23:07:47 -0600

    Update internal/cmd/local/command_service_connect.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/cmd/local/command_service_connect.go
+++ internal/cmd/local/command_service_connect.go
@@ -389,17 +389,8 @@ func makeRequest(method, url string, body []byte) (string, error) {
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-		},
-		ForceAttemptHTTP2: true,
-	}
-
-	client := &http.Client{
-		Timeout:   10 * time.Second,
-		Transport: transport,
-	}
+	client := utils.DefaultClient()
+	client.Timeout = 10*time.Second
 	res, err := client.Do(req)
 	if err != nil {
 		return "", err
