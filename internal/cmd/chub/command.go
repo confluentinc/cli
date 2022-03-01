434f27c991292ea66563b006fc1e363c06e0e683
David Hyde <DABH@users.noreply.github.com>
2022-03-01 16:29:44 -0600

    Update internal/cmd/chub/command.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/cmd/chub/command.go
+++ internal/cmd/chub/command.go
@@ -57,7 +57,7 @@ const (
 
 	daemonOption = "-daemon"
 
-	connectProcessRegex = "org\\.apache\\.kafka\\.connect\\.cli\\.Connect(Distributed|Standalone)"
+	connectProcessRegex = `org\.apache\.kafka\.connect\.cli\.Connect(Distributed|Standalone)`
 )
 
 func New(prerunner pcmd.PreRunner) *cobra.Command {
