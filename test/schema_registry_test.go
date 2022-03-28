b958e71fb7b314d2929db2e0745357fb9121c3f9
Jingjing Tian <jtian@confluent.io>
2022-03-28 10:28:08 -0700

    Fix incorrect options for SR subject mode update (#1216)
    * Fix incorrect options for SR subject mode update
    
    * Fix lint

--- test/schema_registry_test.go
+++ test/schema_registry_test.go
@@ -22,6 +22,8 @@ func (s *CLITestSuite) TestSchemaRegistry() {
 		{args: "schema-registry schema --help", fixture: "schema-registry/schema-help.golden"},
 		{args: "schema-registry subject --help", fixture: "schema-registry/subject-help.golden"},
 		{args: "schema-registry exporter --help", fixture: "schema-registry/exporter-help.golden"},
+		{args: "schema-registry cluster update --help", fixture: "schema-registry/cluster-update-help.golden"},
+		{args: "schema-registry subject update --help", fixture: "schema-registry/subject-update-help.golden"},
 
 		{args: "schema-registry cluster describe", fixture: "schema-registry/describe.golden"},
 		{args: "schema-registry cluster update --environment=" + testserver.SRApiEnvId, fixture: "schema-registry/update-missing-flags.golden", wantErrCode: 1},
@@ -71,7 +73,7 @@ func (s *CLITestSuite) TestSchemaRegistry() {
 		},
 		{
 			name:    "schema-registry subject update mode",
-			args:    "schema-registry subject update testSubject --mode READ --api-key=key --api-secret=secret --environment=" + testserver.SRApiEnvId,
+			args:    "schema-registry subject update testSubject --mode READONLY --api-key=key --api-secret=secret --environment=" + testserver.SRApiEnvId,
 			fixture: "schema-registry/subject-update-mode.golden",
 		},
 
