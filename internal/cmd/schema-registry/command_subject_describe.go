9d520edf70a1d64e9bb7c39c95a9934d1ebc52c0
Miles Todzo <33913881+mtodzo@users.noreply.github.com>
2022-04-06 09:59:30 -0700

    add shorthand for sr subject describe (#1244)

--- internal/cmd/schema-registry/command_subject_describe.go
+++ internal/cmd/schema-registry/command_subject_describe.go
@@ -29,7 +29,7 @@ func (c *subjectCommand) newDescribeCommand() *cobra.Command {
 		),
 	}
 
-	cmd.Flags().Bool("deleted", false, "View the deleted schema.")
+	cmd.Flags().BoolP("deleted", "D", false, "View the deleted schema.")
 	pcmd.AddApiKeyFlag(cmd, c.AuthenticatedCLICommand)
 	pcmd.AddApiSecretFlag(cmd)
 	pcmd.AddContextFlag(cmd, c.CLICommand)
