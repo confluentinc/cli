bbc124dfe08a615ba6730cb44098b6c42c3a1db1
Brian Strauch <bstrauch@confluent.io>
2022-04-11 13:11:47 -0700

    fix resize error message (#1251)

--- internal/pkg/errors/error_message.go
+++ internal/pkg/errors/error_message.go
@@ -122,7 +122,7 @@ const (
 	BYOKSupportErrorMsg                           = "BYOK is available on AWS and GCP."
 	CKUMoreThanZeroErrorMsg                       = "`--cku` value must be greater than 0"
 	CKUMoreThanOneErrorMsg                        = "`--cku` value must be greater than 1 for High Durability"
-	ClusterResizeNotSupported                     = "Cluster resize is support only on dedicated clusters."
+	ClusterResizeNotSupported                     = "cluster resize is only supported on dedicated clusters"
 	CloudRegionNotAvailableErrorMsg               = "\"%s\" is not an available region for \"%s\""
 	CloudRegionNotAvailableSuggestions            = "To view a list of available regions for \"%s\", use `confluent kafka region list --cloud %s`."
 	CloudProviderNotAvailableErrorMsg             = "\"%s\" is not an available cloud provider"
@@ -206,7 +206,7 @@ const (
 	SchemaNotFoundErrorMsg                   = "schema registry subject or version not found"
 	SchemaNotFoundSuggestions                = "List available subjects with `confluent schema-registry subject list`.\n" +
 		"List available versions with `confluent schema-registry subject describe`"
-	NoSubjectLevelConfigErrorMsg 			 = `subject "%s" does not have subject-level compatibility configured`
+	NoSubjectLevelConfigErrorMsg = `subject "%s" does not have subject-level compatibility configured`
 
 	// secret commands
 	EnterInputTypeErrorMsg    = "enter %s"
