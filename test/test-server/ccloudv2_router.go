bd3a63e425f8bf3f6e9fba1be33050311edc455d
MuweiHe <83249359+MuweiHe@users.noreply.github.com>
2022-04-11 17:48:37 -0700

    CLI-1711 refactor quotas client into v2 client package (#1247)
    * chore: minor version bump v2.10.0 [ci skip]
    
    * integrate quotas client into v2 client
    
    * clean up
    
    * refactor
    
    * rename new client method
    
    Co-authored-by: Miles Todzo <mtodzo@confluent.io>

--- test/test-server/ccloudv2_router.go
+++ test/test-server/ccloudv2_router.go
@@ -8,14 +8,15 @@ import (
 )
 
 var ccloudv2Handlers = map[string]func(*testing.T) http.HandlerFunc{
-	"/cmk/v2/clusters/{id}":         HandleCmkCluster,
-	"/cmk/v2/clusters":              HandleCmkClusters,
-	"/org/v2/environments/{id}":     HandleOrgEnvironment,
-	"/org/v2/environments":          HandleOrgEnvironments,
-	"/iam/v2/users/{id}":            HandleIamUser,
-	"/iam/v2/users":                 HandleIamUsers,
-	"/iam/v2/service-accounts/{id}": HandleIamServiceAccount,
-	"/iam/v2/service-accounts":      HandleIamServiceAccounts,
+	"/cmk/v2/clusters/{id}":            handleCmkCluster,
+	"/cmk/v2/clusters":                 handleCmkClusters,
+	"/iam/v2/users/{id}":               handleIamUser,
+	"/iam/v2/users":                    handleIamUsers,
+	"/iam/v2/service-accounts/{id}":    handleIamServiceAccount,
+	"/iam/v2/service-accounts":         handleIamServiceAccounts,
+	"/org/v2/environments/{id}":        handleOrgEnvironment,
+	"/org/v2/environments":             handleOrgEnvironments,
+	"/service-quota/v2/applied-quotas": handleAppliedQuotas,
 }
 
 type V2Router struct {
