e644420484274f0cd7922ed865d94af543c30501
Alexandra Rodoni <arodoni@confluent.io>
2022-04-21 17:36:22 -0700

    CLI-1744 Notes about headers when producing and consuming

--- internal/cmd/kafka/command_topic_consume.go
+++ internal/cmd/kafka/command_topic_consume.go
@@ -22,6 +22,7 @@ func newConsumeCommand(prerunner pcmd.PreRunner, clientId string) *cobra.Command
 	cmd := &cobra.Command{
 		Use:         "consume <topic>",
 		Short:       "Consume messages from a Kafka topic.",
+		Long:        "Consume messages from a Kafka topic.\n\nA truncated header content will print out if not empty. You can use the --full-header flag to view the complete un-truncated header content.",
 		Args:        cobra.ExactArgs(1),
 		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireCloudLogin},
 		Example: examples.BuildExampleString(
