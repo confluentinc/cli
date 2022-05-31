94523c6a99bb3a801ce6cd8cd9e70cc50410bd8b
MuweiHe <mhe@confluent.io>
2022-05-31 14:29:51 -0700

    Update error_message.go

--- internal/pkg/errors/error_message.go
+++ internal/pkg/errors/error_message.go
@@ -173,7 +173,7 @@ const (
 	ProducingToCompactedTopicErrorMsg    = "producer has detected an INVALID_RECORD error for topic %s"
 	ProducingToCompactedTopicSuggestions = "If the topic has schema validation enabled, ensure you are producing with a schema-enabled producer.\n" +
 		"If your topic is compacted, ensure you are producing a record with a key."
-	FailedToLoadSchemaSuggestions = "Specify a schema by passing the path to a schema file to `--schema` flag, or by passing a registered schema ID to `--schema-id`."
+	FailedToLoadSchemaSuggestions = "Specify a schema by passing the path to a schema file to the `--schema` flag, or by passing a registered schema ID to the `--schema-id` flag."
 
 	// Cluster Link commands
 	EmptyConfigErrorMsg = "Config file name is empty or config file is empty."
