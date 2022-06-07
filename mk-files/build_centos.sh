59dcd4a9e54f6c6b8404b39b29cdbb0290010d65
MuweiHe <mhe@confluent.io>
2022-06-07 14:56:03 -0700

    renaming

--- mk-files/build_centos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ..
-cp ~/.netrc .
-docker build . -f Dockerfile_centos -t cli-centos-builder-image
-docker container create --name cli-centos-builder cli-centos-builder-image
-docker container cp cli-centos-builder:/go/src/github.com/confluentinc/cli/dist/. ./dist/
-docker container rm cli-centos-builder
--- mk-files/release.mk
+++ mk-files/release.mk
@@ -60,11 +60,11 @@ restore-librdkafka-amd64:
 		sudo mv $(RDKAFKA_PATH)/librdkafka_amd64.a $(RDKAFKA_PATH)/librdkafka_darwin.a;\
 	fi
 
-# The centos container doesn't need to publish to S3 so it doesn't need to $(caasenv-authenticate)
-.PHONY: gorelease-centos
-gorelease-centos:
+# The glibc container doesn't need to publish to S3 so it doesn't need to $(caasenv-authenticate)
+.PHONY: gorelease-linux-glibc
+gorelease-linux-glibc:
 	GO111MODULE=off go get -u github.com/inconshreveable/mousetrap && \
-	GOPRIVATE=github.com/confluentinc GONOSUMDB=github.com/confluentinc,github.com/golangci/go-misc VERSION=$(VERSION) HOSTNAME="$(HOSTNAME)" S3FOLDER=$(S3_STAG_FOLDER_NAME)/confluent-cli goreleaser release --rm-dist -f .goreleaser-centos.yml
+	GOPRIVATE=github.com/confluentinc GONOSUMDB=github.com/confluentinc,github.com/golangci/go-misc VERSION=$(VERSION) HOSTNAME="$(HOSTNAME)" S3FOLDER=$(S3_STAG_FOLDER_NAME)/confluent-cli goreleaser release --rm-dist -f .goreleaser-linux-glibc.yml
 
 
 # This builds the Darwin, Windows and Linux binaries using goreleaser on the host computer. Goreleaser takes care of uploading the resulting binaries/archives/checksums to S3.
@@ -73,10 +73,10 @@ gorelease:
 	$(eval token := $(shell (grep github.com ~/.netrc -A 2 | grep password || grep github.com ~/.netrc -A 2 | grep login) | head -1 | awk -F' ' '{ print $$2 }'))
 	$(aws-authenticate) && \
 	GO111MODULE=off go get -u github.com/inconshreveable/mousetrap && \
-	./build_centos.sh && \
-	aws s3 cp dist/confluent_$(VERSION)_centos_amd64.tar.gz $(S3_STAG_PATH)/confluent-cli/archives/$(VERSION_NO_V)/confluent_$(VERSION)_centos_amd64.tar.gz; \
-	aws s3 cp dist/confluent_centos_amd64/confluent $(S3_STAG_PATH)/confluent-cli/binaries/$(VERSION_NO_V)/confluent_$(VERSION_NO_V)_centos_amd64; \
-	cat dist/confluent_$(VERSION_NO_V)_checksums_centos.txt >> dist/confluent_$(VERSION_NO_V)_checksums.txt
+	./build_linux_glibc.sh && \
+	aws s3 cp dist/confluent_$(VERSION)_linux_glibc_amd64.tar.gz $(S3_STAG_PATH)/confluent-cli/archives/$(VERSION_NO_V)/confluent_$(VERSION)_linux_glibc_amd64.tar.gz; \
+	aws s3 cp dist/confluent_linux_glibc_amd64/confluent $(S3_STAG_PATH)/confluent-cli/binaries/$(VERSION_NO_V)/confluent_$(VERSION_NO_V)_linux_glibc_amd64; \
+	cat dist/confluent_$(VERSION_NO_V)_checksums_linux_glibc.txt >> dist/confluent_$(VERSION_NO_V)_checksums.txt
 	GOPRIVATE=github.com/confluentinc VERSION=$(VERSION) HOSTNAME="$(HOSTNAME)" GITHUB_TOKEN=$(token) S3FOLDER=$(S3_STAG_FOLDER_NAME)/confluent-cli goreleaser release --rm-dist -f .goreleaser.yml || true && \
 	make restore-librdkafka-amd64
 
