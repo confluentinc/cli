549c5edbcf2d2901b4fab5f415467cfaa7fe0fd9
MuweiHe <mhe@confluent.io>
2022-06-07 14:58:24 -0700

    Update release.mk

--- mk-files/release.mk
+++ mk-files/release.mk
@@ -1,4 +1,4 @@
-ARCHIVE_TYPES=darwin_amd64.tar.gz darwin_arm64.tar.gz linux_amd64.tar.gz windows_amd64.zip
+ARCHIVE_TYPES=darwin_amd64.tar.gz darwin_arm64.tar.gz linux_amd64.tar.gz linux_glibc_amd64.tar.gz windows_amd64.zip
 
 .PHONY: release
 release: check-branch commit-release tag-release
@@ -73,10 +73,7 @@ gorelease:
 	$(eval token := $(shell (grep github.com ~/.netrc -A 2 | grep password || grep github.com ~/.netrc -A 2 | grep login) | head -1 | awk -F' ' '{ print $$2 }'))
 	$(aws-authenticate) && \
 	GO111MODULE=off go get -u github.com/inconshreveable/mousetrap && \
-	./build_linux_glibc.sh && \
-	aws s3 cp dist/confluent_$(VERSION)_linux_glibc_amd64.tar.gz $(S3_STAG_PATH)/confluent-cli/archives/$(VERSION_NO_V)/confluent_$(VERSION)_linux_glibc_amd64.tar.gz; \
-	aws s3 cp dist/confluent_linux_glibc_amd64/confluent $(S3_STAG_PATH)/confluent-cli/binaries/$(VERSION_NO_V)/confluent_$(VERSION_NO_V)_linux_glibc_amd64; \
-	cat dist/confluent_$(VERSION_NO_V)_checksums_linux_glibc.txt >> dist/confluent_$(VERSION_NO_V)_checksums.txt
+	./build_linux_glibc.sh || true && \
 	GOPRIVATE=github.com/confluentinc VERSION=$(VERSION) HOSTNAME="$(HOSTNAME)" GITHUB_TOKEN=$(token) S3FOLDER=$(S3_STAG_FOLDER_NAME)/confluent-cli goreleaser release --rm-dist -f .goreleaser.yml || true && \
 	make restore-librdkafka-amd64
 
