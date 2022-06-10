c1c56592c5efd949e8576922edbc9fcbbab072e7
Brian Strauch <bstrauch@confluent.io>
2022-06-09 17:43:22 -0700

    organize imports

--- internal/cmd/login/command.go
+++ internal/cmd/login/command.go
@@ -8,16 +8,13 @@ import (
 	"regexp"
 	"strings"
 
+	orgv1 "github.com/confluentinc/cc-structs/kafka/org/v1"
 	"github.com/confluentinc/ccloud-sdk-go-v1"
 	"github.com/spf13/cobra"
 
 	"github.com/confluentinc/cli/internal/cmd/admin"
-
-	"github.com/confluentinc/cli/internal/pkg/ccloudv2"
-
-	orgv1 "github.com/confluentinc/cc-structs/kafka/org/v1"
-
 	pauth "github.com/confluentinc/cli/internal/pkg/auth"
+	"github.com/confluentinc/cli/internal/pkg/ccloudv2"
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	v1 "github.com/confluentinc/cli/internal/pkg/config/v1"
 	"github.com/confluentinc/cli/internal/pkg/errors"
