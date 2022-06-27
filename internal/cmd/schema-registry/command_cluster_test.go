5cedcac60153f81443e66aa288757d9f2a8ecbfa
Anshul-Goyal <agoyal@confluent.io>
2022-06-27 13:21:25 +0530

    DGS-3797 adding package flag

--- internal/cmd/schema-registry/command_cluster_test.go
+++ internal/cmd/schema-registry/command_cluster_test.go
@@ -98,7 +98,7 @@ func (suite *ClusterTestSuite) newCMD() *cobra.Command {
 
 func (suite *ClusterTestSuite) TestCreateSR() {
 	cmd := suite.newCMD()
-	cmd.SetArgs([]string{"cluster", "enable", "--cloud", "aws", "--geo", "us"})
+	cmd.SetArgs([]string{"cluster", "enable", "--cloud", "aws", "--geo", "us", "--package", "advanced"})
 	err := cmd.Execute()
 	req := require.New(suite.T())
 	req.Nil(err)
