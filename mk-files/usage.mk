0016f422332a46235d34e6a300f1dade9f32c198
Brian Strauch <bstrauch@confluent.io>
2022-07-05 10:23:05 -0700

    Fix `make update-whitelist` target (#1342)
    * fix update-whitelist target
    
    * capitalize pr title

--- mk-files/usage.mk
+++ mk-files/usage.mk
@@ -8,19 +8,16 @@ endef
 
 export MIGRATEDOWN
 update-whitelist:
-	$(eval CC_CLI_SERVICE=$(shell mktemp -d)/cc-cli-service)
-	
+	$(eval CC_CLI_SERVICE=$(shell mktemp -d)/cc-cli-service) && \
 	git clone git@github.com:confluentinc/cc-cli-service.git $(CC_CLI_SERVICE) && \
 	cd $(CC_CLI_SERVICE) && \
 	git checkout -b update-whitelist-$(BUMPED_VERSION) && \
 	make db-migrate-create NAME=$(BUMPED_VERSION) && \
-	
-	go run -ldflags "-X main.version=$(BUMPED_VERSION)" cmd/usage/main.go > $(find $(CC_CLI_SERVICE)/db/migrations/ $(BUMPED_VERSION).up.sql)
-	echo "$$MIGRATEDOWN" > $(find $(CC_CLI_SERVICE)/db/migrations/ $(BUMPED_VERSION).down.sql)
-	
+	go run -ldflags "-X main.version=$(BUMPED_VERSION)" cmd/usage/main.go > $$(find $(CC_CLI_SERVICE)/db/migrations/ -name "*_$(BUMPED_VERSION).up.sql") && \
+	echo "$$MIGRATEDOWN" > $$(find $(CC_CLI_SERVICE)/db/migrations/ -name "*_$(BUMPED_VERSION).down.sql") && \
 	cd $(CC_CLI_SERVICE) && \
 	make db-migrate-up && \
 	git add . && \
 	git commit -m "update whitelist for $(BUMPED_VERSION)" && \
 	git push origin update-whitelist-$(BUMPED_VERSION) && \
-	hub pull-request -b master -m "update whitelist for $(BUMPED_VERSION)"
+	hub pull-request -b master -m "Update whitelist for $(BUMPED_VERSION)"
