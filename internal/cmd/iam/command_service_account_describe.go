3126d0c2b2e703b47ff2cf047fc4611acf370010
Steven Gagniere <sgagniere@confluent.io>
2022-07-11 16:04:44 -0700

    Removed unnecessary error message

--- internal/cmd/iam/command_service_account_describe.go
+++ internal/cmd/iam/command_service_account_describe.go
@@ -6,7 +6,6 @@ import (
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/errors"
 	"github.com/confluentinc/cli/internal/pkg/output"
-	"github.com/confluentinc/cli/internal/pkg/resource"
 )
 
 func (c serviceAccountCommand) newDescribeCommand() *cobra.Command {
@@ -24,9 +23,6 @@ func (c serviceAccountCommand) newDescribeCommand() *cobra.Command {
 
 func (c serviceAccountCommand) describe(cmd *cobra.Command, args []string) error {
 	serviceAccountId := args[0]
-	if resource.LookupType(serviceAccountId) != resource.ServiceAccount {
-		return errors.New(errors.BadServiceAccountIDErrorMsg)
-	}
 
 	sa, httpResp, err := c.V2Client.GetIamServiceAccount(serviceAccountId)
 	if err != nil {
