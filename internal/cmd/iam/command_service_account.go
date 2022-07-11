b9ae053d5c453cca64ff1a5663bf0a0080ebd675
Steven Gagniere <108363707+sgagniere@users.noreply.github.com>
2022-07-11 16:31:39 -0700

    [CLI-1813] iam service-account describe command (#1355)
    * Added describe command for iam service accounts. Still needs debugging/testing
    
    * Added integration test for service-account describe command
    
    * added a newline at end of file
    
    * Arranged commands in alphabetical order in command_service_account and made new variable for command_service_account_describe
    
    * Removed unnecessary error message

--- internal/cmd/iam/command_service_account.go
+++ internal/cmd/iam/command_service_account.go
@@ -30,6 +30,7 @@ func newServiceAccountCommand(prerunner pcmd.PreRunner) *cobra.Command {
 
 	cmd.AddCommand(c.newCreateCommand())
 	cmd.AddCommand(c.newDeleteCommand())
+	cmd.AddCommand(c.newDescribeCommand())
 	cmd.AddCommand(c.newListCommand())
 	cmd.AddCommand(c.newUpdateCommand())
 
