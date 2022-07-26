f958caffa9d19854e9d7bb88d248a54685527bed
Sumit Tokkar <sumit.tokkar@gmail.com>
2022-07-26 16:23:35 -0500

    Update internal/cmd/stream-share/command_provider_share_delete.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/cmd/stream-share/command_provider_share_delete.go
+++ internal/cmd/stream-share/command_provider_share_delete.go
@@ -1,11 +1,11 @@
 package streamshare
 
 import (
+	"github.com/spf13/cobra"
+	
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/errors"
 	"github.com/confluentinc/cli/internal/pkg/examples"
-	"github.com/confluentinc/cli/internal/pkg/utils"
-	"github.com/spf13/cobra"
 )
 
 func (s *providerShareCommand) newDeleteCommand() *cobra.Command {
