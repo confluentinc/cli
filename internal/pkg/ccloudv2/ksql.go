335cbc4ad6c3b77b346b2390cb4123c0e29076fb
Tomasz Nguyen <tonguyen@confluent.io>
2022-07-27 11:40:33 +0100

    feat: use the v2 api for create and describe
    This currently ignores output formatting

--- internal/pkg/ccloudv2/ksql.go
+++ internal/pkg/ccloudv2/ksql.go
@@ -19,12 +19,27 @@ func newKsqlClient(baseURL, userAgent string, isTest bool) *ksql.APIClient {
 	return ksql.NewAPIClient(cfg)
 }
 
-
 func (c *Client) ksqlApiContext() context.Context {
 	return context.WithValue(context.Background(), ksql.ContextAccessToken, c.AuthToken)
 }
 
 func (c *Client) ListKsqlClusters(environmentId string) (ksql.KsqldbcmV2ClusterList, *_nethttp.Response, error) {
-	c.KsqlClient.ClustersKsqldbcmV2Api
 	return c.KsqlClient.ClustersKsqldbcmV2Api.ListKsqldbcmV2Clusters(c.ksqlApiContext()).Environment(environmentId).Execute()
+}
+
+func (c *Client) CreateKsqlCluster(displayName, environmentId, kafkaClusterId, credentialIdentity string, csus int32, logExcludeRows bool) (ksql.KsqldbcmV2Cluster, *_nethttp.Response, error) {
+	cluster := ksql.KsqldbcmV2Cluster{
+		Spec: &ksql.KsqldbcmV2ClusterSpec{
+			DisplayName:        &displayName,
+			Csu:                &csus,
+			KafkaCluster:       &ksql.ObjectReference{Id: kafkaClusterId},
+			CredentialIdentity: &ksql.ObjectReference{Id: credentialIdentity},
+			Environment:        &ksql.ObjectReference{Id: environmentId},
+		},
+	}
+	return c.KsqlClient.ClustersKsqldbcmV2Api.CreateKsqldbcmV2Cluster(c.ksqlApiContext()).KsqldbcmV2Cluster(cluster).Execute()
+}
+
+func (c *Client) DescribeKsqlCluster(ksqlClusterId string) (ksql.KsqldbcmV2Cluster, *_nethttp.Response, error) {
+	return c.KsqlClient.ClustersKsqldbcmV2Api.GetKsqldbcmV2Cluster(c.ksqlApiContext(), ksqlClusterId).Execute()
 }
