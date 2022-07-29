a2c7a54de3f8d19bcd18d8ed189d56f3bea0108a
Kevin Wu <kwu@confluent.io>
2022-07-29 11:07:44 -0700

    adding deprecateCmdTree unit test

--- internal/pkg/featureflags/announcements_and_deprecation_test.go
+++ internal/pkg/featureflags/announcements_and_deprecation_test.go
@@ -2,6 +2,7 @@ package featureflags
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -13,5 +14,22 @@ func TestLDResponseToMap(t *testing.T) {
 		ld[i] = ldResp[i]
 	}
 	cmdToFlagsAndMsg := LDResponseToMap(ld)
-	require.Equal(t, fmt.Sprint(cmdToFlagsAndMsg), "map[kafka cluster list :{[all] DEPRECATED} ksql app:{[] DEPRECATED}]")
+	require.Equal(t, "map[kafka cluster list :{[all] DEPRECATED} ksql app:{[] DEPRECATED}]", fmt.Sprint(cmdToFlagsAndMsg))
+}
+
+func TestDeprecateCommandTree(t *testing.T) {
+	cmds := make([]*cobra.Command, 4)
+	for i := 0; i < 4; i++ {
+		cmds[i] = &cobra.Command{
+			Short: "short",
+			Long:  "long description",
+		}
+	}
+	cmds[0].AddCommand(cmds[1], cmds[2])
+	cmds[1].AddCommand(cmds[3])
+	DeprecateCommandTree(cmds[0])
+	for _, cmd := range cmds {
+		require.Equal(t, "DEPRECATED: short", cmd.Short)
+		require.Equal(t, "DEPRECATED: long description", cmd.Long)
+	}
 }
