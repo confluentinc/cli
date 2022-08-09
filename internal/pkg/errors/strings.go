5f3aa236522460389a1c7f5d1d3fafb0d2918f53
Steven Gagniere <sgagniere@confluent.io>
2022-08-09 13:16:24 -0700

    Changed a string to use double quotes instead of backticks to match the style of the rest of the strings

--- internal/pkg/errors/strings.go
+++ internal/pkg/errors/strings.go
@@ -146,5 +146,5 @@ const (
 	UpdateSuccessMsg = "Updated the %s of %s \"%s\" to \"%s\".\n"
 
 	// Stream Sharing commands
-	DeletedProviderShareMsg = `Deleted provider share "%s".`
+	DeletedProviderShareMsg = "Deleted provider share \"%s\"."
 )
