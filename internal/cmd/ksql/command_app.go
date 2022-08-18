a0a2e24153de7085682425aa7e7963228721451c
kevin-wu24 <66326898+kevin-wu24@users.noreply.github.com>
2022-08-18 13:35:21 -0500

    [CLI-1881] Announcements and Deprecation Framework (#1370)
    * checkpoint with working feature flag
    
    * fixed launchDarkly init bug with unit tests
    
    * code review fixes
    
    * added flag deprecation
    
    * adding unit test for parsing LD response
    
    * adding deprecateCmdTree unit test
    
    * adding deprecateFlag unit test
    
    * adding prerunner logic, will uncomment later
    
    * updated flag deprecation
    
    * deprecation finished
    
    * adding announcements and printing messages before flagged commands/flags, updated int-tests
    
    * code review changes 1/2
    
    * 2/2 code review changes
    
    * code review fixes
    
    * more code review fixes
    
    * code review fixes pt3
    
    * grouping related constants
    
    * replacing warning message with deprecation framework
    
    * fixing typo in command.go
    
    * code review fixes
    
    * removing duplicate functions in ksql cmds
    
    * code review fixes
    
    * fixing bug when feature flags are empty
    
    * more code review fixes

--- internal/cmd/ksql/command_app.go
+++ internal/cmd/ksql/command_app.go
@@ -4,24 +4,24 @@ import (
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
-	"github.com/confluentinc/cli/internal/pkg/errors"
 )
+
+const app = "app"
 
 func newAppCommand(prerunner pcmd.PreRunner) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:         "app",
-		Short:       "DEPRECATED: Manage ksqlDB apps.",
-		Long:        "DEPRECATED: Manage ksqlDB apps. " + errors.KSQLAppDeprecateWarning,
+		Use:         app,
+		Short:       "Manage ksqlDB apps.",
 		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireCloudLogin},
 	}
 
 	c := &ksqlCommand{pcmd.NewAuthenticatedStateFlagCommand(cmd, prerunner)}
 
-	cmd.AddCommand(c.newConfigureAclsCommand(true))
-	cmd.AddCommand(c.newCreateCommand(true))
-	cmd.AddCommand(c.newDeleteCommand(true))
-	cmd.AddCommand(c.newDescribeCommand(true))
-	cmd.AddCommand(c.newListCommand(true))
+	cmd.AddCommand(c.newConfigureAclsCommand(app))
+	cmd.AddCommand(c.newCreateCommand(app))
+	cmd.AddCommand(c.newDeleteCommand(app))
+	cmd.AddCommand(c.newDescribeCommand(app))
+	cmd.AddCommand(c.newListCommand(app))
 
 	return cmd
 }
---
+++ internal/pkg/featureflags/announcements_and_deprecation_test.go
@@ -0,0 +1,62 @@
+package featureflags
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAnnouncementsOrDeprecation(t *testing.T) {
+	ldResp := []map[string]interface{}{
+		{"message": "DEPRECATED", "pattern": "ksql app"},
+		{"message": "DEPRECATED", "pattern": "kafka cluster list --all --context"},
+	}
+	ld := make([]interface{}, len(ldResp))
+	for i := range ldResp {
+		ld[i] = ldResp[i]
+	}
+	cmdToFlagsAndMsg := GetAnnouncementsOrDeprecation(ld)
+	expected := map[string]*FlagsAndMsg{}
+	expected["ksql app"] = &FlagsAndMsg{CmdMessage: "DEPRECATED"}
+	expected["kafka cluster list"] = &FlagsAndMsg{
+		Flags:        []string{"all", "context"},
+		FlagMessages: []string{"DEPRECATED", "DEPRECATED"},
+	}
+	for cmd, flagsAndMsg := range cmdToFlagsAndMsg {
+		require.Equal(t, expected[cmd], flagsAndMsg)
+	}
+}
+
+func TestDeprecateCommandTree(t *testing.T) {
+	cmds := dummyCmds()
+	DeprecateCommandTree(cmds[0])
+	for _, cmd := range cmds {
+		require.Equal(t, "DEPRECATED: short", cmd.Short)
+		require.Equal(t, "DEPRECATED: long description", cmd.Long)
+	}
+}
+
+func TestDeprecateFlags(t *testing.T) {
+	cmds := dummyCmds()
+	DeprecateFlags(cmds[0], []string{"flag1", "f"})
+	for _, cmd := range cmds {
+		require.Equal(t, "DEPRECATED: testing flag deprecation", cmd.Flag("flag1").Usage)
+		require.Equal(t, "DEPRECATED: testing shorthand deprecation", cmd.Flag("flag2").Usage)
+	}
+}
+
+func dummyCmds() []*cobra.Command {
+	cmds := make([]*cobra.Command, 3)
+	for i := 0; i < 3; i++ {
+		cmds[i] = &cobra.Command{
+			Short: "short",
+			Long:  "long description",
+		}
+		cmds[i].Flags().String("flag1", "true", "testing flag deprecation")
+		cmds[i].Flags().StringP("flag2", "f", "true", "testing shorthand deprecation")
+	}
+	cmds[0].AddCommand(cmds[1])
+	cmds[1].AddCommand(cmds[2])
+	return cmds
+}
