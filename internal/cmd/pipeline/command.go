a912d1ddc5ab290cb892da422b06d482758f99ac
sgaddam-confluent <sgaddam@confluent.io>
2022-09-26 10:01:52 -0500

    use temporary ccloud APIs

--- internal/cmd/pipeline/command.go
+++ internal/cmd/pipeline/command.go
@@ -6,7 +6,6 @@ import (
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	v1 "github.com/confluentinc/cli/internal/pkg/config/v1"
 	dynamicconfig "github.com/confluentinc/cli/internal/pkg/dynamic-config"
-	launchdarkly "github.com/confluentinc/cli/internal/pkg/featureflags"
 )
 
 type Pipeline struct {
@@ -14,6 +13,12 @@ type Pipeline struct {
 	Name  string `json:"name"`
 	State string `json:"state"`
 }
+
+var (
+	pipelineListFields           = []string{"Id", "Name", "State"}
+	pipelineListHumanLabels      = []string{"Id", "Name", "State"}
+	pipelineListStructuredLabels = []string{"Id", "Name", "State"}
+)
 
 type command struct {
 	*pcmd.AuthenticatedCLICommand
@@ -23,7 +28,7 @@ type command struct {
 func New(cfg *v1.Config, prerunner pcmd.PreRunner) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:         "pipeline",
-		Short:       "Manage stream designer pipelines.",
+		Short:       "Manage Stream Designer pipelines.",
 		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonAPIKeyCloudLogin},
 	}
 
@@ -42,8 +47,6 @@ func New(cfg *v1.Config, prerunner pcmd.PreRunner) *cobra.Command {
 
 	dc := dynamicconfig.New(cfg, nil, nil)
 	_ = dc.ParseFlagsIntoConfig(cmd)
-
-	c.Hidden = !cfg.IsTest && !launchdarkly.Manager.BoolVariation("cli.client_stream_designer.enable", dc.Context(), v1.CliLaunchDarklyClient, true, false)
 
 	return c.Command
 }
--- internal/cmd/pipeline/command_update.go
+++ internal/cmd/pipeline/command_update.go
@@ -1,17 +1,14 @@
 package pipeline
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"io"
-	"net/http"
-	"net/http/cookiejar"
 	"os"
 
 	"github.com/spf13/cobra"
 
+	sdv1 "github.com/confluentinc/ccloud-sdk-go-v2/stream-designer/v1"
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
+	"github.com/confluentinc/cli/internal/pkg/examples"
 	"github.com/confluentinc/cli/internal/pkg/utils"
 )
 
@@ -21,6 +18,12 @@ func (c *command) newUpdateCommand(prerunner pcmd.PreRunner) *cobra.Command {
 		Short: "Update an existing pipeline.",
 		Args:  cobra.ExactArgs(1),
 		RunE:  c.update,
+		Example: examples.BuildExampleString(
+			examples.Example{
+				Text: "Request to update a pipeline in Stream Designer with a new name, description, and source code located in current local directory",
+				Code: `confluent pipeline update pipe-12345 --name "NewPipeline" -- description "NewDescription" -- sql-file .`,
+			},
+		),
 	}
 
 	cmd.Flags().String("name", "", "New pipeline name.")
@@ -40,71 +43,31 @@ func (c *command) update(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	var client http.Client
-	jar, err := cookiejar.New(nil)
-	if err != nil {
-		return err
-	}
-
-	client = http.Client{
-		Jar: jar,
-	}
-
-	cookie := &http.Cookie{
-		Name:   "auth_token",
-		Value:  c.State.AuthToken,
-		MaxAge: 300,
-	}
-
 	if name == "" && description == "" && sqlFile == "" {
 		utils.Println(cmd, "At least one field must be specified with --name, --description, or --sql-file")
 		return nil
 	}
 
 	if name != "" || description != "" {
-		postMap := make(map[string]string)
+		updateBody := sdv1.NewSdV1PipelineUpdate()
 		if name != "" {
-			postMap["name"] = name
+			updateBody.SetName(name)
 		}
 		if description != "" {
-			postMap["description"] = description
+			updateBody.SetDescription(description)
 		}
 
-		postBody, _ := json.Marshal(postMap)
-		bytesPostBody := bytes.NewBuffer(postBody)
-
-		req, err := http.NewRequest(http.MethodPatch, fmt.Sprintf("https://devel.cpdev.cloud/api/sd/v1/environments/%s/clusters/%s/pipelines/%s", c.Context.GetCurrentEnvironmentId(), cluster.ID, args[0]), bytesPostBody)
+		// call api
+		_, resp, err := c.V2Client.UpdateSdPipeline(c.EnvironmentId(), cluster.ID, args[0], *updateBody)
 		if err != nil {
 			return err
 		}
 
-		req.AddCookie(cookie)
-		req.Header.Add("Content-Type", "application/json")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-
-		if resp.StatusCode == 200 && err == nil {
-			utils.Println(cmd, "Updated pipeline: "+args[0])
-		} else {
-			utils.Print(cmd, "Could not update pipeline code: "+args[0])
-			if err != nil {
-				return err
-			} else if body != nil {
-				utils.Print(cmd, " with error: "+string(body))
-				return nil
-			}
-		}
+		utils.Println(cmd, `Updated pipeline:  "%s"`, args[0])
 	}
 
 	if sqlFile != "" {
+		// get SQL content from filepath
 		putBody, err := os.Open(sqlFile)
 		if err != nil {
 			return err
@@ -112,43 +75,16 @@ func (c *command) update(cmd *cobra.Command, args []string) error {
 
 		defer putBody.Close()
 
-		// TODO: Modify PUT /{pipeline_id}/content API with a new @Consumes SQL file to import SQL file
-		req, err := http.NewRequest("PUT", fmt.Sprintf("https://devel.cpdev.cloud/api/sd/v1/environments/%s/clusters/%s/pipelines/%s/content", c.Context.GetCurrentEnvironmentId(), cluster.ID, args[0]), putBody)
+		// replace with PUT request body when minispec file is updated
+		updateBody := sdv1.NewSdV1PipelineUpdate()
+
+		// call PUT api when minispec file is updated
+		_, resp, err := c.V2Client.UpdateSdPipeline(c.EnvironmentId(), cluster.ID, args[0], *updateBody)
 		if err != nil {
 			return err
 		}
 
-		req.AddCookie(cookie)
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-
-		if resp.StatusCode == 200 && err == nil {
-			utils.Println(cmd, "Updated pipeline: "+args[0])
-		} else {
-			utils.Print(cmd, "Could not update pipeline code: "+args[0])
-			if err != nil {
-				return err
-			} else if body != nil {
-				var data map[string]interface{}
-				err = json.Unmarshal([]byte(string(body)), &data)
-				if err != nil {
-					return err
-				}
-				if data["title"] != "{}" {
-					utils.Println(cmd, data["title"].(string))
-				}
-				utils.Println(cmd, data["action"].(string))
-			}
-		}
+		utils.Println(cmd, `Replaced pipeline:  "%s"`, args[0])
 	}
-
 	return nil
 }
