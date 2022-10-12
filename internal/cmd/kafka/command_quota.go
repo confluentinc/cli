d0c94551a3b902535190f3ae61db4c61582ff601
Miles Todzo <33913881+mtodzo@users.noreply.github.com>
2022-10-12 16:19:55 -0700

    Client quotas GA changes (#1462)
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/cmd/kafka/command_quota.go
+++ internal/cmd/kafka/command_quota.go
@@ -46,13 +46,13 @@ type printableQuota struct {
 func quotaToPrintable(quota kafkaquotas.KafkaQuotasV1ClientQuota, format string) *printableQuota {
 	s := printableQuota{
 		Id:          *quota.Id,
-		DisplayName: *quota.DisplayName,
-		Description: *quota.Description,
-		Ingress:     *quota.Throughput.IngressByteRate,
-		Egress:      *quota.Throughput.EgressByteRate,
-		Principals:  principalsToString(*quota.Principals),
-		Cluster:     quota.Cluster.Id,
-		Environment: quota.Environment.Id,
+		DisplayName: quota.Spec.GetDisplayName(),
+		Description: quota.Spec.GetDescription(),
+		Ingress:     quota.Spec.Throughput.GetIngressByteRate(),
+		Egress:      quota.Spec.Throughput.GetEgressByteRate(),
+		Principals:  principalsToString(*quota.Spec.Principals),
+		Cluster:     quota.Spec.Cluster.GetId(),
+		Environment: quota.Spec.Environment.GetId(),
 	}
 	if format == output.Human.String() {
 		s.Ingress = s.Ingress + " B/s"
@@ -61,7 +61,7 @@ func quotaToPrintable(quota kafkaquotas.KafkaQuotasV1ClientQuota, format string)
 	return &s
 }
 
-func principalsToString(principals []kafkaquotas.ObjectReference) string {
+func principalsToString(principals []kafkaquotas.GlobalObjectReference) string {
 	principalStr := ""
 	for i, principal := range principals {
 		principalStr += principal.Id
--- internal/cmd/kafka/command_quota_update.go
+++ internal/cmd/kafka/command_quota_update.go
@@ -2,12 +2,12 @@ package kafka
 
 import (
 	kafkaquotas "github.com/confluentinc/ccloud-sdk-go-v2/kafka-quotas/v1"
-	"github.com/confluentinc/cli/internal/pkg/set"
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/examples"
 	"github.com/confluentinc/cli/internal/pkg/output"
+	"github.com/confluentinc/cli/internal/pkg/set"
 )
 
 func (c *quotaCommand) newUpdateCommand() *cobra.Command {
@@ -42,28 +42,30 @@ func (c *quotaCommand) update(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	updateName, err := getUpdatedName(cmd, *quota.DisplayName)
+	updateName, err := getUpdatedName(cmd, quota.Spec.GetDisplayName())
 	if err != nil {
 		return err
 	}
-	updateDescription, err := getUpdatedDescription(cmd, *quota.Description)
+	updateDescription, err := getUpdatedDescription(cmd, *quota.Spec.Description)
 	if err != nil {
 		return err
 	}
-	updateThroughput, err := getUpdatedThroughput(cmd, quota.Throughput)
+	updateThroughput, err := getUpdatedThroughput(cmd, quota.Spec.Throughput)
 	if err != nil {
 		return err
 	}
-	updatePrincipals, err := c.getUpdatedPrincipals(cmd, *quota.Principals)
+	updatePrincipals, err := c.getUpdatedPrincipals(cmd, *quota.Spec.Principals)
 	if err != nil {
 		return err
 	}
 	quotaUpdate := kafkaquotas.KafkaQuotasV1ClientQuotaUpdate{
-		Id:          &quotaId,
-		DisplayName: &updateName,
-		Description: &updateDescription,
-		Throughput:  updateThroughput,
-		Principals:  updatePrincipals,
+		Id: &quotaId,
+		Spec: &kafkaquotas.KafkaQuotasV1ClientQuotaSpecUpdate{
+			DisplayName: &updateName,
+			Description: &updateDescription,
+			Throughput:  updateThroughput,
+			Principals:  updatePrincipals,
+		},
 	}
 	updatedQuota, err := c.V2Client.UpdateKafkaQuota(quotaUpdate)
 	if err != nil {
@@ -74,7 +76,7 @@ func (c *quotaCommand) update(cmd *cobra.Command, args []string) error {
 	return output.DescribeObject(cmd, printQuota, quotaListFields, humanRenames, structuredRenames)
 }
 
-func (c *quotaCommand) getUpdatedPrincipals(cmd *cobra.Command, updatePrincipals []kafkaquotas.ObjectReference) (*[]kafkaquotas.ObjectReference, error) {
+func (c *quotaCommand) getUpdatedPrincipals(cmd *cobra.Command, updatePrincipals []kafkaquotas.GlobalObjectReference) (*[]kafkaquotas.GlobalObjectReference, error) {
 	if cmd.Flags().Changed("add-principals") {
 		serviceAccountsToAdd, err := cmd.Flags().GetStringSlice("add-principals")
 		if err != nil {
