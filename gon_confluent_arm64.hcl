1551e0bd32fb12067827bd43df421b81707be6a3
Steven Gagniere <sgagniere@confluent.io>
2022-10-13 12:29:34 -0700

    created scripts directory and moved gon files & scripts inside

--- gon_confluent_arm64.hcl
+++
@@ -1,9 +0,0 @@
-source = ["path/to/file"]
-bundle_id = "io.confluent.cli.confluent"
-
-apple_id {
-}
-
-sign {
-  application_identity = "Developer ID Application: Confluent, Inc."
-}
--- scripts/gon_filepath_editor.sh
+++ scripts/gon_filepath_editor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run sed inside this script to get around difficulties in running it directly inside a post hook
-sed "s|path/to/file|$1|" $2 > dist/$2
+sed "s|path/to/file|$1|" scripts/gon_confluent.hcl > dist/gon_confluent_$2.hcl
