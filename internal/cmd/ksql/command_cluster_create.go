f4f5db562cddda3d02f28b5d1d346dea1ed287f2
Martin Tang <matang@confluent.io>
2022-11-03 13:46:32 +0000

    Space out ksql cluster polling by 100ms

--- internal/cmd/ksql/command_cluster_create.go
+++ internal/cmd/ksql/command_cluster_create.go
@@ -2,11 +2,13 @@ package ksql
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/errors"
+	"github.com/confluentinc/cli/internal/pkg/log"
 	"github.com/confluentinc/cli/internal/pkg/output"
 	"github.com/confluentinc/cli/internal/pkg/utils"
 )
@@ -18,7 +20,7 @@ func (c *ksqlCommand) newCreateCommand(resource string) *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE:  c.create,
 	}
-	
+
 	cmd.Flags().String("credential-identity", "", `User account ID or service account ID to be associated with this cluster. We will create an API key associated with this identity and use it to authenticate the ksqlDB cluster with Kafka.`)
 	cmd.Flags().Int32("csu", 4, "Number of CSUs to use in the cluster.")
 	cmd.Flags().Bool("log-exclude-rows", false, "Exclude row data in the processing log.")
@@ -61,16 +63,21 @@ func (c *ksqlCommand) create(cmd *cobra.Command, args []string) error {
 	// endpoint value filled later, loop until endpoint information is not null (usually just one describe call is enough)
 	endpoint := cluster.Status.GetHttpEndpoint()
 
+	log.CliLogger.Trace("Polling ksqlDB cluster")
+
+	ticker := time.NewTicker(100 * time.Millisecond)
 	// use count to prevent the command from hanging too long waiting for the endpoint value
-	count := 0
-	for endpoint == "" && count < 3 {
+	for count := 0; endpoint == "" && count < 3; count++ {
+		if count != 0 {
+			<-ticker.C
+		}
 		res, err := c.V2Client.DescribeKsqlCluster(*cluster.Id, c.EnvironmentId())
 		if err != nil {
 			return err
 		}
 		endpoint = res.Status.GetHttpEndpoint()
-		count++
 	}
+	ticker.Stop()
 	if endpoint == "" {
 		utils.ErrPrintln(cmd, errors.EndPointNotPopulatedMsg)
 	}
