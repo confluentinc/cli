614faa6e2b2a1fa09e77cec0d0b3d273d06c80a6
Lucy Fan <lfan@confluent.io>
2022-12-07 15:58:45 -0800

    Only have KSQL behind RBAC Stage 3 flag

--- internal/cmd/iam/command_rbac_role_list.go
+++ internal/cmd/iam/command_rbac_role_list.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/antihax/optional"
 	"github.com/confluentinc/go-printer"
-	"github.com/confluentinc/mds-sdk-go/mdsv2alpha1"
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
@@ -39,26 +38,21 @@ func (c *roleCommand) list(cmd *cobra.Command, _ []string) error {
 }
 
 func (c *roleCommand) ccloudList(cmd *cobra.Command) error {
-	var roles []mdsv2alpha1.Role
-
-	// add public and dataplane roles
-	publicAndDataplaneNamespace := []string{publicNamespace.Value(), dataplaneNamespace.Value()}
-	publicAndDataplaneNamespaceOpt := optional.NewString(strings.Join(publicAndDataplaneNamespace, ","))
-	publicAndDataplaneRoles, err := c.namespaceRoles(publicAndDataplaneNamespaceOpt)
+	// add public dataplane and datagovernance roles
+	namespaces := []string{publicNamespace.Value(), dataplaneNamespace.Value(), dataGovernanceNamespace.Value()}
+	opt := optional.NewString(strings.Join(namespaces, ","))
+	roles, err := c.namespaceRoles(opt)
 	if err != nil {
 		return err
 	}
-	roles = append(roles, publicAndDataplaneRoles...)
 
-	// add ksql and datagovernance roles
+	// add ksql roles
 	if os.Getenv("XX_DATAPLANE_3_ENABLE") != "" {
-		ksqlAndDataGovernanceNamespace := []string{ksqlNamespace.Value(), dataGovernanceNamespace.Value()}
-		ksqlAndDataGovernanceNamespaceOpt := optional.NewString(strings.Join(ksqlAndDataGovernanceNamespace, ","))
-		ksqlAndDataGovernanceRoles, err := c.namespaceRoles(ksqlAndDataGovernanceNamespaceOpt)
+		ksqlRoles, err := c.namespaceRoles(ksqlNamespace)
 		if err != nil {
 			return err
 		}
-		roles = append(roles, ksqlAndDataGovernanceRoles...)
+		roles = append(roles, ksqlRoles...)
 	}
 
 	format, err := cmd.Flags().GetString(output.FlagName)
