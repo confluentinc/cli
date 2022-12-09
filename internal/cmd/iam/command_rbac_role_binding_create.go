3a5dd7b0e7d7f62b0de280355dee9dcdf7893e65
Victoria Bialas <londoncalling@users.noreply.github.com>
2022-12-09 11:11:20 -0800

    Update internal/cmd/iam/command_rbac_role_binding_create.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/cmd/iam/command_rbac_role_binding_create.go
+++ internal/cmd/iam/command_rbac_role_binding_create.go
@@ -34,7 +34,7 @@ func (c *roleBindingCommand) newCreateCommand() *cobra.Command {
 				Code: "confluent iam rbac role-binding create --principal User:sa-123456 --role MetricsViewer",
 			},
 			examples.Example{
-				Text: `Grant the role "ResourceOwner" to principal "User:u-123456" to all subjects for the |sr| “lsrc-123456” in the environment “env-12345”:`,
+				Text: `Grant the "ResourceOwner" role to principal "User:u-123456" and all subjects for Schema Registry cluster "lsrc-123456" in environment "env-12345":`,
 				Code: "confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource Subject:*",
 			},
 			examples.Example{
