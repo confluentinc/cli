0e197bd6798eeffe11baaa4722ea1a5d9e9ac605
Brian Strauch <bstrauch@confluent.io>
2022-12-13 14:47:34 -0800

    debug

--- test/smoke/login-basic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Test that Confluent Cloud login works in a production environment with basic (username and password) credentials.
-# Usage: bash login-basic.sh <email> <password>
-
-echo -e "$1\n$2\n" | HOME=$(mktemp -d) $(find dist/ -name confluent) login
