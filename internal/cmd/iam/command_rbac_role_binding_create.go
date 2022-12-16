96cea01078800a44a52c8947349c5a594ab63668
Panagiotis Garefalakis <pgarefalakis@confluent.io>
2022-12-15 21:43:51 -0800

    Update command_rbac_role_binding_create.go

--- internal/cmd/iam/command_rbac_role_binding_create.go
+++ internal/cmd/iam/command_rbac_role_binding_create.go
@@ -39,15 +39,15 @@ func (c *roleBindingCommand) newCreateCommand() *cobra.Command {
 			},
 			examples.Example{
 				Text: `Grant the "ResourceOwner" role to principal "User:u-123456" and subject "test" for the Schema Registry cluster "lsrc-123456" in the environment "env-12345":`,
-				Code: "confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource \"Subject:test\"",
+				Code: `confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource "Subject:test"`,
 			},
 			examples.Example{
 				Text: `Grant the "ResourceOwner" role to principal "User:u-123456" and all subjects in schema context "schema_context" for Schema Registry cluster "lsrc-123456" in the environment "env-12345":`,
-				Code: "confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource \"Subject::.schema_context:*\"",
+				Code: `confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource "Subject::.schema_context:*"`,
 			},
 			examples.Example{
 				Text: `Grant the "ResourceOwner" role to principal "User:u-123456" and subject "test" in schema context "schema_context" for Schema Registry "lsrc-123456" in the environment "env-12345":`,
-				Code: "confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource \"Subject::.schema_context:test\"",
+				Code: `confluent iam rbac role-binding create --principal User:u-123456 --role ResourceOwner --environment env-12345 --schema-registry-cluster-id lsrc-123456 --resource "Subject::.schema_context:test"`,
 			},
 		)
 	} else {
