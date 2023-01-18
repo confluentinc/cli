d10ec183786d092fce292f3d663c6474a8f2166d
tikimims <39631000+tikimims@users.noreply.github.com>
2023-01-18 13:11:10 -0500

    Update internal/cmd/kafka/command_acl_delete_onprem.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/cmd/kafka/command_acl_delete_onprem.go
+++ internal/cmd/kafka/command_acl_delete_onprem.go
@@ -17,7 +17,7 @@ func (c *aclCommand) newDeleteCommandOnPrem() *cobra.Command {
 		RunE:  c.deleteOnPrem,
 		Example: examples.BuildExampleString(
 			examples.Example{
-				Text: `Delete all READ access ACLs for the specified user (providing embedded Kafka REST Proxy endpoint).`,
+				Text: "Delete all READ access ACLs for the specified user (providing embedded Kafka REST Proxy endpoint).",
 				Code: `confluent kafka acl delete --url http://localhost:8090/kafka --operation READ --allow --topic Test --principal User:Jane --host "*"`,
 			},
 			examples.Example{
