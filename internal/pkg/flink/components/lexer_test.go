7a61500fb499adf648534ef6b5d3b17627f008da
Gustavo de Morais <gdemorais@confluent.io>
2023-02-13 14:57:31 +0100

    test: add failing tests for lexer

---
+++ internal/pkg/flink/components/lexer_test.go
@@ -0,0 +1,46 @@
+package components
+
+import (
+	"testing"
+
+	prompt "github.com/c-bata/go-prompt"
+)
+
+// property based testing?
+func TextLexer(t *testing.T) {
+	// given
+	line := "SELECT FIELD FROM TABLE;"
+
+	// when
+	elements := lexer(line)
+
+	// then
+	for i, element := range elements {
+		if i > 0 && i < 6 {
+			if element.Color != HIGHLIGHT_COLOR {
+				t.Errorf("lexer() = %d, want %d", element.Color, HIGHLIGHT_COLOR)
+			}
+		}
+	}
+}
+
+func TextWordLexer(t *testing.T) {
+	// given
+	line := "SELECT FIELD FROM TABLE WHERE FIELD = 2;"
+
+	// when
+	elements := wordLexer(line)
+
+	// then
+	for _, element := range elements {
+
+		_, isKeyWord := sqlKeywords[element.Text]
+
+		if isKeyWord && element.Color != HIGHLIGHT_COLOR {
+			t.Errorf("lexer() = %q, want %q", element.Color, HIGHLIGHT_COLOR)
+		} else if !isKeyWord && element.Color != prompt.White {
+			t.Errorf("lexer() = %q, want %q", element.Color, HIGHLIGHT_COLOR)
+		}
+
+	}
+}
