98f51f4b1f2fa4050e91bb7f15c098debdec0cb9
Steven Gagniere <sgagniere@confluent.io>
2023-02-14 16:47:24 -0800

    update comment

--- internal/pkg/form/form.go
+++ internal/pkg/form/form.go
@@ -114,8 +114,8 @@ func ConfirmDeletion(cmd *cobra.Command, promptMsg, stringToType string) (bool, 
 }
 
 func ConfirmEnter(cmd *cobra.Command) error {
-	// Note: this function does not check against any regex;
-	// any string is acceptable
+	// This function prevents echoing of user input instead of displaying text or *'s
+	// so that the CLI will appear to wait until 'enter' or 'Ctrl-C' are entered.
 	utils.Print(cmd, "Press enter to continue or Ctrl-C to cancel:")
 
 	if _, err := term.ReadPassword(int(os.Stdin.Fd())); err != nil {
