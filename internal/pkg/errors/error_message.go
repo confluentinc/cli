614f868ebcbd657205393f83417a8f165d607e1e
MuweiHe <83249359+MuweiHe@users.noreply.github.com>
2023-03-01 13:27:40 -0800

    Update internal/pkg/errors/error_message.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/pkg/errors/error_message.go
+++ internal/pkg/errors/error_message.go
@@ -145,7 +145,7 @@ const (
 	CloudRegionNotAvailableSuggestions               = "To view a list of available regions for \"%s\", use `confluent kafka region list --cloud %s`."
 	CloudProviderNotAvailableErrorMsg                = `"%s" is not an available cloud provider`
 	CloudProviderNotAvailableSuggestions             = "To view a list of available cloud providers and regions, use `confluent kafka region list`."
-	TopicDoesNotExistOrMissingPermissionsErrorMsg    = `topic "%s" does not exist or you do not have the ACLs or role bindings required to describe it`
+	TopicDoesNotExistOrMissingPermissionsErrorMsg    = `topic "%s" does not exist or user does not have the ACLs or role bindings required to describe it`
 	TopicDoesNotExistOrMissingPermissionsSuggestions = "To list topics for Kafka cluster \"%s\", use `confluent kafka topic list --cluster %s`.\nTo list ACLs use `confluent kafka acl list --cluster %s`.\nTo list role bindings use `confluent iam rbac role-binding list`."
 	InvalidAvailableFlagErrorMsg                     = "invalid value \"%s\" for `--availability` flag"
 	InvalidAvailableFlagSuggestions                  = "Allowed values for `--availability` flag are: %s, %s."
