e0651950ba9a3a521b14ba4e2d7b44710c8a3603
Steven Gagniere <sgagniere@confluent.io>
2023-03-02 18:29:43 -0800

    Remove build-arg

--- scripts/build_linux_glibc.sh
+++ scripts/build_linux_glibc.sh
@@ -5,7 +5,7 @@ aws ecr get-login-password --region us-west-1 | docker login --username AWS --pa
 go mod vendor
 
 # Build linux/amd64
-docker build . -f ./dockerfiles/Dockerfile_linux_glibc -t cli-linux-glibc-builder-image --build-arg GO_VERSION=$(cat .go-version)
+docker build . -f ./dockerfiles/Dockerfile_linux_glibc -t cli-linux-glibc-builder-image
 docker container create --name cli-linux-glibc-builder cli-linux-glibc-builder-image
 docker container cp cli-linux-glibc-builder:/go/src/github.com/confluentinc/cli/dist/. ./dist/
 docker container rm cli-linux-glibc-builder
@@ -13,9 +13,9 @@ docker container rm cli-linux-glibc-builder
 # Build linux/arm64
 architecture=$(uname -m)
 if [ "$architecture" == 'x86_64' ]; then
-  docker build . -f ./dockerfiles/Dockerfile_linux_glibc_arm64_from_amd64 -t cli-linux-glibc-arm64-builder-image  --build-arg GO_VERSION=$(cat .go-version)
+  docker build . -f ./dockerfiles/Dockerfile_linux_glibc_arm64_from_amd64 -t cli-linux-glibc-arm64-builder-image
 else
-  docker build . -f ./dockerfiles/Dockerfile_linux_glibc_arm64 -t cli-linux-glibc-arm64-builder-image  --build-arg GO_VERSION=$(cat .go-version)
+  docker build . -f ./dockerfiles/Dockerfile_linux_glibc_arm64 -t cli-linux-glibc-arm64-builder-image
 fi
 docker container create --name cli-linux-glibc-arm64-builder cli-linux-glibc-arm64-builder-image
 docker container cp cli-linux-glibc-arm64-builder:/go/src/github.com/confluentinc/cli/dist/. ./dist/
