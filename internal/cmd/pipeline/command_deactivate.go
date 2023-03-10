7cad86e6fd65d5db9716e48cf1fb1ae17ed9fecd
Raghavendra Guru <rguru@confluent.io>
2023-03-09 18:02:47 -0800

    Making SR id and KSQL as optional for pipeline create command

--- internal/cmd/pipeline/command_deactivate.go
+++ internal/cmd/pipeline/command_deactivate.go
@@ -38,12 +38,10 @@ func (c *command) deactivate(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	updatePipeline := streamdesignerv1.SdV1PipelineUpdate{
-		Spec: &streamdesignerv1.SdV1PipelineSpecUpdate{
-			Activated:          streamdesignerv1.PtrBool(false),
-			RetainedTopicNames: &retainedTopics,
-		},
-	}
+	updatePipeline := streamdesignerv1.SdV1Pipeline{Spec: &streamdesignerv1.SdV1PipelineSpec{
+		Activated:          streamdesignerv1.PtrBool(false),
+		RetainedTopicNames: &retainedTopics,
+	}}
 
 	pipeline, err := c.V2Client.UpdateSdPipeline(c.EnvironmentId(), cluster.ID, args[0], updatePipeline)
 	if err != nil {
