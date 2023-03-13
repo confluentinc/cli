0dc5a36380d930279e4e294df46671e22f5555d8
Steven Gagniere <sgagniere@confluent.io>
2023-03-13 12:58:46 -0700

    add multiple delete for users

--- internal/cmd/iam/command_user_delete.go
+++ internal/cmd/iam/command_user_delete.go
@@ -1,22 +1,25 @@
 package iam
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
-	"github.com/confluentinc/cli/internal/pkg/errors"
+	perrors "github.com/confluentinc/cli/internal/pkg/errors"
 	"github.com/confluentinc/cli/internal/pkg/form"
 	"github.com/confluentinc/cli/internal/pkg/output"
 	"github.com/confluentinc/cli/internal/pkg/resource"
+	"github.com/confluentinc/cli/internal/pkg/types"
+	"github.com/confluentinc/cli/internal/pkg/utils"
 )
 
-func (c userCommand) newDeleteCommand() *cobra.Command {
+func (c *userCommand) newDeleteCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "delete <id>",
-		Short: "Delete a user from your organization.",
-		Args:  cobra.ExactArgs(1),
+		Use:   "delete <id-1> [id-2] ... [id-N]",
+		Short: "Delete users from your organization.",
+		Args:  cobra.MinimumNArgs(1),
 		RunE:  c.delete,
 	}
 
@@ -25,26 +28,63 @@ func (c userCommand) newDeleteCommand() *cobra.Command {
 	return cmd
 }
 
-func (c userCommand) delete(cmd *cobra.Command, args []string) error {
-	resourceId := args[0]
-	if resource.LookupType(resourceId) != resource.User {
-		return fmt.Errorf(errors.BadResourceIDErrorMsg, resource.UserPrefix)
+func (c *userCommand) delete(cmd *cobra.Command, args []string) error {
+	var errs error
+	for _, resourceId := range args {
+		if resource.LookupType(resourceId) != resource.User {
+			errs = errors.Join(errs, perrors.Errorf(perrors.BadResourceIDErrorMsg, resource.UserPrefix))
+		}
+	}
+	if errs != nil {
+		return errs
 	}
 
-	user, err := c.V2Client.GetIamUserById(resourceId)
+	fullName, err := c.checkExistence(cmd, args)
 	if err != nil {
 		return err
 	}
 
-	promptMsg := fmt.Sprintf(errors.DeleteResourceConfirmMsg, resource.User, resourceId, user.GetFullName())
-	if _, err := form.ConfirmDeletion(cmd, promptMsg, user.GetFullName()); err != nil {
+	if _, err := form.ConfirmDeletionType(cmd, resource.User, fullName, args); err != nil {
 		return err
 	}
 
-	if err := c.V2Client.DeleteIamUser(resourceId); err != nil {
-		return errors.Errorf(errors.DeleteResourceErrorMsg, resource.User, resourceId, err)
+	errs = nil
+	for _, resourceId := range args {
+		if err := c.V2Client.DeleteIamUser(resourceId); err != nil {
+			errs = errors.Join(errs, perrors.Errorf(perrors.DeleteResourceErrorMsg, resource.User, resourceId, err))
+		} else {
+			output.Printf(perrors.DeletedResourceMsg, resource.User, resourceId)
+		}
 	}
 
-	output.Printf(errors.DeletedResourceMsg, resource.User, resourceId)
-	return nil
+	return errs
+}
+
+func (c *userCommand) checkExistence(cmd *cobra.Command, args []string) (string, error) {
+	// Single
+	if len(args) == 1 {
+		if user, err := c.V2Client.GetIamUserById(args[0]); err != nil {
+			return "", err
+		} else {
+			return user.GetFullName(), nil
+		}
+	}
+
+	// Multiple
+	users, err := c.V2Client.ListIamUsers()
+	if err != nil {
+		return "", err
+	}
+
+	userSet := types.NewSet()
+	for _, user := range users {
+		userSet.Add(user.GetId())
+	}
+
+	invalidUsers := userSet.Difference(args)
+	if len(invalidUsers) > 0 {
+		return "", perrors.New(fmt.Sprintf(perrors.AccessForbiddenErrorMsg, resource.User, utils.ArrayToCommaDelimitedStringWithAnd(invalidUsers)))
+	}
+
+	return "", nil
 }
--- test/iam_test.go
+++ test/iam_test.go
@@ -221,6 +221,8 @@ func (s *CLITestSuite) TestIAMUserDescribe() {
 func (s *CLITestSuite) TestIAMUserDelete() {
 	tests := []CLITest{
 		{args: "iam user delete u-2 --force", fixture: "iam/user/delete.golden"},
+		{args: "iam user delete u-11aaa u-11bbb", fixture: "iam/user/delete-multiple-fail.golden", exitCode: 1},
+		{args: "iam user delete u-11aaa u-22bbb", input: "y\n", fixture: "iam/user/delete-multiple-success.golden"},
 		{args: "iam user delete u-2", input: "Bono\n", fixture: "iam/user/delete-prompt.golden"},
 		{args: "iam user delete 0 --force", fixture: "iam/user/bad-resource-id.golden", exitCode: 1},
 		{args: "iam user delete u-1 --force", fixture: "iam/user/delete-dne.golden", exitCode: 1},
