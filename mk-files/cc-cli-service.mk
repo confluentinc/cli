2cb100faf856b345d390a547ace62e89e29c8edb
Brian Strauch <bstrauch@confluent.io>
2023-03-17 12:23:15 -0700

    Fix regex in `make promote` (#1798)

--- mk-files/cc-cli-service.mk
+++ mk-files/cc-cli-service.mk
@@ -31,7 +31,7 @@ promote:
 	cd $(CC_CLI_SERVICE) && \
 	export VAULT_ADDR=https://vault.cireops.gcp.internal.confluent.cloud && \
 	vault login -method=oidc -path=okta && \
-	halctl --context prod --vault-oidc-role halyard-prod --vault-token "$$(cat ~/.vault-token)" --vault-login-path "auth/app/prod/login" release service environment version list cc-cli-service stag | sed -n 's/\*.*\(0.[0-9]*.0\).*/\1/p' > $(DIR)/stag.txt && \
+	halctl --context prod --vault-oidc-role halyard-prod --vault-token "$$(cat ~/.vault-token)" --vault-login-path "auth/app/prod/login" release service environment version list cc-cli-service stag | sed -n 's/\*.*\(0\.[0-9]*\.0\).*/\1/p' > $(DIR)/stag.txt && \
 	git checkout -b promote-$$(cat $(DIR)/stag.txt) && \
 	for env in devel prod; do \
 		halctl --context prod --vault-oidc-role halyard-prod --vault-token "$$(cat ~/.vault-token)" --vault-login-path "auth/app/prod/login" release service environment version list cc-cli-service $${env} | grep $$(cat $(DIR)/stag.txt) | grep -o -E '[0-9]+' | head -1 > $(DIR)/$${env}.txt; \
