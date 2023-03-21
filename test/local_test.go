c132e826b3a7df4113044e1830cc02d194dd070e
MuweiHe <mhe@confluent.io>
2023-03-21 12:15:00 -0700

    fix unit and integration tests

--- test/local_test.go
+++ test/local_test.go
@@ -2,7 +2,9 @@ package test
 
 func (s *CLITestSuite) TestLocal() {
 	tests := []CLITest{
+		{args: "local kafka stop", fixture: "local/stop.golden"},
 		{args: "local kafka start", fixture: "local/start.golden", regex: true},
+
 		{args: "local kafka stop", fixture: "local/stop.golden"},
 	}
 
