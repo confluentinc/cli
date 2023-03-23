3fc90b15e827aed2fc1a59ed7ab0654aba722225
Brian Strauch <bstrauch@confluent.io>
2023-03-22 20:38:48 -0700

    Globally set `Content-Type` header in integration tests (#1792)

--- test/test-server/ccloud_router.go
+++ test/test-server/ccloud_router.go
@@ -9,63 +9,44 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// cloud urls
-const (
-	sessions            = "/api/sessions"
-	me                  = "/api/me"
-	loginRealm          = "/api/login/realm"
-	account             = "/api/accounts/{id}"
-	accounts            = "/api/accounts"
-	envMetadata         = "/api/env_metadata"
-	serviceAccounts     = "/api/service_accounts"
-	serviceAccount      = "/api/service_accounts/{id}"
-	schemaRegistries    = "/api/schema_registries"
-	schemaRegistry      = "/api/schema_registries/{id}"
-	priceTable          = "/api/organizations/{id}/price_table"
-	paymentInfo         = "/api/organizations/{id}/payment_info"
-	promoCodeClaims     = "/api/organizations/{id}/promo_code_claims"
-	users               = "/api/users"
-	v2alphaAuthenticate = "/api/metadata/security/v2alpha1/authenticate"
-	signup              = "/api/signup"
-	launchDarklyProxy   = "/ldapi/sdk/eval/{env}/users/{user:[a-zA-Z0-9=\\-\\/]+}"
-	externalIdentities  = "/api/external_identities"
-	freeTrialInfo       = "/api/growth/v1/free-trial-info"
-)
+var ccloudHandlers = []route{
+	{"/api/accounts", handleEnvironments},
+	{"/api/accounts/{id}", handleEnvironment},
+	{"/api/env_metadata", handleEnvMetadata},
+	{"/api/external_identities", handleExternalIdentities},
+	{"/api/growth/v1/free-trial-info", handleFreeTrialInfo},
+	{"/api/login/realm", handleLoginRealm},
+	{"/api/metadata/security/v2alpha1/authenticate", handleV2Authenticate},
+	{"/api/organizations/{id}/payment_info", handlePaymentInfo},
+	{"/api/organizations/{id}/price_table", handlePriceTable},
+	{"/api/organizations/{id}/promo_code_claims", handlePromoCodeClaims},
+	{"/api/schema_registries/{id}", handleSchemaRegistry},
+	{"/api/service_accounts", handleServiceAccounts},
+	{"/api/service_accounts/{id}", handleServiceAccount},
+	{"/api/sessions", handleLogin},
+	{"/api/users", handleUsers},
+	{"/ldapi/sdk/eval/{env}/users/{user:[a-zA-Z0-9=\\-\\/]+}", handleLaunchDarkly},
+}
 
 type CloudRouter struct {
 	*mux.Router
-	srApiUrl   string
-	kafkaRPUrl string
+	srApiUrl string
 }
 
 // New CloudRouter with all cloud handlers
 func NewCloudRouter(t *testing.T, isAuditLogEnabled bool) *CloudRouter {
-	c := &CloudRouter{Router: mux.NewRouter()}
-	c.buildCcloudRouter(t, isAuditLogEnabled)
-	return c
-}
+	router := &CloudRouter{Router: mux.NewRouter()}
+	router.Use(defaultHeaderMiddleware)
 
-// Add handlers for cloud endpoints
-func (c *CloudRouter) buildCcloudRouter(t *testing.T, isAuditLogEnabled bool) {
-	c.HandleFunc(sessions, handleLogin(t))
-	c.HandleFunc(me, c.HandleMe(t, isAuditLogEnabled))
-	c.HandleFunc(loginRealm, handleLoginRealm(t))
-	c.HandleFunc(signup, c.HandleSignup(t))
-	c.HandleFunc(envMetadata, c.HandleEnvMetadata(t))
-	c.HandleFunc(launchDarklyProxy, c.HandleLaunchDarkly(t))
-	c.HandleFunc(externalIdentities, handleExternalIdentities(t))
-	c.addSchemaRegistryRoutes(t)
-	c.addEnvironmentRoutes(t)
-	c.addOrgRoutes(t)
-	c.addUserRoutes(t)
-	c.addV2AlphaRoutes(t)
-	c.addServiceAccountRoutes(t)
-	c.addGrowthRoutes(t)
-}
+	for _, route := range ccloudHandlers {
+		router.HandleFunc(route.path, route.handler(t))
+	}
 
-func (c CloudRouter) addV2AlphaRoutes(t *testing.T) {
-	c.HandleFunc(v2alphaAuthenticate, c.HandleV2Authenticate(t))
-	c.addRoutesAndReplies(t, v2Base, v2RoutesAndReplies)
+	router.HandleFunc("/api/me", handleMe(t, isAuditLogEnabled))
+	router.HandleFunc("/api/schema_registries", router.HandleSchemaRegistries(t))
+	router.addRoutesAndReplies(t, "/api/metadata/security/v2alpha1/roles", v2RoutesAndReplies)
+
+	return router
 }
 
 func (c CloudRouter) addRoutesAndReplies(t *testing.T, base string, routesAndReplies map[string]string) {
@@ -75,40 +56,10 @@ func (c CloudRouter) addRoutesAndReplies(t *testing.T, base string, routesAndRep
 	for route, reply := range routesAndReplies {
 		s := reply
 		c.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/json")
 			_, err := io.WriteString(w, s)
 			require.NoError(t, err)
 		})
 	}
 
 	c.HandleFunc(base, c.HandleAllRolesRoute(t))
-}
-
-func (c *CloudRouter) addSchemaRegistryRoutes(t *testing.T) {
-	c.HandleFunc(schemaRegistries, c.HandleSchemaRegistries(t))
-	c.HandleFunc(schemaRegistry, c.HandleSchemaRegistry(t))
-}
-
-func (c *CloudRouter) addUserRoutes(t *testing.T) {
-	c.HandleFunc(users, c.HandleUsers(t))
-}
-
-func (c *CloudRouter) addOrgRoutes(t *testing.T) {
-	c.HandleFunc(priceTable, c.HandlePriceTable(t))
-	c.HandleFunc(paymentInfo, c.HandlePaymentInfo(t))
-	c.HandleFunc(promoCodeClaims, c.HandlePromoCodeClaims(t))
-}
-
-func (c *CloudRouter) addEnvironmentRoutes(t *testing.T) {
-	c.HandleFunc(accounts, c.HandleEnvironments(t))
-	c.HandleFunc(account, c.HandleEnvironment(t))
-}
-
-func (c *CloudRouter) addServiceAccountRoutes(t *testing.T) {
-	c.HandleFunc(serviceAccounts, c.HandleServiceAccounts(t))
-	c.HandleFunc(serviceAccount, c.HandleServiceAccount(t))
-}
-
-func (c *CloudRouter) addGrowthRoutes(t *testing.T) {
-	c.HandleFunc(freeTrialInfo, c.HandleFreeTrialInfo(t))
 }
