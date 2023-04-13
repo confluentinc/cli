4e16121e35555039eb1c081c3370e41930e8d891
Brian Strauch <bstrauch@confluent.io>
2023-04-13 09:29:11 -0700

    Fix CCloud environment lookup for SSO login (#1855)

---
+++ internal/pkg/auth/sso/auth0_utils_test.go
@@ -0,0 +1,29 @@
+package sso
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	testserver "github.com/confluentinc/cli/test/test-server"
+)
+
+func TestGetCCloudEnvFromBaseUrl(t *testing.T) {
+	for baseUrl, want := range map[string]string{
+		"":                                "prod",
+		":no-scheme-error.com":            "prod",
+		"confluent.cloud":                 "prod",
+		"default-to-prod.com":             "prod",
+		"https://confluent.cloud":         "prod",
+		"https://confluent.cloud/":        "prod",
+		"https://devel.cpdev.cloud":       "devel",
+		"https://devel.cpdev.cloud/":      "devel",
+		"https://prefix.priv.cpdev.cloud": "cpd",
+		"https://stag.cpdev.cloud":        "stag",
+		"https://stag.cpdev.cloud/":       "stag",
+		testserver.TestCloudUrl.String():  "test",
+	} {
+		env := getCCloudEnvFromBaseUrl(baseUrl)
+		assert.Equal(t, want, env)
+	}
+}
