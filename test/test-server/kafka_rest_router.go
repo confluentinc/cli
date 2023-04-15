c3acad0ee0d0d6088957292329c9298c9e607b58
Steven Gagniere <sgagniere@confluent.io>
2023-04-14 17:07:53 -0700

    update kafka link

--- test/test-server/kafka_rest_router.go
+++ test/test-server/kafka_rest_router.go
@@ -992,6 +992,11 @@ func handleKafkaRPLags(t *testing.T) http.HandlerFunc {
 // Handler for: "/kafka/v3/clusters/{cluster_id}/links/{link_name}/configs"
 func handleKafkaRPLinkConfigs(t *testing.T) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		link := mux.Vars(r)["link"]
+		if link == "link-dne" {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
 		switch r.Method {
 		case http.MethodGet:
 			err := json.NewEncoder(w).Encode(cpkafkarestv3.ListLinkConfigsResponseDataList{Data: []cpkafkarestv3.ListLinkConfigsResponseData{
@@ -1005,7 +1010,7 @@ func handleKafkaRPLinkConfigs(t *testing.T) http.HandlerFunc {
 					Sensitive: false,
 					Source:    "source-1",
 					Synonyms:  []string{"rfmb", "bmfr"},
-					LinkName:  "link-1",
+					LinkName:  link,
 				},
 				{
 					Kind:      "",
@@ -1017,7 +1022,7 @@ func handleKafkaRPLinkConfigs(t *testing.T) http.HandlerFunc {
 					Sensitive: false,
 					Source:    "source-2",
 					Synonyms:  nil,
-					LinkName:  "link-1",
+					LinkName:  link,
 				},
 			}})
 			require.NoError(t, err)
