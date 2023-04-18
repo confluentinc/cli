a7920f7bf4a866bd86a822134658dcf3165891c7
Gustavo de Morais <gdemorais@confluent.io>
2023-04-18 14:21:51 +0200

    feat: confluent flink shell on devel + error messages

--- internal/cmd/flink/command_shell.go
+++ internal/cmd/flink/command_shell.go
@@ -2,6 +2,7 @@ package flink
 
 import (
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
+	"github.com/confluentinc/cli/internal/pkg/errors"
 	"github.com/confluentinc/cli/internal/pkg/examples"
 	client "github.com/confluentinc/flink-sql-client"
 	application "github.com/confluentinc/flink-sql-client/pkg/controller"
@@ -21,6 +22,7 @@ func (c *command) newStartFlinkSqlClientCommand() *cobra.Command {
 		),
 	}
 	cmd.Flags().String("compute-pool", "", "Flink compute pool ID.")
+	cmd.Flags().String("kafka-cluster", "", "Kafka cluster ID.")
 	pcmd.AddEnvironmentFlag(cmd, c.AuthenticatedCLICommand)
 	pcmd.AddContextFlag(cmd, c.CLICommand)
 	pcmd.AddOutputFlag(cmd)
@@ -29,14 +31,38 @@ func (c *command) newStartFlinkSqlClientCommand() *cobra.Command {
 }
 
 func (c *command) startFlinkSqlClient(cmd *cobra.Command, args []string) error {
+	resourceId := c.Context.GetOrganization().GetResourceId()
+
+	//Compute pool can be set as a flag or as default in the context
 	computePool, err := cmd.Flags().GetString("compute-pool")
+	if computePool == "" || err != nil {
+		if c.Context.GetCurrentFlinkComputePool() == "" {
+			return errors.NewErrorWithSuggestions("No compute pool set", "Please set a compute pool to be used. You can either set a default persitent compute pool \"confluent flink compute-pool use lfc-123\" or pass the flag \"--compute-pool lfcp-12345\".")
+		} else {
+			computePool = c.Context.GetCurrentFlinkComputePool()
+		}
+	}
+
+	kafkaClusterId, err := cmd.Flags().GetString("kafka-cluster")
+	if kafkaClusterId == "" || err != nil {
+		if c.Context.KafkaClusterContext.GetActiveKafkaClusterId() == "" {
+			return errors.NewErrorWithSuggestions("No Kafka Cluster set", "Please set a Kafka cluster to be used. You can either set a default Kafka cluster \"confluent kafka cluster use lkc-12345\" or pass the flag \"--kafka-cluster lkc-12345\".")
+		} else {
+			kafkaClusterId = c.Context.KafkaClusterContext.GetActiveKafkaClusterId()
+		}
+	}
+
+	enviromentId, err := c.Context.EnvironmentId()
 	if err != nil {
 		return err
 	}
-	resourceId := c.Context.GetOrganization().GetResourceId()
-	ctx := c.Context.Config.Context()
-	kafkaClusterId := ctx.KafkaClusterContext.GetActiveKafkaClusterId()
 
-	client.StartApp(c.EnvironmentId(), resourceId, kafkaClusterId, computePool, c.AuthToken(), &application.ApplicationOptions{MOCK_STATEMENTS_OUTPUT_DEMO: true})
+	client.StartApp(enviromentId, resourceId, kafkaClusterId, computePool, c.AuthToken(), &application.ApplicationOptions{
+		FLINK_GATEWAY_URL:        "https://flink.us-west-2.aws.devel.cpdev.cloud",
+		HTTP_CLIENT_UNSAFE_TRACE: true,
+		DEFAULT_PROPERTIES: map[string]string{
+			"execution.runtime-mode": "streaming",
+		},
+	})
 	return nil
 }
