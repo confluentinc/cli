75dbad7a48d65a6e0ae07e5956941631edb5957c
Steven Gagniere <sgagniere@confluent.io>
2023-04-18 11:57:25 -0700

    remove old version of confirm delete function [ci skip]

--- internal/pkg/form/form.go
+++ internal/pkg/form/form.go
@@ -72,36 +72,6 @@ func (f *Form) Prompt(prompt Prompt) error {
 	return nil
 }
 
-func ConfirmDeletion(cmd *cobra.Command, promptMsg, stringToType string) (bool, error) {
-	force, err := cmd.Flags().GetBool("force")
-	if err != nil {
-		return false, err
-	}
-	if force {
-		return true, nil
-	}
-
-	prompt := NewPrompt(os.Stdin)
-	isYesNo := stringToType == ""
-	f := New(Field{ID: "confirm", Prompt: promptMsg, IsYesOrNo: isYesNo})
-	if err := f.Prompt(prompt); err != nil && isYesNo {
-		return false, errors.New(errors.FailedToReadInputErrorMsg)
-	} else if err != nil {
-		return false, err
-	}
-
-	if isYesNo {
-		return f.Responses["confirm"].(bool), nil
-	}
-
-	if f.Responses["confirm"].(string) == stringToType || f.Responses["confirm"].(string) == fmt.Sprintf(`"%s"`, stringToType) {
-		return true, nil
-	}
-
-	DeleteResourceConfirmSuggestions := "Use the `--force` flag to delete without a confirmation prompt."
-	return false, errors.NewErrorWithSuggestions(fmt.Sprintf(`input does not match "%s"`, stringToType), DeleteResourceConfirmSuggestions)
-}
-
 func ConfirmDeletionWithString(cmd *cobra.Command, resourceType, id, stringToType string) error {
 	promptMsg := fmt.Sprintf(errors.DeleteResourceConfirmMsg, resourceType, id, stringToType)
 	return ConfirmDeletionTypeCustomPrompt(cmd, promptMsg, stringToType)
