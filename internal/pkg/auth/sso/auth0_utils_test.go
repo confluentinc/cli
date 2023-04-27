5ae81c42988d48261496ff405fd3497b238b6085
Brian Strauch <bstrauch@confluent.io>
2023-04-27 14:56:36 -0700

    fix tests

--- internal/pkg/auth/sso/auth0_utils_test.go
+++ internal/pkg/auth/sso/auth0_utils_test.go
@@ -10,22 +10,20 @@ import (
 
 func TestGetCCloudEnvFromBaseUrl(t *testing.T) {
 	for baseUrl, want := range map[string]string{
-		"":                                   "prod",
-		":no-scheme-error.com":               "prod",
-		"confluent.cloud":                    "prod",
-		"default-to-prod.com":                "prod",
-		"https://confluent.cloud":            "prod",
-		"https://confluent.cloud/":           "prod",
-		"https://confluentgov-internal.com":  "fedramp-internal",
-		"https://confluentgov-internal.com/": "fedramp-internal",
-		"https://confluentgov.com":           "fedramp",
-		"https://confluentgov.com/":          "fedramp",
-		"https://devel.cpdev.cloud":          "devel",
-		"https://devel.cpdev.cloud/":         "devel",
-		"https://prefix.priv.cpdev.cloud":    "cpd",
-		"https://stag.cpdev.cloud":           "stag",
-		"https://stag.cpdev.cloud/":          "stag",
-		testserver.TestCloudUrl.String():     "test",
+		"":                         "prod",
+		":no-scheme-error.com":     "prod",
+		"confluent.cloud":          "prod",
+		"default-to-prod.com":      "prod",
+		"https://confluent.cloud":  "prod",
+		"https://confluent.cloud/": "prod",
+		"https://infra.confluentgov-internal.com":  "fedramp-internal",
+		"https://infra.confluentgov-internal.com/": "fedramp-internal",
+		"https://devel.cpdev.cloud":                "devel",
+		"https://devel.cpdev.cloud/":               "devel",
+		"https://prefix.priv.cpdev.cloud":          "cpd",
+		"https://stag.cpdev.cloud":                 "stag",
+		"https://stag.cpdev.cloud/":                "stag",
+		testserver.TestCloudUrl.String():           "test",
 	} {
 		env := GetCCloudEnvFromBaseUrl(baseUrl)
 		assert.Equal(t, want, env)
