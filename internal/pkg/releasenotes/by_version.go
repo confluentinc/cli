d98770a0788138254fd26225a5ad024fbf60b310
Brian Strauch <bstrauch@confluent.io>
2023-05-04 15:51:50 -0700

    add newlines

--- internal/pkg/releasenotes/by_version.go
+++ internal/pkg/releasenotes/by_version.go
@@ -7,8 +7,10 @@ import (
 
 type byVersion []*github.RepositoryTag
 
-func (v byVersion) Len() int      { return len(v) }
+func (v byVersion) Len() int { return len(v) }
+
 func (v byVersion) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
+
 func (v byVersion) Less(i, j int) bool {
 	vi, err := version.NewSemver(v[i].GetName())
 	if err != nil {
