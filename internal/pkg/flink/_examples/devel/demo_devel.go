0bb6f4c2008ed7fea226d69994eaff63af736acd
Joe Moser <johannes@moser.wtf>
2023-05-04 15:22:33 +0200

    update names for qos environment (#157)

--- internal/pkg/flink/_examples/devel/demo_devel.go
+++ internal/pkg/flink/_examples/devel/demo_devel.go
@@ -9,8 +9,8 @@ func main() {
 	integration := "LHLM6WR52IF5EPFR:1PztlitmLzoogth7i7iYopfAxvMykU14+CYHr/ViEvSgPHCDPPRfS1kpkLIcAK7R"
 	integrationPlayback := "PCWFUBHIAZ3YYIUP:VzTDEnhFAsKH9MzZrJRdSACWp3yrYTPUF39kflXpTW/euRNQcB8UTopCMNMLfM6v"
 	integrationWebsite := "3HENPABCYN7WKU4A:x/cOHMjBO7BdMeFVIfzO/9n8w1ik4ryA49GkisAjQBr0EZ4uCjIAzQ211/Pom+c3"
-	metrics := "ZC632C3FY2WCLQ6Z:kY/v0TAw1qBPrtpkYsaqFlAe5fdBGh3M5hYldCzvkUK065bqwiPEU8a7BsaTjkbv"
-	metricsQos := "XPLV5KXN42IA6OWJ:ZXuXs6osL4uSayWh5i8SeU9Js8wNBpzQu6cMbiixdu6KJQEOciRKOFkox0m8frei"
+	qos := "ZC632C3FY2WCLQ6Z:kY/v0TAw1qBPrtpkYsaqFlAe5fdBGh3M5hYldCzvkUK065bqwiPEU8a7BsaTjkbv"
+	qosQosMetrics := "XPLV5KXN42IA6OWJ:ZXuXs6osL4uSayWh5i8SeU9Js8wNBpzQu6cMbiixdu6KJQEOciRKOFkox0m8frei"
 	engagement := "VOUPSZQWODCAU7X6:b3P8sfTFNo54IeZCLPznNjmpnFxzoyjJ0VqTkGsDUKIwJWqK7H14QmDMhr0FMPyE"
 	engagementCoreMetrics := "KBIHCYSML52E4DME:AiNdyjhWs9oL2kIc0XoUusHPygeW01gJMkUiKCxrhNpZ78ow2j8EBP8EZ+jUr0hD"
 	engagementPersonalization := "KG3K2LCJEVD6T7YH:jBaU6oiBEAV25fhOzQb/UEnVW6qmEqLdJpfiTjdSHSDUdsJUWlnxk/v7RoKgldHA"
@@ -29,11 +29,11 @@ func main() {
 				"catalog":                "integration",
 				"confluent.kafka.keys": "playback:" + integrationPlayback + ";lkc-j0m02p:" + integrationPlayback + ";" +
 					"website:" + integrationWebsite + ";lkc-rkmkgp:" + integrationWebsite + ";" +
-					"qos:" + metricsQos + ";lkc-zn7n9y:" + metricsQos + ";" +
+					"qos_metrics:" + qosQosMetrics + ";lkc-zn7n9y:" + qosQosMetrics + ";" +
 					"core_metrics:" + engagementCoreMetrics + ";lkc-30p0m0:" + engagementCoreMetrics + ";" +
 					"personalization:" + engagementPersonalization + ";lkc-n0m096:" + engagementPersonalization,
 				"confluent.schema_registry.keys": "integration:" + integration + ";" +
-					"metrics:" + metrics + ";" +
+					"qos:" + qos + ";" +
 					"engagement:" + engagement,
 			},
 		})
