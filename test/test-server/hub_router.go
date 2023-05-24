38a03fa04ccf049632ba953a788bf654d8efbadc
Steven Gagniere <sgagniere@confluent.io>
2023-05-24 10:38:20 +0800

    style

--- test/test-server/hub_router.go
+++ test/test-server/hub_router.go
@@ -6,13 +6,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var hubRoutes = []route{
+	{"/api/plugins/{owner}/{id}", handleHubPlugin},
+	{"/api/plugins/{owner}/{id}/versions/{version}", handleHubPluginVersion},
+	{"/api/plugins/{owner}/{id}/versions/{version}/{archive}", handleHubPluginArchive},
+}
+
 func NewHubRouter(t *testing.T) *mux.Router {
 	router := mux.NewRouter()
 	router.Use(defaultHeaderMiddleware)
 
-	router.HandleFunc("/api/plugins/{owner}/{id}", handleHubPlugin(t))
-	router.HandleFunc("/api/plugins/{owner}/{id}/versions/{version}", handleHubPluginVersion(t))
-	router.HandleFunc("/api/plugins/{owner}/{id}/versions/{version}/{archive}", handleHubPluginArchive(t))
+	for _, route := range hubRoutes {
+		router.HandleFunc(route.path, route.handler(t))
+	}
 
 	return router
 }
