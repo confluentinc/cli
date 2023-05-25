9900cfa0d1592e77337574570956310070036457
MuweiHe <83249359+MuweiHe@users.noreply.github.com>
2023-05-25 13:16:41 -0700

    [CLI-193] Allow updating kafka topic partition count (#1928)
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- test/test-server/kafka_rest_router.go
+++ test/test-server/kafka_rest_router.go
@@ -271,8 +271,9 @@ func handleKafkaRPTopicConfigs(t *testing.T) http.HandlerFunc {
 				topicConfigList := cpkafkarestv3.TopicConfigDataList{
 					Data: []cpkafkarestv3.TopicConfigData{
 						{
-							Name:  "compression.type",
-							Value: cpkafkarestv3.PtrString("gzip"),
+							Name:       "compression.type",
+							Value:      cpkafkarestv3.PtrString("producer"),
+							IsReadOnly: true,
 						},
 						{
 							Name:  "retention.ms",
@@ -519,13 +520,21 @@ func handleKafkaRPConfigsAlter(t *testing.T) http.HandlerFunc {
 func handleKafkaRPTopic(t *testing.T) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		topic := mux.Vars(r)["topic"]
-		if topic != "topic-exist" && topic != "topic-exist-2" {
+		if topic != "topic-exist" && topic != "topic-exist-2" && topic != "topic-exist-rest" {
 			require.NoError(t, writeErrorResponse(w, http.StatusNotFound, 40403, "This server does not host this topic-partition."))
 			return
 		}
 		switch r.Method {
 		case http.MethodDelete:
 			w.WriteHeader(http.StatusNoContent)
+		case http.MethodGet:
+			data := cckafkarestv3.TopicData{PartitionsCount: 3}
+			err := json.NewEncoder(w).Encode(data)
+			require.NoError(t, err)
+		case http.MethodPatch:
+			data := cckafkarestv3.TopicData{PartitionsCount: 6}
+			err := json.NewEncoder(w).Encode(data)
+			require.NoError(t, err)
 		}
 	}
 }
