38d173c4aa9a61695837b3186b4ffc1ee2afdbdd
Yannick Pferr <ypferr@confluent.io>
2023-05-25 11:16:18 +0200

    refactor: use slice of structs to preserve insertion order

--- internal/pkg/flink/internal/results/result_formatter.go
+++ internal/pkg/flink/internal/results/result_formatter.go
@@ -1,18 +1,21 @@
 package results
 
-func distributeLeftoverCharacters(columnWidths []int, truncatedColumns map[int]int, leftoverCharacters int) {
-	// distribute extra characters greedy-style (each column takes as much as it needs if possible)
+type truncatedColumn struct {
+	idx            int
+	truncatedChars int
+}
+
+func distributeLeftoverCharacters(columnWidths []int, truncatedColumns []truncatedColumn, leftoverCharacters int) {
+	// distribute extra characters greedily (each column takes as much as it needs if possible)
 	for leftoverCharacters > 0 {
-		for truncatedColIdx, missingChars := range truncatedColumns {
-			if missingChars > leftoverCharacters {
-				columnWidths[truncatedColIdx] += leftoverCharacters
-				truncatedColumns[truncatedColIdx] -= leftoverCharacters
+		for _, col := range truncatedColumns {
+			if col.truncatedChars > leftoverCharacters {
+				columnWidths[col.idx] += leftoverCharacters
 				return
 			}
 
-			columnWidths[truncatedColIdx] += missingChars
-			truncatedColumns[truncatedColIdx] = 0
-			leftoverCharacters -= missingChars
+			columnWidths[col.idx] += col.truncatedChars
+			leftoverCharacters -= col.truncatedChars
 		}
 	}
 }
@@ -34,22 +37,25 @@ func GetTruncatedColumnWidths(columnWidths []int, maxCharacters int) []int {
 	charsPerColumn := maxCharacters / numColumns
 	leftoverChars := maxCharacters % numColumns
 
-	truncatedCols := map[int]int{}
-	newColumns := make([]int, numColumns)
+	var truncatedColumns []truncatedColumn // slice of struct instead of map because we need to preserve the order
+	truncatedColumnWidths := make([]int, numColumns)
 	for i, col := range columnWidths {
 		if col > charsPerColumn {
-			newColumns[i] = charsPerColumn
-			truncatedCols[i] = col - charsPerColumn
+			truncatedColumnWidths[i] = charsPerColumn
+			truncatedColumns = append(truncatedColumns, truncatedColumn{
+				idx:            i,
+				truncatedChars: col - charsPerColumn,
+			})
 			continue
 		}
 
-		newColumns[i] = col
+		truncatedColumnWidths[i] = col
 		leftoverChars += charsPerColumn - col
 	}
 
-	distributeLeftoverCharacters(newColumns, truncatedCols, leftoverChars)
+	distributeLeftoverCharacters(truncatedColumnWidths, truncatedColumns, leftoverChars)
 
-	return newColumns
+	return truncatedColumnWidths
 }
 
 func TruncateString(str string, maxCharCountToDisplay int) string {
