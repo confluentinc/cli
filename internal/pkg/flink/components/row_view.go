94e70ca89639651f1e8c0ccde2d86b0c4c5c177f
Gustavo de Morais <gdemorais@confluent.io>
2023-05-25 14:44:41 +0200

    refactor: create helper for formattingShortcuts

--- internal/pkg/flink/components/row_view.go
+++ internal/pkg/flink/components/row_view.go
@@ -1,9 +1,6 @@
 package components
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/rivo/tview"
 
 	"github.com/confluentinc/cli/internal/pkg/flink/types"
@@ -21,11 +18,8 @@ func CreateRowView(textView *tview.TextView) *tview.Flex {
 		SetDynamicColors(true).
 		SetRegions(true).
 		SetWrap(false)
-	sb := strings.Builder{}
-	for index, shortcut := range shortcuts {
-		sb.WriteString(fmt.Sprintf(`[[white]%s] ["%d"][darkcyan]%s[white][""]  `, shortcut.KeyText, index, shortcut.Text))
-	}
-	shortcutsView.SetText(sb.String())
+
+	shortcutsView.SetText(formatShortcuts(shortcuts))
 
 	return tview.NewFlex().
 		SetDirection(tview.FlexRow).
