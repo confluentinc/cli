b974438f633da80b5e2567aeeb186a71c31d5929
Yannick Pferr <ypferr@confluent.io>
2023-06-12 19:56:37 +0200

    style: place number in front time.Duration unit

--- internal/pkg/flink/config/results.go
+++ internal/pkg/flink/config/results.go
@@ -3,7 +3,7 @@ package config
 import "time"
 
 const (
-	InitialWaitTime         = time.Millisecond * 300
+	InitialWaitTime         = 300 * time.Millisecond
 	WaitTimeIncrease        = 300
 	DefaultTimeoutDuration  = 10 * time.Minute
 	ShouldCleanupStatements = false
