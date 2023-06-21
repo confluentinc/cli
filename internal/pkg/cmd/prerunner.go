fb5ff7f77bdf415aa39fab2d128cd8cdac926564
MuweiHe <mhe@confluent.io>
2023-06-21 12:04:54 -0700

    remove redundant calls to decrypt creds

--- internal/pkg/cmd/prerunner.go
+++ internal/pkg/cmd/prerunner.go
@@ -313,10 +313,6 @@ func (r *PreRun) Authenticated(command *AuthenticatedCLICommand) func(*cobra.Com
 			return err
 		}
 
-		if err := r.Config.DecryptCredentials(); err != nil {
-			return err
-		}
-
 		setContextErr := r.setAuthenticatedContext(command)
 		if setContextErr != nil {
 			if _, ok := setContextErr.(*errors.NotLoggedInError); ok {
@@ -573,10 +569,6 @@ func (r *PreRun) AuthenticatedWithMDS(command *AuthenticatedCLICommand) func(*co
 		}
 
 		if err := r.Config.DecryptContextStates(); err != nil {
-			return err
-		}
-
-		if err := r.Config.DecryptCredentials(); err != nil {
 			return err
 		}
 
@@ -846,10 +838,6 @@ func (r *PreRun) HasAPIKey(command *HasAPIKeyCLICommand) func(*cobra.Command, []
 		}
 
 		if err := r.Config.DecryptContextStates(); err != nil {
-			return err
-		}
-
-		if err := r.Config.DecryptCredentials(); err != nil {
 			return err
 		}
 
