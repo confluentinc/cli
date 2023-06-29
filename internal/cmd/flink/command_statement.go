934817848db36710b9dc49499385af74b8776650
Yannick Pferr <ypferr@confluent.io>
2023-06-29 20:50:26 +0200

    refactor: use built-in sort of list struct

--- internal/cmd/flink/command_statement.go
+++ internal/cmd/flink/command_statement.go
@@ -2,6 +2,7 @@ package flink
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 
@@ -9,11 +10,12 @@ import (
 )
 
 type statementOut struct {
-	Name         string `human:"Name" serialized:"name"`
-	Statement    string `human:"Statement" serialized:"statement"`
-	ComputePool  string `human:"Compute Pool" serialized:"compute_pool"`
-	Status       string `human:"Status" serialized:"status"`
-	StatusDetail string `human:"Status Detail,omitempty" serialized:"status_detail,omitempty"`
+	CreationDate time.Time `human:"Creation Date" serialized:"creation_date"`
+	Name         string    `human:"Name" serialized:"name"`
+	Statement    string    `human:"Statement" serialized:"statement"`
+	ComputePool  string    `human:"Compute Pool" serialized:"compute_pool"`
+	Status       string    `human:"Status" serialized:"status"`
+	StatusDetail string    `human:"Status Detail,omitempty" serialized:"status_detail,omitempty"`
 }
 
 func (c *command) newStatementCommand() *cobra.Command {
--- internal/cmd/flink/command_statement_list.go
+++ internal/cmd/flink/command_statement_list.go
@@ -1,11 +1,7 @@
 package flink
 
 import (
-	"sort"
-
 	"github.com/spf13/cobra"
-
-	"github.com/confluentinc/ccloud-sdk-go-v2/flink-gateway/v1alpha1"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/output"
@@ -42,12 +38,10 @@ func (c *command) statementList(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	sortStatementsByCreationTime(statements)
-
 	list := output.NewList(cmd)
-	list.Sort(false) // disable the default sort
 	for _, statement := range statements {
 		list.Add(&statementOut{
+			CreationDate: statement.Metadata.GetCreatedAt(),
 			Name:         statement.Spec.GetStatementName(),
 			Statement:    statement.Spec.GetStatement(),
 			ComputePool:  statement.Spec.GetComputePoolId(),
@@ -56,13 +50,4 @@ func (c *command) statementList(cmd *cobra.Command, args []string) error {
 		})
 	}
 	return list.Print()
-}
-
-func sortStatementsByCreationTime(statements []v1alpha1.SqlV1alpha1Statement) {
-	// sort ascending by creation time - with most recent statement being listed at the bottom of the table
-	sort.Slice(statements, func(i, j int) bool {
-		metadataStatement1 := statements[i].GetMetadata()
-		metadataStatement2 := statements[j].GetMetadata()
-		return metadataStatement1.GetCreatedAt().Before(metadataStatement2.GetCreatedAt())
-	})
 }
