9e92ee2efc86d3ff63023fd64139383bbfa132e4
Brian Strauch <bstrauch@confluent.io>
2023-06-29 13:27:00 -0700

    Update internal/cmd/pipeline/command_create.go

--- internal/cmd/pipeline/command_create.go
+++ internal/cmd/pipeline/command_create.go
@@ -22,7 +22,7 @@ func (c *command) newCreateCommand() *cobra.Command {
 		RunE:  c.create,
 		Example: examples.BuildExampleString(
 			examples.Example{
-				Text: `Create a Stream Designer pipeline with description "my-pipeline".`,
+				Text: `Create a Stream Designer pipeline with name "my-pipeline".`,
 				Code: `confluent pipeline create --name my-pipeline --description "example pipeline"`,
 			},
 			examples.Example{
