46721d8885a4de12e5ff0aade3254d8543c32e71
Yannick Pferr <ypferr@confluent.io>
2023-07-07 12:29:32 +0200

    refactor: use helpers to construct info boxes

--- internal/pkg/flink/components/table_info_bar.go
+++ internal/pkg/flink/components/table_info_bar.go
@@ -44,6 +44,14 @@ func (t *TableInfoBar) SetFetchState(fetchState types.FetchState) {
 }
 
 func (t *TableInfoBar) updateInfoBar() {
+	t.infoBar.Clear()
+	t.infoBar.
+		AddItem(t.constructAutoRefreshInfo(), 0, 1, false).
+		AddItem(t.constructRowInfo(), 0, 1, false).
+		AddItem(t.constructLastRefreshInfo(), 0, 1, false)
+}
+
+func (t *TableInfoBar) constructAutoRefreshInfo() tview.Primitive {
 	autoRefreshInfo := tview.NewTextView().SetDynamicColors(true).SetTextAlign(tview.AlignLeft).SetText("Fetch state: [darkcyan]-[white]")
 	switch t.fetchState {
 	case types.Completed:
@@ -55,20 +63,21 @@ func (t *TableInfoBar) updateInfoBar() {
 	case types.Running:
 		autoRefreshInfo.SetText("Fetch state: [darkcyan]running[white]")
 	}
+	return autoRefreshInfo
+}
 
+func (t *TableInfoBar) constructRowInfo() tview.Primitive {
 	rowInfo := tview.NewTextView().SetDynamicColors(true).SetTextAlign(tview.AlignCenter).SetText("")
 	if t.selectedRowIdx > 0 && t.totalNumRows > 0 {
 		rowInfo.SetText(fmt.Sprintf("Row: [darkcyan]%v[white] of [darkcyan]%v[white]", t.selectedRowIdx, t.totalNumRows))
 	}
+	return rowInfo
+}
 
+func (t *TableInfoBar) constructLastRefreshInfo() tview.Primitive {
 	lastRefreshInfo := tview.NewTextView().SetDynamicColors(true).SetTextAlign(tview.AlignRight).SetText("Last refresh: [darkcyan]-[white]")
 	if t.lastRefreshTimestamp != nil {
 		lastRefreshInfo.SetText(fmt.Sprintf("Last refresh: [darkcyan]%s[white]", t.lastRefreshTimestamp.Format("15:04:05.000")))
 	}
-
-	t.infoBar.Clear()
-	t.infoBar.
-		AddItem(autoRefreshInfo, 0, 1, false).
-		AddItem(rowInfo, 0, 1, false).
-		AddItem(lastRefreshInfo, 0, 1, false)
+	return lastRefreshInfo
 }
