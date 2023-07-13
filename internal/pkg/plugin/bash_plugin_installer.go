af301be28ea30cf7061790f7519a2bc4266aee01
Steven Gagniere <sgagniere@confluent.io>
2023-07-13 16:18:03 -0700

    add version check for bash

--- internal/pkg/plugin/bash_plugin_installer.go
+++ internal/pkg/plugin/bash_plugin_installer.go
@@ -1,7 +1,14 @@
 package plugin
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+
 	"github.com/hashicorp/go-version"
+
+	"github.com/confluentinc/cli/internal/pkg/errors"
+	"github.com/confluentinc/cli/internal/pkg/exec"
 )
 
 type BashPluginInstaller struct {
@@ -10,7 +17,28 @@ type BashPluginInstaller struct {
 	InstallDir    string
 }
 
-func (b *BashPluginInstaller) CheckVersion(_ *version.Version) error {
+func (b *BashPluginInstaller) CheckVersion(ver *version.Version) error {
+	versionCmd := exec.NewCommand("bash", "--version")
+
+	out, err := versionCmd.Output()
+	if err != nil {
+		return errors.NewErrorWithSuggestions(fmt.Sprintf(programNotFoundErrorMsg, "bash"), programNotFoundSuggestions)
+	}
+
+	re := regexp.MustCompile(`^[1-9][0-9]*\.[0-9]+\.[0-9]+\([0-9]+\)-[a-z0-9]*$`)
+	for _, word := range strings.Split(string(out), " ") {
+		if re.MatchString(word) {
+			parenthesisIdx := strings.Index(word, "(")
+			installedVer, err := version.NewVersion(word[:parenthesisIdx])
+			if err != nil {
+				return errors.Errorf(unableToParseVersionErrorMsg, "bash")
+			}
+			if installedVer.GreaterThan(ver) {
+				return errors.Errorf(insufficientVersionErrorMsg, "bash", installedVer, ver)
+			}
+		}
+	}
+
 	return nil
 }
 
