658c6ba3d0f3d62b955867c26428f089b28ee808
Steven Gagniere <sgagniere@confluent.io>
2023-07-14 16:50:14 -0700

    add multiple delete to flink iam bindings

--- internal/cmd/flink/command_iam_binding_delete.go
+++ internal/cmd/flink/command_iam_binding_delete.go
@@ -1,22 +1,20 @@
 package flink
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	"github.com/confluentinc/cli/internal/pkg/errors"
 	"github.com/confluentinc/cli/internal/pkg/form"
-	"github.com/confluentinc/cli/internal/pkg/output"
 	"github.com/confluentinc/cli/internal/pkg/resource"
+	"github.com/confluentinc/cli/internal/pkg/types"
 )
 
 func (c *command) newIamBindingDeleteCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "delete <id>",
-		Short: "Delete a Flink IAM binding.",
-		Args:  cobra.ExactArgs(1),
+		Use:   "delete <id-1> [id-2] ... [id-n]",
+		Short: "Delete Flink IAM bindings.",
+		Args:  cobra.MinimumNArgs(1),
 		RunE:  c.iamBindingDelete,
 	}
 
@@ -32,16 +30,50 @@ func (c *command) iamBindingDelete(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	promptMsg := fmt.Sprintf(errors.DeleteResourceConfirmMsg, resource.FlinkIamBinding, args[0], args[0])
-	if ok, err := form.ConfirmDeletion(cmd, promptMsg, args[0]); err != nil || !ok {
+	if err := c.confirmDeletionIamBinding(cmd, environmentId, args); err != nil {
 		return err
 	}
 
-	if err := c.V2Client.DeleteFlinkIAMBinding(args[0], environmentId); err != nil {
+	deleteFunc := func(id string) error {
+		return c.V2Client.DeleteFlinkIAMBinding(id, environmentId)
+	}
+
+	deleted, err := resource.Delete(args, deleteFunc, nil)
+	resource.PrintDeleteSuccessMsg(deleted, resource.FlinkIamBinding)
+
+	return err
+}
+
+func (c *command) confirmDeletionIamBinding(cmd *cobra.Command, environmentId string, args []string) error {
+	iamBindings, err := c.V2Client.ListFlinkIAMBindings(environmentId, "", "", "")
+	if err != nil {
+		return err
+	}
+	iamBindingsSet := types.NewSet[string]()
+	for _, iamBinding := range iamBindings {
+		iamBindingsSet.Add(iamBinding.GetId())
+	}
+
+	describeFunc := func(id string) error {
+		if !iamBindingsSet.Contains(id) {
+			return errors.Errorf(`%s "%s" not found`, resource.FlinkIamBinding, id)
+		}
+		return nil
+	}
+
+	if err := resource.ValidateArgs(pcmd.FullParentName(cmd), args, resource.FlinkIamBinding, describeFunc); err != nil {
 		return err
 	}
 
-	output.Printf(errors.DeletedResourceMsg, resource.FlinkIamBinding, args[0])
+	if len(args) == 1 {
+		if err := form.ConfirmDeletionWithString(cmd, form.DefaultPromptString(resource.FlinkIamBinding, args[0], args[0]), args[0]); err != nil {
+			return err
+		}
+	} else {
+		if ok, err := form.ConfirmDeletionYesNo(cmd, form.DefaultYesNoPromptString(resource.FlinkIamBinding, args)); err != nil || !ok {
+			return err
+		}
+	}
 
 	return nil
 }
