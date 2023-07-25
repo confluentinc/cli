58cd9a507c0fa2b0af5528d4f5a4794208da11ad
MuweiHe <mhe@confluent.io>
2023-07-25 15:54:40 -0700

    build

---
+++ docker/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const WebSocket = require('ws');
+const { exec } = require('child_process');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('WebSocket connection established.');
+
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+
+    // Execute the received command in the shell
+    exec(textDecoder.decode(message), (error, stdout, stderr) => {
+      if (error) {
+        console.error('Error executing command:', error.message);
+        ws.send('Error executing command: ' + error.message);
+      } else {
+        console.log('Command output:', stdout);
+        ws.send(stdout);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket connection closed.');
+  });
+});
+
+server.listen(8080, () => {
+  console.log('WebSocket server listening on port 8080.');
+});
