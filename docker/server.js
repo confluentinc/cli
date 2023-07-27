e1e017d92596a4f0e8cc55381194eb040ad095f7
MuweiHe <mhe@confluent.io>
2023-07-27 15:45:44 -0700

    Update server.js

--- docker/server.js
+++ docker/server.js
@@ -5,6 +5,7 @@ const { exec } = require('child_process');
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
 const wss = new WebSocket.Server({
