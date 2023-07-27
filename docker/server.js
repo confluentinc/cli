2e3d4e090d408c8751318f104ffae645112ded5d
MuweiHe <mhe@confluent.io>
2023-07-26 21:04:32 -0700

    Update server.js

--- docker/server.js
+++ docker/server.js
@@ -4,6 +4,7 @@ const { exec } = require('child_process');
 
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
+const textDecoder = new TextDecoder();
 
 wss.on('connection', (ws) => {
   console.log('WebSocket connection established.');
