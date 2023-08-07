d001ed53c47f23e768e0f8b42c7341f13973f390
MuweiHe <mhe@confluent.io>
2023-08-07 17:18:51 +0800

    initial

--- internal/cmd/schema-registry/command_config.go
+++ internal/cmd/schema-registry/command_config.go
@@ -1,10 +1,15 @@
 package schemaregistry
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
+	"github.com/tidwall/pretty"
 
 	pcmd "github.com/confluentinc/cli/internal/pkg/cmd"
 	v1 "github.com/confluentinc/cli/internal/pkg/config/v1"
+	"github.com/confluentinc/cli/internal/pkg/errors"
 )
 
 func (c *command) newConfigCommand(cfg *v1.Config) *cobra.Command {
@@ -15,6 +20,19 @@ func (c *command) newConfigCommand(cfg *v1.Config) *cobra.Command {
 	}
 
 	cmd.AddCommand(c.newConfigDescribeCommand(cfg))
+	cmd.AddCommand(c.newConfigDeleteCommand(cfg))
 
 	return cmd
+}
+
+func catchSubjectLevelConfigNotFoundError(err error, subject string) error {
+	if err != nil && strings.Contains(err.Error(), "Not Found") {
+		return errors.New(fmt.Sprintf(`subject "%s" does not have subject-level compatibility configured`, subject))
+	}
+
+	return err
+}
+
+func prettyJson(str []byte) string {
+	return strings.TrimSpace(string(pretty.Pretty(str)))
 }
