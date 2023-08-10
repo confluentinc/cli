75a1a832bffa5b68bdaf5242140afbde3e21e7b5
Brian Strauch <bstrauch@confluent.io>
2023-08-10 14:33:44 -0700

    fix readability

--- internal/cmd/cluster/command_unregister.go
+++ internal/cmd/cluster/command_unregister.go
@@ -50,9 +50,11 @@ func (c *unregisterCommand) unregister(cmd *cobra.Command, _ []string) error {
 	if err != nil {
 		return pcluster.HandleClusterError(err, httpResp)
 	}
-	if !slices.ContainsFunc(clusterInfos, func(cluster mds.ClusterInfo) bool {
+
+	found := slices.ContainsFunc(clusterInfos, func(cluster mds.ClusterInfo) bool {
 		return cluster.ClusterName == clusterName
-	}) {
+	})
+	if !found {
 		return errors.Errorf(errors.UnknownClusterErrorMsg, clusterName)
 	}
 
