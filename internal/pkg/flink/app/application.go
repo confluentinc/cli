977397ffa8b08478923589fe57cd357cbed38bb0
kevin-wu24 <kwu@confluent.io>
2023-08-14 10:44:24 -0500

    removing typo

--- internal/pkg/flink/app/application.go
+++ internal/pkg/flink/app/application.go
@@ -53,7 +53,7 @@ func StartApp(client ccloudv2.GatewayClientInterface, tokenRefreshFunc func() er
 	stdinBefore := utils.GetStdin()
 	consoleParser := utils.GetConsoleParser()
 	if consoleParser == nil {
-		utils.OutputErr("Error: failed to set initialize console parser")
+		utils.OutputErr("Error: failed to initialize console parser")
 		return
 	}
 	appController.AddCleanupFunction(func() {
