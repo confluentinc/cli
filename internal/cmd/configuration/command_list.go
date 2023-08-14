15dc29b68714fe76511cf79cf224d8f755cb12f1
kevin-wu24 <kwu@confluent.io>
2023-08-14 14:45:47 -0500

    formatting list

--- internal/cmd/configuration/command_list.go
+++ internal/cmd/configuration/command_list.go
@@ -30,14 +30,20 @@ func (c *command) list(cmd *cobra.Command, _ []string) error {
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
 		value := reflect.ValueOf(c.config).Elem().FieldByName(field.Name)
+		var values []reflect.Value
 		jsonTag := getJsonFieldName(field)
 		if jsonTag != "" {
 			_, ok := settableFields[jsonTag]
-			list.Add(&configurationOut{
+			configOut := &configurationOut{
 				Name:     jsonTag,
 				Value:    fmt.Sprintf("%v", value),
 				Settable: ok,
-			})
+			}
+			if value.Kind() == reflect.Map {
+				values = value.MapKeys()
+				configOut.Value = fmt.Sprintf("%v", values)
+			}
+			list.Add(configOut)
 		}
 	}
 	return list.Print()
