79312ef3056f632abb0c0a200b373edf27976dde
Yannick Pferr <ypferr@confluent.io>
2023-08-18 16:16:43 +0200

    feat: add timezone config usage hint to set_completer

--- pkg/flink/internal/autocomplete/set_completer.go
+++ pkg/flink/internal/autocomplete/set_completer.go
@@ -11,7 +11,7 @@ import (
 func SetCompleter(in prompt.Document) []prompt.Suggest {
 	s := []prompt.Suggest{
 		{Text: fmt.Sprintf("SET '%s' = '10000';", config.ConfigKeyResultsTimeout), Description: "Total amount of time in milliseconds to wait before timing out the request waiting for results to be ready."},
-		{Text: fmt.Sprintf("SET '%s' = 'Europe/Berlin';", config.ConfigKeyLocalTimeZone), Description: "Used to set the timezone for the current session"},
+		{Text: fmt.Sprintf("SET '%s' = 'Europe/Berlin';", config.ConfigKeyLocalTimeZone), Description: "Used to set the timezone for the current session either with a TZID ('Europe/Berlin'), a fixed offset ('GMT+02:00') or just 'UTC'"},
 	}
 
 	return SuggestFromPrefix(s, in.TextBeforeCursor())
