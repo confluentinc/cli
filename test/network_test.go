694cc5bca0a7200929bd32f709c7a92d30ee6add
Sherwin Young <hyoung@confluent.io>
2023-08-25 15:23:55 -0400

    Add integration tests for confluent network create

--- test/network_test.go
+++ test/network_test.go
@@ -2,6 +2,9 @@ package test
 
 func (s *CLITestSuite) TestNetwork() {
 	tests := []CLITest{
+		{args: "network create aws-tgw --cloud aws --region us-west-2 --connection-types transitgateway --zones usw2-az1,usw2-az2,usw2-az4 --cidr 10.1.0.0/16 --environment env-00000", fixture: "network/create.golden"},
+		{args: "network create aws-tgw --cloud aws --region us-west-2 --connection-types transitgateway --zones usw2-az1,usw2-az2,usw2-az4 --cidr 10.1.0.0/16 --environment env-00000 --output json", fixture: "network/create-json.golden"},
+		{args: "network create aws-tgw --cloud aws --region us-west-2 --connection-types transitgateway --zones usw2-az1,usw2-az2,usw2-az4 --environment env-00000", fixture: "network/create-missing-cidr.golden", exitCode: 1},
 		{args: "network describe n-abcde1", fixture: "network/describe.golden"},
 		{args: "network describe n-abcde1 --output yaml", fixture: "network/describe-yaml.golden"},
 		{args: "network describe", fixture: "network/describe-missing-id.golden", exitCode: 1},
@@ -17,6 +20,7 @@ func (s *CLITestSuite) TestNetwork() {
 		{args: "network update n-abcde1 --name new-network-name", fixture: "network/update.golden"},
 		{args: "network update n-invalid --name new-network-name", fixture: "network/update-network-not-exist.golden", exitCode: 1},
 	}
+
 	for _, test := range tests {
 		test.login = "cloud"
 		s.runIntegrationTest(test)
@@ -24,6 +28,13 @@ func (s *CLITestSuite) TestNetwork() {
 }
 
 func (s *CLITestSuite) TestNetwork_Autocomplete() {
-	test := CLITest{args: `__complete network describe ""`, login: "cloud", fixture: "network/describe-autocomplete.golden"}
-	s.runIntegrationTest(test)
+	tests := []CLITest{
+		{args: `__complete network describe ""`, login: "cloud", fixture: "network/describe-autocomplete.golden"},
+		{args: `__complete network create new-network --connection-types ""`, login: "cloud", fixture: "network/create-autocomplete-connection-types.golden"},
+		{args: `__complete network create new-network --dns-resolution ""`, login: "cloud", fixture: "network/create-autocomplete-dns-resolution.golden"},
+	}
+	for _, test := range tests {
+		test.login = "cloud"
+		s.runIntegrationTest(test)
+	}
 }
