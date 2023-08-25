8c292852ff05f33e40f3345257d141b68001dd03
Sherwin Young <hyoung@confluent.io>
2023-08-25 14:38:07 -0400

    Consolidate network tests into one function

--- test/network_test.go
+++ test/network_test.go
@@ -1,51 +1,22 @@
 package test
 
-func (s *CLITestSuite) TestNetworkDescribe() {
+func (s *CLITestSuite) TestNetwork() {
 	tests := []CLITest{
 		{args: "network describe n-abcde1", fixture: "network/describe.golden"},
 		{args: "network describe n-abcde1 --output yaml", fixture: "network/describe-yaml.golden"},
 		{args: "network describe", fixture: "network/describe-missing-id.golden", exitCode: 1},
 		{args: "network describe n-invalid", fixture: "network/describe-invalid.golden", exitCode: 1},
-	}
-	for _, test := range tests {
-		test.login = "cloud"
-		s.runIntegrationTest(test)
-	}
-}
-
-func (s *CLITestSuite) TestNetworkDelete() {
-	tests := []CLITest{
 		{args: "network delete n-abcde1 --force", fixture: "network/delete.golden"},
 		{args: "network delete n-abcde1", input: "y\n", fixture: "network/delete-prompt.golden"},
 		{args: "network delete n-dependency --force", fixture: "network/delete-network-with-dependency.golden", exitCode: 1},
 		{args: "network delete n-invalid --force", fixture: "network/delete-network-not-exist.golden", exitCode: 1},
-	}
-	for _, test := range tests {
-		test.login = "cloud"
-		s.runIntegrationTest(test)
-	}
-}
-
-func (s *CLITestSuite) TestNetworkUpdate() {
-	tests := []CLITest{
+		{args: "network list", fixture: "network/list.golden"},
+		{args: "network list --output json", fixture: "network/list-json.golden"},
 		{args: "network update", fixture: "network/update-missing-args.golden", exitCode: 1},
 		{args: "network update n-abcde1", fixture: "network/update-missing-flags.golden", exitCode: 1},
 		{args: "network update n-abcde1 --name new-network-name", fixture: "network/update.golden"},
 		{args: "network update n-invalid --name new-network-name", fixture: "network/update-network-not-exist.golden", exitCode: 1},
 	}
-
-	for _, test := range tests {
-		test.login = "cloud"
-		s.runIntegrationTest(test)
-	}
-}
-
-func (s *CLITestSuite) TestNetworkList() {
-	tests := []CLITest{
-		{args: "network list", fixture: "network/list.golden"},
-		{args: "network list --output json", fixture: "network/list-json.golden"},
-	}
-
 	for _, test := range tests {
 		test.login = "cloud"
 		s.runIntegrationTest(test)
