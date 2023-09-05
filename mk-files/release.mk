796be57f284a9a223b4e68d0d5fdc38ac06d6ce0
Brian Strauch <bstrauch@confluent.io>
2023-09-05 14:35:10 -0500

    [CLI-2710] Update packaging during release (#2220)

--- mk-files/release.mk
+++ mk-files/release.mk
@@ -17,6 +17,8 @@ release: check-branch
 	$(MAKE) publish-installer
 	$(call print-boxed-message,"RELEASING TO PROD FOLDER $(S3_BUCKET_PATH)")
 	$(MAKE) release-to-prod
+	$(call print-boxed-message,"UPDATING PACKAGING")
+	$(MAKE) update-packaging
 	$(call print-boxed-message,"UPDATING MUCKRAKE")
 	$(MAKE) update-muckrake
 	$(call print-boxed-message,"PUBLISHING DOCS")
@@ -163,7 +165,7 @@ update-muckrake:
 	git clone git@github.com:confluentinc/muckrake.git $(MUCKRAKE) && \
 	cd $(MUCKRAKE) && \
 	git fetch --all && \
-	branch="cli-$${version}" && \
+	branch=bump-cli && \
 	base=$$(git branch --remote --format "%(refname:short)" | sed -n "s|^origin/\([1-9][0-9]*\.[0-9][0-9]*\.x\)$$|\1|p" | tail -1) && \
 	git checkout $$base && \
 	git checkout $$branch || git checkout -b $$branch && \
@@ -171,6 +173,33 @@ update-muckrake:
 	$(SED) -i "s|VERSION = \".*\"|VERSION = \"$${version}\"|" muckrake/services/cli.py && \
 	$(SED) -i "s|get_cli .*|get_cli $${version}|" vagrant/base-redhat.sh && \
 	$(SED) -i "s|get_cli .*|get_cli $${version}|" vagrant/base-ubuntu.sh && \
+	git commit -am "bump cli to v$${version}" && \
+	$(call dry-run,git push -u origin $$branch) && \
+	if ! gh pr view $$branch; then \
+		$(call dry-run,gh pr create --base $${base} --title "Bump CLI to v$${version}" --body "") && \
+		$(call dry-run,gh pr merge --squash --auto); \
+	fi
+
+	rm -rf $(DIR)
+
+.PHONY: update-packaging
+update-packaging:
+	$(eval DIR=$(shell mktemp -d))
+	$(eval CLI_RELEASE=$(DIR)/cli-release)
+	$(eval PACKAGING=$(DIR)/packaging)
+
+	git clone git@github.com:confluentinc/cli-release.git $(CLI_RELEASE) && \
+	cd $(CLI_RELEASE) && \
+	version=$$(ls release-notes | $(SED) "s/.json$$//" | sort --version-sort | tail -1) && \
+	git clone git@github.com:confluentinc/packaging.git $(PACKAGING) && \
+	cd $(PACKAGING) && \
+	git fetch --all && \
+	branch="bump-cli" && \
+	base=$$(git branch --remote --format "%(refname:short)" | sed -n "s|^origin/\([1-9][0-9]*\.[0-9][0-9]*\.x\)$$|\1|p" | tail -1) && \
+	git checkout $$base && \
+	git checkout $$branch || git checkout -b $$branch && \
+	$(SED) -i "s|cli_BRANCH=\".*\"|cli_BRANCH=\"$${version}\"|" settings.sh && \
+	$(SED) -i "s|CLI_VERSION=.*|CLI_VERSION=$${version}|" release_testing/bin/smoke_test.sh && \
 	git commit -am "bump cli to v$${version}" && \
 	$(call dry-run,git push -u origin $$branch) && \
 	if ! gh pr view $$branch; then \
