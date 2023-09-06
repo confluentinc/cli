f6ec31a618c327df0a76718c10f251009b53d6da
Bishwajeet <111917051+bideyio@users.noreply.github.com>
2023-09-05 17:08:11 -0700

    [FCP-919] Handle redirection from flink gateway endpoints (#2231)

--- pkg/ccloudv2/flink_gateway.go
+++ pkg/ccloudv2/flink_gateway.go
@@ -2,12 +2,15 @@ package ccloudv2
 
 import (
 	"context"
+	"errors"
+	"net/http"
 
 	"github.com/google/uuid"
 
 	flinkgatewayv1alpha1 "github.com/confluentinc/ccloud-sdk-go-v2/flink-gateway/v1alpha1"
 
 	"github.com/confluentinc/cli/v3/pkg/errors/flink"
+	"github.com/confluentinc/cli/v3/pkg/log"
 )
 
 type GatewayClientInterface interface {
@@ -27,7 +30,23 @@ type FlinkGatewayClient struct {
 func NewFlinkGatewayClient(url, userAgent string, unsafeTrace bool, authToken string) *FlinkGatewayClient {
 	cfg := flinkgatewayv1alpha1.NewConfiguration()
 	cfg.Debug = unsafeTrace
-	cfg.HTTPClient = NewRetryableHttpClient(unsafeTrace)
+	cfg.HTTPClient = NewRetryableHttpClientWithRedirect(unsafeTrace,
+		func(req *http.Request, via []*http.Request) error {
+			// Default net/http implementation allows 10 redirects - https://go.dev/src/net/http/client.go.
+			// Lowered the redirect limit to fail fast in case of redirect cycles
+			if len(via) >= 5 {
+				return errors.New("stopped after 5 redirects")
+			}
+			log.CliLogger.Debugf("Following redirect with authorization to %s", req.URL)
+			// Customize the redirect to add authorization header on 307 Redirect.
+			// This is required as the Location header returned by the gateway may not be an exact subdomain and Authorization
+			// header is copied only when the hostname is exactly the same or an exact subdomain of the hostname
+			// Ideally, we should check the status code returned in via[0].Response. However, via[0].Response is nil in underlying
+			// retryable http implementation
+			req.Header.Add("Authorization", "Bearer "+authToken)
+
+			return nil
+		})
 	cfg.Servers = flinkgatewayv1alpha1.ServerConfigurations{{URL: url}}
 	cfg.UserAgent = userAgent
 
