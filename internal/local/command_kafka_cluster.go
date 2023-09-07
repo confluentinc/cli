7cdf09899023c587b1a6412aa38df8d07098bcbe
MuweiHe <mhe@confluent.io>
2023-09-07 10:08:40 -0700

    comments vii

--- internal/local/command_kafka_cluster.go
+++ internal/local/command_kafka_cluster.go
@@ -7,7 +7,7 @@ import (
 func (c *command) newKafkaClusterCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "cluster",
-		Short: "Manage local Kafka cluster.",
+		Short: "Manage the local Kafka cluster.",
 	}
 
 	cmd.AddCommand(c.newKafkaClusterConfigurationCommand())
