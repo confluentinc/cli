e35d9e4b802c8321420e7fc83de3797738ca2aba
Brian Strauch <bstrauch@confluent.io>
2023-09-08 15:22:27 -0500

    fix regex test

--- test/flink_test.go
+++ test/flink_test.go
@@ -62,7 +62,7 @@ func (s *CLITestSuite) TestFlinkRegion() {
 func (s *CLITestSuite) TestFlinkStatement() {
 	tests := []CLITest{
 		{args: `flink statement create my-statement --sql "INSERT * INTO table;" --cloud aws --region eu-west-1 --service-account sa-123456`, fixture: "flink/statement/create.golden"},
-		{args: `flink statement create --sql "INSERT * INTO table;" --cloud aws --region eu-west-1 --service-account sa-123456 -o yaml`, fixture: "flink/statement/create-no-name-yaml.golden"},
+		{args: `flink statement create --sql "INSERT * INTO table;" --cloud aws --region eu-west-1 --service-account sa-123456 -o yaml`, fixture: "flink/statement/create-no-name-yaml.golden", regex: true},
 		{args: `flink statement create my-statement --sql "INSERT * INTO table;" --cloud aws --region eu-west-1`, fixture: "flink/statement/create-service-account-warning.golden"},
 		{args: "flink statement delete my-statement --force --cloud aws --region eu-west-1", fixture: "flink/statement/delete.golden"},
 		{args: "flink statement list --compute-pool lfcp-123456", fixture: "flink/statement/list.golden"},
