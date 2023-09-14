4d418ee6b11c051a195c08c8e676965dd472cd7b
Steven Gagniere <108363707+sgagniere@users.noreply.github.com>
2023-09-14 11:16:26 -0700

    Update internal/flink/command_shell.go
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/flink/command_shell.go
+++ internal/flink/command_shell.go
@@ -180,7 +180,7 @@ func (c *command) startFlinkSqlClient(prerunner pcmd.PreRunner, cmd *cobra.Comma
 }
 
 func reportUsage(cmd *cobra.Command, cfg *config.Config, unsafeTrace bool) func() {
-	if cfg.IsCloudLogin() && !cfg.HasGovHostname() {
+	if !cfg.HasGovHostname() {
 		return func() {
 			u := ppanic.CollectPanic(cmd, nil, cfg)
 			u.Report(cfg.GetCloudClientV2(unsafeTrace))
