e48a013af0b9518d0508701bdc53ac986e5940b5
Luca Filipponi <lfilipponi@confluent.io>
2023-09-15 08:19:47 +0100

    update help message and fixed tests

--- internal/iam/command_rbac_role_binding_create.go
+++ internal/iam/command_rbac_role_binding_create.go
@@ -54,7 +54,7 @@ func (c *roleBindingCommand) newCreateCommand() *cobra.Command {
 				Code: "confluent iam rbac role-binding create --principal User:u-123456 --role FlinkDeveloper --environment env-12345",
 			},
 			examples.Example{
-				Text: `Grant the role "FlinkDeveloper" to the principal "User:u-123456", in the environment "env-12345" for the compute pool "lfcp-123456" in the Flink region "us-east-2":`,
+				Text: `Grant the role "FlinkDeveloper" to the principal "User:u-123456", in the environment "env-12345" for the ComputePool "lfcp-123456" in the Flink region "us-east-2":`,
 				Code: "confluent iam rbac role-binding create --principal User:u-123456 --role FlinkDeveloper --environment env-12345 --flink-region aws.us-east-2 --resource compute-pool:lfcp-123456",
 			},
 		)
