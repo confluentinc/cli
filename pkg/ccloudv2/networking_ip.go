933e38e4280bfd6f2ed1f617defea842dd728857
Sherwin Young <hyoung@confluent.io>
2023-09-25 16:30:53 -0400

    Add network ip-address list command

---
+++ pkg/ccloudv2/networking_ip.go
@@ -0,0 +1,53 @@
+package ccloudv2
+
+import (
+	"context"
+
+	networkingipv1 "github.com/confluentinc/ccloud-sdk-go-v2/networking-ip/v1"
+
+	"github.com/confluentinc/cli/v3/pkg/errors"
+)
+
+func newNetworkingIpClient(url, userAgent string, unsafeTrace bool) *networkingipv1.APIClient {
+	cfg := networkingipv1.NewConfiguration()
+	cfg.Debug = unsafeTrace
+	cfg.HTTPClient = NewRetryableHttpClient(unsafeTrace)
+	cfg.Servers = networkingipv1.ServerConfigurations{{URL: url}}
+	cfg.UserAgent = userAgent
+
+	return networkingipv1.NewAPIClient(cfg)
+}
+
+func (c *Client) networkingIpApiContext() context.Context {
+	return context.WithValue(context.Background(), networkingipv1.ContextAccessToken, c.AuthToken)
+}
+
+func (c *Client) ListIpAddresses() ([]networkingipv1.NetworkingV1IpAddress, error) {
+	var list []networkingipv1.NetworkingV1IpAddress
+
+	done := false
+	pageToken := ""
+	for !done {
+		page, err := c.executeListIpAddresses(pageToken)
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, page.GetData()...)
+
+		pageToken, done, err = extractNextPageToken(page.GetMetadata().Next)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return list, nil
+}
+
+func (c *Client) executeListIpAddresses(pageToken string) (networkingipv1.NetworkingV1IpAddressList, error) {
+	req := c.NetworkingIpClient.IPAddressesNetworkingV1Api.ListNetworkingV1IpAddresses(c.networkingIpApiContext()).PageSize(ccloudV2ListPageSize)
+	if pageToken != "" {
+		req = req.PageToken(pageToken)
+	}
+
+	resp, httpResp, err := req.Execute()
+	return resp, errors.CatchCCloudV2Error(err, httpResp)
+}
