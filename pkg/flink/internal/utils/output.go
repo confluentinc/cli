270e51c3ec92d49d11ea014f83aa201c7e211702
Yannick Pferr <37211050+YannickPferr@users.noreply.github.com>
2023-09-25 11:16:02 +0200

    fix: truncate multi-line cells in the basic table properly (#2304)

--- pkg/flink/internal/utils/output.go
+++ pkg/flink/internal/utils/output.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"strings"
+
 	fColor "github.com/fatih/color"
 
 	"github.com/confluentinc/cli/v3/pkg/color"
@@ -33,4 +35,19 @@ func OutputWarn(s string) {
 func OutputWarnf(s string, args ...any) {
 	c := fColor.New(color.WarnColor)
 	output.Printf(c.Sprintln(s), args...)
+}
+
+func GetMaxStrWidth(str string) int {
+	// split the string by lines and find the longest line
+	lines := strings.Split(strings.ReplaceAll(str, "\r\n", "\n"), "\n")
+	maxWidth := 0
+	for idx, line := range lines {
+		lineLength := len(line) + 1
+		// the last line does not have a extra new line char that needs to be counted
+		if idx == len(lines)-1 {
+			lineLength = len(line)
+		}
+		maxWidth = max(maxWidth, lineLength)
+	}
+	return maxWidth
 }
