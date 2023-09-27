38f7f499124e6ac21f2f4cc3662ddf0c53f78c00
Brian Strauch <bstrauch@confluent.io>
2023-09-27 09:29:23 -0700

    [CLI-2625] Add `flink statement create` (#2099)

--- internal/flink/command_statement_describe.go
+++ internal/flink/command_statement_describe.go
@@ -31,7 +31,7 @@ func (c *command) statementDescribe(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	client, err := c.GetFlinkGatewayClient()
+	client, err := c.GetFlinkGatewayClient(false)
 	if err != nil {
 		return err
 	}
