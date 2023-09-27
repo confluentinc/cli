44eafb8defab10bce96206a325ce0604061fe808
Brian Strauch <bstrauch@confluent.io>
2023-09-26 17:05:00 -0700

    fix bad merge

--- pkg/flink/internal/utils/output.go
+++ pkg/flink/internal/utils/output.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"strings"
+
 	fColor "github.com/fatih/color"
 
 	"github.com/confluentinc/cli/v3/pkg/color"
@@ -28,4 +30,24 @@ func OutputInfof(s string, args ...any) {
 func OutputWarn(s string) {
 	c := fColor.New(color.WarnColor)
 	output.Println(false, c.Sprint(s))
+}
+
+func OutputWarnf(s string, args ...any) {
+	c := fColor.New(color.WarnColor)
+	output.Printf(false, c.Sprint(s), args...)
+}
+
+func GetMaxStrWidth(str string) int {
+	// split the string by lines and find the longest line
+	lines := strings.Split(strings.ReplaceAll(str, "\r\n", "\n"), "\n")
+	maxWidth := 0
+	for idx, line := range lines {
+		lineLength := len(line) + 1
+		// the last line does not have a extra new line char that needs to be counted
+		if idx == len(lines)-1 {
+			lineLength = len(line)
+		}
+		maxWidth = max(maxWidth, lineLength)
+	}
+	return maxWidth
 }
