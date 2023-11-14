fc977e06aef4735561aaf7e0c1076fb4a6b749c4
Steven Gagniere <sgagniere@confluent.io>
2023-11-14 14:17:56 -0800

    sync deb to s3

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -48,5 +48,7 @@ docker container cp cli-linux-arm64-builder:/cli/deb/. ./deb/
 docker container cp cli-linux-arm64-builder:/cli/rpm/. ./rpm/
 docker container rm cli-linux-arm64-builder
 
+dry-run "aws s3 sync deb s3://confluent-cli-internal/confluent-cli/deb"
 dry-run "aws s3 sync rpm s3://confluent-cli-internal/confluent-cli/rpm"
+dry-run "s3-repo-utils -v website index --fake-index --prefix 'confluent-cli/deb' confluent-cli-internal" #TODO: this command is apparently flaky; might have to run it a few times
 dry-run "s3-repo-utils -v website index --fake-index --prefix 'confluent-cli/rpm' confluent-cli-internal" #TODO: this command is apparently flaky; might have to run it a few times
