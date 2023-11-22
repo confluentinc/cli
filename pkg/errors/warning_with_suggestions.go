d5cdf618726b60d73a06649f2b81b6ade9504a0d
Brian Strauch <bstrauch@confluent.io>
2023-11-22 11:04:34 -0800

    fix warning newline

--- pkg/errors/warning_with_suggestions.go
+++ pkg/errors/warning_with_suggestions.go
@@ -1,21 +1,20 @@
 package errors
 
-type WarningWithSuggestions struct {
+type warningWithSuggestions struct {
 	warnMsg        string
 	suggestionsMsg string
 }
 
-func NewWarningWithSuggestions(warnMsg, suggestionsMsg string) *WarningWithSuggestions {
-	return &WarningWithSuggestions{
+func NewWarningWithSuggestions(warnMsg, suggestionsMsg string) *warningWithSuggestions {
+	return &warningWithSuggestions{
 		warnMsg:        warnMsg,
 		suggestionsMsg: suggestionsMsg,
 	}
 }
 
-func (w *WarningWithSuggestions) DisplayWarningWithSuggestions() string {
+func (w *warningWithSuggestions) DisplayWarningWithSuggestions() string {
 	if w.warnMsg != "" && w.suggestionsMsg != "" {
 		lines := "[WARN] " + w.warnMsg + "\n"
-		lines += "\n"
 		lines += ComposeSuggestionsMessage(w.suggestionsMsg) + "\n"
 		return lines
 	}
