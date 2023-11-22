a77ab4c7aedf320dc6d8cc093370546dbd1d8f05
Brian Strauch <bstrauch@confluent.io>
2023-11-22 09:53:40 -0800

    rm reason

--- pkg/errors/warning_with_suggestions.go
+++ pkg/errors/warning_with_suggestions.go
@@ -2,23 +2,20 @@ package errors
 
 type WarningWithSuggestions struct {
 	warnMsg        string
-	reasonMsg      string
 	suggestionsMsg string
 }
 
-func NewWarningWithSuggestions(warnMsg, reasonMsg, suggestionsMsg string) *WarningWithSuggestions {
+func NewWarningWithSuggestions(warnMsg, suggestionsMsg string) *WarningWithSuggestions {
 	return &WarningWithSuggestions{
 		warnMsg:        warnMsg,
-		reasonMsg:      reasonMsg,
 		suggestionsMsg: suggestionsMsg,
 	}
 }
 
 func (w *WarningWithSuggestions) DisplayWarningWithSuggestions() string {
-	if w.warnMsg != "" && w.reasonMsg != "" && w.suggestionsMsg != "" {
+	if w.warnMsg != "" && w.suggestionsMsg != "" {
 		lines := "[WARN] " + w.warnMsg + "\n"
 		lines += "\n"
-		lines += "Reason: " + w.reasonMsg + "\n"
 		lines += ComposeSuggestionsMessage(w.suggestionsMsg) + "\n"
 		return lines
 	}
