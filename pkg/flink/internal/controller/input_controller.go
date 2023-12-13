c10128249674a4cb428d4a8ec3b565a6597eca9e
Yannick Pferr <37211050+YannickPferr@users.noreply.github.com>
2023-12-13 09:15:28 +0100

    feat: add AltEnter shortcut as command termination trigger (#2487)

--- pkg/flink/internal/controller/input_controller.go
+++ pkg/flink/internal/controller/input_controller.go
@@ -154,12 +154,19 @@ func (c *InputController) Prompt() prompt.IPrompt {
 			},
 		}),
 		prompt.OptionAddASCIICodeBind(prompt.ASCIICodeBind{
+			// Alt/Option + Arrow Left
 			ASCIICode: []byte{0x1b, 0x62},
 			Fn:        prompt.GoLeftWord,
 		}),
 		prompt.OptionAddASCIICodeBind(prompt.ASCIICodeBind{
+			// Alt/Option + Arrow Right
 			ASCIICode: []byte{0x1b, 0x66},
 			Fn:        prompt.GoRightWord,
+		}),
+		prompt.OptionAddASCIICodeBind(prompt.ASCIICodeBind{
+			// Alt/Option + Backspace
+			ASCIICode: []byte{0x1b, 0x7F},
+			Fn:        prompt.DeleteWord,
 		}),
 		prompt.OptionPrefixTextColor(prompt.Yellow),
 		prompt.OptionPreviewSuggestionTextColor(prompt.Blue),
@@ -169,14 +176,10 @@ func (c *InputController) Prompt() prompt.IPrompt {
 		prompt.OptionSetStatementTerminator(func(lastKeyStroke prompt.Key, buffer *prompt.Buffer) bool {
 			text := buffer.Text()
 			text = strings.TrimSpace(text)
-			// We add exit here because we also want to exit without the need of adding semicolon, which is the default flow for all statements
-			if text == "exit" {
-				return true
-			}
-			if text == "" || !strings.HasSuffix(text, ";") {
+			if text == "" {
 				return false
 			}
-			return true
+			return text == "exit" || strings.HasSuffix(text, ";") || lastKeyStroke == prompt.AltEnter
 		}),
 	)
 }
