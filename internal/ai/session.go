e1623797e564535de278905c8c01f597445a2905
Brian Strauch <bstrauch@confluent.io>
2024-01-24 14:30:55 -0800

    add session struct

---
+++ internal/ai/session.go
@@ -0,0 +1,26 @@
+package ai
+
+import (
+	"time"
+
+	aiv1 "github.com/confluentinc/ccloud-sdk-go-v2-internal/ai/v1"
+	"github.com/google/uuid"
+)
+
+type session struct {
+	id        string
+	history   []aiv1.AiV1ChatCompletionsHistory
+	expiresAt time.Time
+}
+
+func NewSession() *session {
+	return &session{
+		id:        uuid.New().String(),
+		history:   []aiv1.AiV1ChatCompletionsHistory{},
+		expiresAt: time.Now().Add(time.Hour),
+	}
+}
+
+func (s *session) isExpired() bool {
+	return time.Now().After(s.expiresAt)
+}
