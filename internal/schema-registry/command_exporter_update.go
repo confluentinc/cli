ba6fd2a77ca2a3fd8f043a49ac49a08be7a9127e
Steven Gagniere <sgagniere@confluent.io>
2024-01-29 17:35:36 -0800

    Remove deprecated '--api-key and --api-secrets' flags from schema-registry commands

--- internal/schema-registry/command_exporter_update.go
+++ internal/schema-registry/command_exporter_update.go
@@ -57,16 +57,6 @@ func (c *command) newExporterUpdateCommand(cfg *config.Config) *cobra.Command {
 	cobra.CheckErr(cmd.Flags().MarkHidden("config-file"))
 	cmd.MarkFlagsMutuallyExclusive("config", "config-file")
 
-	if cfg.IsCloudLogin() {
-		// Deprecated
-		pcmd.AddApiKeyFlag(cmd, c.AuthenticatedCLICommand)
-		cobra.CheckErr(cmd.Flags().MarkHidden("api-key"))
-
-		// Deprecated
-		pcmd.AddApiSecretFlag(cmd)
-		cobra.CheckErr(cmd.Flags().MarkHidden("api-secret"))
-	}
-
 	return cmd
 }
 
