7a04d83a750c8bde030ced8f23c7b09f8683c167
Steven Gagniere <sgagniere@confluent.io>
2024-01-29 17:14:53 -0800

    Rearrange local kafka integration tests because they're order dependent

--- test/local_test.go
+++ test/local_test.go
@@ -27,10 +27,10 @@ func (s *CLITestSuite) TestLocalKafka() {
 	time.Sleep(40 * time.Second)
 
 	tests2 := []CLITest{
-		{args: "local kafka topic configuration list test", fixture: "local/kafka/topic/configuration/list.golden"},
 		{args: "local kafka topic create test", fixture: "local/kafka/topic/create.golden"},
 		{args: "local kafka topic list", fixture: "local/kafka/topic/list.golden"},
 		{args: "local kafka topic describe test", fixture: "local/kafka/topic/describe.golden"},
+		{args: "local kafka topic configuration list test", fixture: "local/kafka/topic/configuration/list.golden"},
 		{args: "local kafka topic delete test --force", fixture: "local/kafka/topic/delete.golden"},
 		{args: "local kafka stop", fixture: "local/kafka/stop.golden", regex: true},
 	}
