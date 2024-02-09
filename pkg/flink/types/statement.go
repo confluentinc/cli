f1124ac8a97c840c3d06701e1b0e24b1770ab01d
Yannick Pferr <ypferr@confluent.io>
2024-02-09 18:09:21 +0100

    feat: generate statement names with format cli-date-time-uuid

--- pkg/flink/types/statement.go
+++ pkg/flink/types/statement.go
@@ -1,7 +1,15 @@
 package types
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"time"
+)
 
 func GenerateStatementName() string {
-	return uuid.New().String()[:18]
+	clientName := "cli"
+	date := time.Now().Format("2006-01-02")
+	localTime := time.Now().Format("150405")
+	id := uuid.New().String()
+	return fmt.Sprintf("%s-%s-%s-%s", clientName, date, localTime, id)
 }
