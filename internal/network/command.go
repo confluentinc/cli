cba2012444d22e0042ad8ad47fd554ee072b57cf
Amy Zhang <146779173+ay-zhang@users.noreply.github.com>
2024-02-12 20:17:24 -0500

    [TRAFFIC-10544] Add gateway to network list/describe (#2599)

--- internal/network/command.go
+++ internal/network/command.go
@@ -21,6 +21,7 @@ type humanOut struct {
 	Id                                         string `human:"ID"`
 	EnvironmentId                              string `human:"Environment"`
 	Name                                       string `human:"Name,omitempty"`
+	Gateway                                    string `human:"Gateway,omitempty"`
 	Cloud                                      string `human:"Cloud"`
 	Region                                     string `human:"Region"`
 	Cidr                                       string `human:"CIDR,omitempty"`
@@ -47,6 +48,7 @@ type serializedOut struct {
 	Id                                         string            `serialized:"id"`
 	EnvironmentId                              string            `serialized:"environment_id"`
 	Name                                       string            `serialized:"name,omitempty"`
+	Gateway                                    string            `serialized:"gateway,omitempty"`
 	Cloud                                      string            `serialized:"cloud"`
 	Region                                     string            `serialized:"region"`
 	Cidr                                       string            `serialized:"cidr,omitempty"`
@@ -130,6 +132,7 @@ func printHumanTable(cmd *cobra.Command, network networkingv1.NetworkingV1Networ
 		Id:                       network.GetId(),
 		EnvironmentId:            network.Spec.Environment.GetId(),
 		Name:                     network.Spec.GetDisplayName(),
+		Gateway:                  network.Spec.GetGateway().Id,
 		Cloud:                    cloud,
 		Region:                   network.Spec.GetRegion(),
 		Zones:                    strings.Join(network.Spec.GetZones(), ", "),
@@ -138,7 +141,7 @@ func printHumanTable(cmd *cobra.Command, network networkingv1.NetworkingV1Networ
 		ActiveConnectionTypes:    strings.Join(network.Status.GetActiveConnectionTypes().Items, ", "),
 	}
 
-	describeFields := []string{"Id", "EnvironmentId", "Name", "Cloud", "Region", "Zones", "Phase", "SupportedConnectionTypes", "ActiveConnectionTypes"}
+	describeFields := []string{"Id", "EnvironmentId", "Name", "Gateway", "Cloud", "Region", "Zones", "Phase", "SupportedConnectionTypes", "ActiveConnectionTypes"}
 
 	if slices.Contains(supportedConnectionTypes, "PRIVATELINK") {
 		human.DnsResolution = network.Spec.DnsConfig.GetResolution()
@@ -203,6 +206,7 @@ func printSerializedTable(cmd *cobra.Command, network networkingv1.NetworkingV1N
 		Id:                       network.GetId(),
 		EnvironmentId:            network.Spec.Environment.GetId(),
 		Name:                     network.Spec.GetDisplayName(),
+		Gateway:                  network.Spec.GetGateway().Id,
 		Cloud:                    network.Spec.GetCloud(),
 		Region:                   network.Spec.GetRegion(),
 		Zones:                    network.Spec.GetZones(),
@@ -211,7 +215,7 @@ func printSerializedTable(cmd *cobra.Command, network networkingv1.NetworkingV1N
 		ActiveConnectionTypes:    network.Status.GetActiveConnectionTypes().Items,
 	}
 
-	describeFields := []string{"Id", "EnvironmentId", "Name", "Cloud", "Region", "Zones", "Phase", "SupportedConnectionTypes", "ActiveConnectionTypes"}
+	describeFields := []string{"Id", "EnvironmentId", "Name", "Gateway", "Cloud", "Region", "Zones", "Phase", "SupportedConnectionTypes", "ActiveConnectionTypes"}
 
 	if slices.Contains(supportedConnectionTypes, "PRIVATELINK") {
 		serialized.DnsResolution = network.Spec.DnsConfig.GetResolution()
