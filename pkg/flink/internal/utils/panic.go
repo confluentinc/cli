3a6740050a5714eb9564419e361fd75bcf41e678
Yannick Pferr <ypferr@confluent.io>
2024-02-25 19:50:02 +0100

    fix: panic recovery w/ limit should only break on panic
    The panic recovery with limit also stopped the execution when the readEvalPrint function was called too quickly. However, it's only supposed to stop execution when there's an actual panic we recovered from

--- pkg/flink/internal/utils/panic.go
+++ pkg/flink/internal/utils/panic.go
@@ -7,44 +7,36 @@ import (
 	"github.com/confluentinc/cli/v3/pkg/log"
 )
 
-type PanicRecovererWithLimitImpl struct {
+type PanicRecoveryWithLimit struct {
 	recovers          int
 	lastRecover       time.Time
 	maxRecovers       int
 	timeBetweenPanics time.Duration
 }
 
-type PanicRecovererWithLimit interface {
-	WithCustomPanicRecovery(fn func(), customRecovery func()) func() error
-}
-
-func NewPanicRecovererWithLimit(maxRecovers int, timeBetweenPanics time.Duration) PanicRecovererWithLimit {
-	recoverer := PanicRecovererWithLimitImpl{
+func NewPanicRecoveryWithLimit(maxRecovers int, timeBetweenPanics time.Duration) PanicRecoveryWithLimit {
+	return PanicRecoveryWithLimit{
 		recovers:          0,
 		maxRecovers:       maxRecovers,
 		lastRecover:       time.Unix(0, 0),
 		timeBetweenPanics: timeBetweenPanics,
 	}
-	return &recoverer
 }
 
-func (p *PanicRecovererWithLimitImpl) WithCustomPanicRecovery(fn func(), customRecovery func()) func() error {
-	if time.Since(p.lastRecover) > p.timeBetweenPanics {
-		p.recovers = 0
-	}
-
-	p.recovers++
-	if p.recovers > p.maxRecovers {
-		return func() error {
-			return errors.NewErrorWithSuggestions(errors.InternalServerErrorMsg, "Run `confluent flink shell -vvv` to enable debug logs when starting the flink shell and report the output to the CLI team. Kindly share steps reproduce, if possible.\nPlease, restart the CLI.")
+func (p *PanicRecoveryWithLimit) WithCustomPanicRecovery(fn func(), customRecovery func()) error {
+	WithCustomPanicRecovery(fn, func() {
+		if time.Since(p.lastRecover) > p.timeBetweenPanics {
+			p.recovers = 0
 		}
-	}
+		p.recovers++
+		p.lastRecover = time.Now()
+		customRecovery()
+	})()
 
-	p.lastRecover = time.Now()
-	return func() error {
-		WithCustomPanicRecovery(fn, customRecovery)()
-		return nil
+	if p.recovers > p.maxRecovers {
+		return errors.NewErrorWithSuggestions(errors.InternalServerErrorMsg, "Run `confluent flink shell -vvv` to enable debug logs when starting the flink shell and report the output to the CLI team. Kindly share steps reproduce, if possible.\nPlease, restart the CLI.")
 	}
+	return nil
 }
 
 func WithPanicRecovery(fn func()) func() {
