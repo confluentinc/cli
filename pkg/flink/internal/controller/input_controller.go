750598d9aa85f101fb72cbd2a190e45e3972e537
Martin Tang <13372091+MarTango@users.noreply.github.com>
2024-03-04 16:11:44 +0000

    KFS-1706 Add missing keyboard shortcuts to flink shell (#2667)

--- pkg/flink/internal/controller/input_controller.go
+++ pkg/flink/internal/controller/input_controller.go
@@ -188,12 +188,6 @@ func (c *InputController) getKeyBindings() []prompt.Option {
 	return append(
 		[]prompt.Option{
 			prompt.OptionAddKeyBind(prompt.KeyBind{
-				Key: prompt.ControlD,
-				Fn: func(b *prompt.Buffer) {
-					c.shouldExit = true
-				},
-			}),
-			prompt.OptionAddKeyBind(prompt.KeyBind{
 				Key: prompt.ControlQ,
 				Fn: func(b *prompt.Buffer) {
 					c.shouldExit = true
@@ -218,16 +212,34 @@ func getUnixBindings() []prompt.Option {
 	return []prompt.Option{
 		prompt.OptionAddASCIICodeBind(
 			prompt.ASCIICodeBind{
-				ASCIICode: []byte{0x1b, 0x62}, // Alt/Option + Arrow Left
+				ASCIICode: []byte{0x1b, 0x62}, // Alt/Option + Arrow Left (sometimes Alt/Option + b)
 				Fn:        prompt.GoLeftWord,
 			},
 			prompt.ASCIICodeBind{
-				ASCIICode: []byte{0x1b, 0x66}, // Alt/Option + Arrow Right
+				ASCIICode: []byte{0x1b, 0x66}, // Alt/Option + Arrow Right (sometimes Alt/Option + f)
 				Fn:        prompt.GoRightWord,
 			},
 			prompt.ASCIICodeBind{
 				ASCIICode: []byte{0x1b, 0x7F}, // Alt/Option + Backspace
 				Fn:        prompt.DeleteWord,
+			},
+			prompt.ASCIICodeBind{
+				ASCIICode: []byte{0x1b, 0x64}, // ForwardDeleteWord (Alt/Option + d)
+				Fn: func(buf *prompt.Buffer) {
+					buf.Delete(buf.Document().FindEndOfCurrentWordWithSpace())
+				},
+			},
+			prompt.ASCIICodeBind{
+				ASCIICode: []byte{0x1b, 0x75}, // UpCaseWord (Alt/Option + u)
+				Fn: func(buf *prompt.Buffer) {
+					buf.InsertText(strings.ToUpper(buf.Document().GetWordAfterCursorWithSpace()), true, true)
+				},
+			},
+			prompt.ASCIICodeBind{
+				ASCIICode: []byte{0x1b, 0x6c}, // DownCaseWord (Alt/Option + l)
+				Fn: func(buf *prompt.Buffer) {
+					buf.InsertText(strings.ToLower(buf.Document().GetWordAfterCursorWithSpace()), true, true)
+				},
 			},
 		),
 	}
