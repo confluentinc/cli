0b0792a6a8fc2a793fd9e5fe84df6b55ffbd709c
Brian Strauch <bstrauch@confluent.io>
2024-03-08 12:23:20 -0800

    Update internal/schema-registry/command_kek_create.go

--- internal/schema-registry/command_kek_create.go
+++ internal/schema-registry/command_kek_create.go
@@ -18,7 +18,7 @@ func (c *command) newKekCreateCommand(cfg *config.Config) *cobra.Command {
 		RunE:  c.kekCreate,
 		Example: examples.BuildExampleString(
 			examples.Example{
-				Text: "Create a KEK with a AWS KMS key:",
+				Text: "Create a KEK with an AWS KMS key:",
 				Code: "confluent schema-registry kek create --name test --kms-type aws-kms --kms-key-id arn:aws:kms:us-west-2:037502941121:key/a1231e22-1n78-4l0d-9d50-9pww5faedb54 --kms-properties KeyUsage=ENCRYPT_DECRYPT,KeyState=Enabled",
 			},
 		),
