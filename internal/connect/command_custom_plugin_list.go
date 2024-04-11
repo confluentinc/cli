cfe7b8915d02ad186076aa4f3d61d1577b02db97
Nicole Du <ndu@confluent.io>
2024-04-11 14:23:19 -0500

    SCP-2403, CLI-3114: Migration-cli-from-aws-AWS-for-cloud-attribute-output (#2707)
    Co-authored-by: Brian Strauch <bstrauch@confluent.io>

--- internal/connect/command_custom_plugin_list.go
+++ internal/connect/command_custom_plugin_list.go
@@ -1,6 +1,8 @@
 package connect
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/v3/pkg/cmd"
@@ -28,7 +30,7 @@ func (c *customPluginCommand) newListCommand() *cobra.Command {
 		),
 	}
 
-	pcmd.AddCloudFlag(cmd)
+	c.addCloudFlag(cmd, "")
 	pcmd.AddContextFlag(cmd, c.CLICommand)
 	pcmd.AddOutputFlag(cmd)
 
@@ -41,6 +43,7 @@ func (c *customPluginCommand) list(cmd *cobra.Command, _ []string) error {
 		return err
 	}
 
+	cloud = strings.ToUpper(cloud)
 	plugins, err := c.V2Client.ListCustomPlugins(cloud)
 	if err != nil {
 		return err
