36a9f7a52a01d68dd6779a99ce050289b6fe673c
Hao Li <1127478+lihaosky@users.noreply.github.com>
2024-05-30 16:53:08 -0700

    [MATRIX-191] only pass secret for create and alter model

--- pkg/flink/config/local_statements.go
+++ pkg/flink/config/local_statements.go
@@ -22,6 +22,27 @@ const (
 	KeyServiceAccount = "client.service-account"
 	KeyStatementName  = "client.statement-name"
 	KeyOutputFormat   = "client.output-format"
+
+	/*
+	   Create Alter model regex. Match create|alter model with
+	     1. allow any space,return,tab between them
+	     2. They are not surrounded by unescaped single quote
+	     3. Single quote is escaped by doubling them like ''.
+	     4. Start beginning of line, with space or `;`
+
+	     (?msi) -> enable multiline, . match \n and case insensitive
+	     \s -> matches space, \n, \t, \r, \f
+	     '' -> match escaped single quote
+	     ?: -> ignore group
+	     '(?:''|[^'])*' -> match anything inside single quote including escaped quote
+	     ?P<query> -> give group match a name
+	     (?:^|;|\\s)(?:create|alter)\\s+model\\s+ -> start with beginning of line, spaces or ;. Match create or alter, then multiple \s, then model, then multiple \s
+
+	     Reference: https://github.com/google/re2/wiki/Syntax
+	                https://pkg.go.dev/regexp#Regexp.FindSubmatch
+	                https://stackoverflow.com/questions/6462578/regex-to-match-all-instances-not-inside-quotes
+	*/
+	CreateAlterModelRegex = "(?msi)''|'(?:''|[^'])*'|(?P<query>(?:^|;|\\s)(?:create|alter)\\s+model\\s+)"
 )
 
 type OutputFormat string
