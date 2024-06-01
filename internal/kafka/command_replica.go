90238518abdb15ecd9bc2cb46db4bb9775e61892
Steven Gagniere <sgagniere@confluent.io>
2024-05-31 17:23:21 -0700

    rename confusing annotation

--- internal/kafka/command_replica.go
+++ internal/kafka/command_replica.go
@@ -14,7 +14,7 @@ func newReplicaCommand(prerunner pcmd.PreRunner) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:         "replica",
 		Short:       "Manage Kafka replicas.",
-		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNonCloudLogin},
+		Annotations: map[string]string{pcmd.RunRequirement: pcmd.RequireNotCloudLogin},
 	}
 
 	c := &replicaCommand{pcmd.NewAuthenticatedWithMDSCLICommand(cmd, prerunner)}
