a1b41523ff194d42410fb5b90d1344fd099d4c23
Tushar Malik <tmalik@confluent.io>
2024-06-12 13:34:55 -0500

    kafka region list output should match flink region list

--- internal/kafka/command_region_list.go
+++ internal/kafka/command_region_list.go
@@ -1,12 +1,20 @@
 package kafka
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/v3/pkg/cmd"
 	"github.com/confluentinc/cli/v3/pkg/kafka"
 	"github.com/confluentinc/cli/v3/pkg/output"
 )
+
+type regionOut struct {
+	Name   string `human:"Name" serialized:"name"`
+	Cloud  string `human:"Cloud" serialized:"cloud"`
+	Region string `human:"Region" serialized:"region"`
+}
 
 func (c *regionCommand) newListCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -35,7 +43,12 @@ func (c *regionCommand) list(cmd *cobra.Command, _ []string) error {
 
 	list := output.NewList(cmd)
 	for _, region := range regions {
-		list.Add(region)
+		out := &regionOut{
+			Cloud:  strings.ToUpper(region.CloudId),
+			Region: region.RegionId,
+			Name:   region.RegionName,
+		}
+		list.Add(out)
 	}
 	return list.Print()
 }
