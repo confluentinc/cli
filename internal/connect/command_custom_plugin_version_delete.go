fdd82e3be8338153db144eb82d652d22fb7d688f
Nicole Du <ndu@confluent.io>
2024-09-17 10:24:26 -0500

    make lint

--- internal/connect/command_custom_plugin_version_delete.go
+++ internal/connect/command_custom_plugin_version_delete.go
@@ -1,12 +1,12 @@
 package connect
 
 import (
-	"github.com/confluentinc/cli/v3/pkg/output"
 	"github.com/spf13/cobra"
 
 	pcmd "github.com/confluentinc/cli/v3/pkg/cmd"
 	"github.com/confluentinc/cli/v3/pkg/deletion"
 	"github.com/confluentinc/cli/v3/pkg/examples"
+	"github.com/confluentinc/cli/v3/pkg/output"
 	"github.com/confluentinc/cli/v3/pkg/resource"
 )
 
