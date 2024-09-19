65b698d4c77863daefac8380e639ff889079145e
Steven Gagniere <108363707+sgagniere@users.noreply.github.com>
2024-09-19 10:17:52 -0700

    allow = for boolean flags in examples (#85)

--- cmd/lint/main.go
+++ cmd/lint/main.go
@@ -37,7 +37,9 @@ var commandRules = []linter.CommandRule{
 	linter.RequireStartWithCapital("Long"),
 
 	linter.RequireListRequiredFlagsFirst(),
-	linter.Filter(linter.RequireValidExamples(), linter.ExcludeCommand("pipeline update")),
+	linter.Filter(linter.RequireValidExamples(),
+		linter.ExcludeCommand("pipeline update"),
+		linter.ExcludeCommand("flink statement update")),
 
 	// Soft Requirements
 	linter.Filter(linter.RequireLengthBetween("Short", 10, 60),
