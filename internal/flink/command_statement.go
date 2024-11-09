4383d699126edcf9ef11cc5b1ca7efba5eef02e8
Channing Dong <165202380+channingdong@users.noreply.github.com>
2024-11-08 17:35:33 -0800

    Update help files and address nits

--- internal/flink/command_statement.go
+++ internal/flink/command_statement.go
@@ -31,8 +31,8 @@ func (c *command) newStatementCommand() *cobra.Command {
 	cmd.AddCommand(c.newStatementDescribeCommand())
 	cmd.AddCommand(c.newStatementExceptionCommand())
 	cmd.AddCommand(c.newStatementListCommand())
-	cmd.AddCommand(c.newStatementStopCommand())
 	cmd.AddCommand(c.newStatementResumeCommand())
+	cmd.AddCommand(c.newStatementStopCommand())
 	cmd.AddCommand(c.newStatementUpdateCommand())
 
 	return cmd
