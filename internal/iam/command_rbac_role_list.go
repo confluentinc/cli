4884f922b3d812733d35d6b21249155bf5d83554
aydenadair <aadair@confluent.io>
2025-01-30 11:28:16 -0700

    Update ccloud list integration test to function like actual CLI call

--- internal/iam/command_rbac_role_list.go
+++ internal/iam/command_rbac_role_list.go
@@ -2,10 +2,9 @@ package iam
 
 import (
 	"github.com/antihax/optional"
-	"github.com/spf13/cobra"
-
 	pcmd "github.com/confluentinc/cli/v4/pkg/cmd"
 	"github.com/confluentinc/cli/v4/pkg/output"
+	"github.com/spf13/cobra"
 )
 
 func (c *roleCommand) newListCommand() *cobra.Command {
