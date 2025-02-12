7ea460f9e1df298c23c18a33c05fab7de01a06af
swathirameshcflt <sramesh@confluent.io>
2025-02-12 14:42:58 -0800

    fix: lint errors

--- internal/iam/command_certificate_pool_create.go
+++ internal/iam/command_certificate_pool_create.go
@@ -1,14 +1,14 @@
 package iam
 
 import (
-	"github.com/confluentinc/cli/v4/pkg/config"
-	"github.com/confluentinc/cli/v4/pkg/featureflags"
 	"github.com/spf13/cobra"
 
 	certificateauthorityv2 "github.com/confluentinc/ccloud-sdk-go-v2/certificate-authority/v2"
 
 	pcmd "github.com/confluentinc/cli/v4/pkg/cmd"
+	"github.com/confluentinc/cli/v4/pkg/config"
 	"github.com/confluentinc/cli/v4/pkg/examples"
+	"github.com/confluentinc/cli/v4/pkg/featureflags"
 )
 
 func (c *certificatePoolCommand) newCreateCommand(cfg *config.Config) *cobra.Command {
