bb628d0857328672bfa300b30810924ad88071cc
Cynthia Qin <cqin@confluent.io>
2025-03-19 13:11:20 -0700

    Update tests and golden

--- internal/kafka/command_cluster_create.go
+++ internal/kafka/command_cluster_create.go
@@ -46,7 +46,7 @@ func (c *clusterCommand) newCreateCommand() *cobra.Command {
 			},
 			examples.Example{
 				Text: "Create a new dedicated cluster that uses a customer-managed encryption key in AWS:",
-				Code: "confluent kafka cluster create my-cluster --cloud aws --region us-west-2 --zone usw2-az1 --type dedicated --cku 1 --byok cck-a123z",
+				Code: "confluent kafka cluster create my-cluster --cloud aws --region us-west-2 --zone usw2-az1 --type dedicated --cku 1 --byok cck-a123z --network n-abc123",
 			},
 			examples.Example{
 				Text: "Create a new Freight cluster that uses a customer-managed encryption key in AWS:",
@@ -63,7 +63,7 @@ func (c *clusterCommand) newCreateCommand() *cobra.Command {
 	pcmd.AddAvailabilityFlag(cmd)
 	pcmd.AddTypeFlag(cmd)
 	cmd.Flags().Int("cku", 0, `Number of Confluent Kafka Units (non-negative). Required for Kafka clusters of type "dedicated".`)
-	cmd.Flags().String("zone", "", `Specify zone selection for single zone clusters. Optional for Kafka clusters of type "dedicated".`)
+	cmd.Flags().String("zone", "", `Specify zone selection for single zone clusters. Optional for private network Kafka clusters of type "dedicated" with the input of a Network ID.`)
 	pcmd.AddByokKeyFlag(cmd, c.AuthenticatedCLICommand)
 	pcmd.AddNetworkFlag(cmd, c.AuthenticatedCLICommand)
 	pcmd.AddContextFlag(cmd, c.CLICommand)
