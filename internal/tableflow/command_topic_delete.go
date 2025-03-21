ca1c5dd7e015fc80a1ac0c4ae1fd6dc9972328a9
Daniel Ayaz <dayaz@confluent.io>
2025-03-21 16:04:07 -0500

    Edit

--- internal/tableflow/command_topic_delete.go
+++ internal/tableflow/command_topic_delete.go
@@ -73,7 +73,7 @@ func (c *command) delete(cmd *cobra.Command, args []string) error {
 	if len(deletedTopic) == 1 {
 		output.Printf(c.Config.EnableColor, deleteMsg, resource.Topic, fmt.Sprintf(`"%s"`, deletedTopic[0]))
 	} else if len(deletedTopic) > 1 {
-		output.Printf(c.Config.EnableColor, deleteMsg, resource.Plural(resource.Topic), utils.ArrayToCommaDelimitedString(deletedTopic, "and"))
+		output.Printf(c.Config.EnableColor, deleteMsg, plural.Plural(resource.Topic), utils.ArrayToCommaDelimitedString(deletedTopic, "and"))
 	}
 
 	return err
