70f50367a465c47efd75676d527eccd620e71d60
Cynthia Qin <cqin@confluent.io>
2025-03-28 14:21:18 -0700

    Update tests and resolve linter

--- internal/iam/command_provider_update.go
+++ internal/iam/command_provider_update.go
@@ -27,8 +27,8 @@ func (c *identityProviderCommand) newUpdateCommand() *cobra.Command {
 	cmd.Flags().String("name", "", "Name of the identity provider.")
 	cmd.Flags().String("description", "", "Description of the identity provider.")
 	cmd.Flags().String("identity-claim", "", "The JSON Web Token (JWT) claim to extract the authenticating identity to Confluent resources from "+
-		"[Registered Claim Names](https://datatracker.ietf.org/doc/html/rfc7519#section-4.1). This appears "+
-		"in audit log records. Note: if the client specifies mapping to one identity pool ID, the identity "+
+		"Registered Claim Names. This appears in audit log records. "+
+		"Note: if the client specifies mapping to one identity pool ID, the identity "+
 		"claim configured with that pool will be used instead.")
 	pcmd.AddContextFlag(cmd, c.CLICommand)
 	pcmd.AddOutputFlag(cmd)
