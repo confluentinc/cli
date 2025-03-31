2eef0628cf52437f1c9950f3d9fd9d75ee682348
Channing Dong <165202380+channingdong@users.noreply.github.com>
2025-03-31 10:45:33 -0700

    Slightly change on the order of subcommands

--- internal/flink/command.go
+++ internal/flink/command.go
@@ -45,8 +45,8 @@ func New(cfg *config.Config, prerunner pcmd.PreRunner) *cobra.Command {
 		cmd.AddCommand(c.newArtifactCommand())
 		cmd.AddCommand(c.newComputePoolCommand())
 		cmd.AddCommand(c.newConnectivityTypeCommand())
-		cmd.AddCommand(c.newRegionCommand())
 		cmd.AddCommand(c.newEndpointCommand())
+		cmd.AddCommand(c.newRegionCommand())
 		cmd.AddCommand(c.newShellCommand(prerunner))
 		cmd.AddCommand(c.newStatementCommand())
 	}
