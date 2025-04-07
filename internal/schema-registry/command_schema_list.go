e1f7c21ef53f5b79f4f2b0b69d09fcb8cfc78e15
tmalik <167480759+tmalikconfluent@users.noreply.github.com>
2025-04-07 15:02:53 -0500

    CLI-3464 - Support certificate path and key path for schema registry on-prem mtls (#3054)
    Co-authored-by: Steven Gagniere <sgagniere@confluent.io>
    Co-authored-by: Steven Gagniere <108363707+sgagniere@users.noreply.github.com>

--- internal/schema-registry/command_schema_list.go
+++ internal/schema-registry/command_schema_list.go
@@ -48,7 +48,7 @@ func (c *command) newSchemaListCommand(cfg *config.Config) *cobra.Command {
 	if cfg.IsCloudLogin() {
 		pcmd.AddEnvironmentFlag(cmd, c.AuthenticatedCLICommand)
 	} else {
-		addCaLocationFlag(cmd)
+		addCaLocationAndClientPathFlags(cmd)
 	}
 	addSchemaRegistryEndpointFlag(cmd)
 	pcmd.AddOutputFlag(cmd)
