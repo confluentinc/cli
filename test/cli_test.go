b1d1410d2e5d35e7a9343d256419afeb560eba05
Daniel Ayaz <dayaz@confluent.io>
2025-04-22 13:47:22 -0500

    Revert "edit test for windows"
    This reverts commit 3dadbfb9081d1ca4ce138cbd61713aeedc9e2a0d.

--- test/cli_test.go
+++ test/cli_test.go
@@ -82,20 +82,14 @@ func (s *CLITestSuite) SetupSuite() {
 	err := os.Chdir("..")
 	req.NoError(err)
 
+	target := "build-for-integration-test"
 	if runtime.GOOS == "windows" {
-		// On Windows, build directly with go instead of using make
-		testBin = "test/bin/confluent.exe"
-		// Ensure the test/bin directory exists
-		err = os.MkdirAll("test/bin", 0755)
-		req.NoError(err)
-		output, err := exec.Command("go", "build", "-o", testBin, "./cmd/confluent").CombinedOutput()
-		req.NoError(err, string(output))
-	} else {
-		// On Unix-like systems, use make
-		target := "build-for-integration-test"
-		output, err := exec.Command("make", target).CombinedOutput()
-		req.NoError(err, string(output))
+		target += "-windows"
+		testBin += ".exe"
 	}
+
+	output, err := exec.Command("make", target).CombinedOutput()
+	req.NoError(err, string(output))
 
 	s.TestBackend = testserver.StartTestBackend(s.T(), true) // by default do not disable audit-log
 	os.Setenv("DISABLE_AUDIT_LOG", "false")
