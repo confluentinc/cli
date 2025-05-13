688342b677551d354d08db0a98d20683a294b3a7
rveerannagowda <rveerannagowda@confluent.io>
2025-05-13 01:45:46 -0400

    Add auth type optional argument

--- pkg/flink/utils.go
+++ pkg/flink/utils.go
@@ -14,7 +14,7 @@ var (
 		"elastic":        {"api-key"},
 		"pinecone":       {"api-key"},
 		"couchbase":      {"username", "password"},
-		"confluent-jdbc": {"username", "password"},
+		"confluent-jdbc": {"username", "password", "auth"},
 	}
 
 	ConnectionSecretTypeMapping = map[string][]string{
@@ -25,6 +25,7 @@ var (
 		"service-key":       {"vertexai"},
 		"username":          {"mongodb", "couchbase", "confluent-jdbc"},
 		"password":          {"mongodb", "couchbase", "confluent-jdbc"},
+		"auth":              {"confluent-jdbc"},
 	}
 
 	ConnectionRequiredSecretMapping = map[string][]string{
@@ -49,5 +50,6 @@ var (
 		"service-key":       "SERVICE_KEY",
 		"username":          "USERNAME",
 		"password":          "PASSWORD",
+		"auth":              "AUTH",
 	}
 )
