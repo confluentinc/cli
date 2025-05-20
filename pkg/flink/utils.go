46ee5c08f29060535b8ce85569f84bd1d6e5a2cd
Letao Jiang <ljiang@confluent.io>
2025-05-19 19:52:28 -0700

    add dynamic secret check

--- pkg/flink/utils.go
+++ pkg/flink/utils.go
@@ -15,22 +15,23 @@ var (
 		"pinecone":       {"api-key"},
 		"couchbase":      {"username", "password"},
 		"confluent_jdbc": {"username", "password"},
-		"rest":           {"username", "password", "bearer-token", "oauth-token-endpoint", "oauth-client-id", "oauth-client-secret", "oauth-scope"},
+		"rest":           {"username", "password", "auth-type", "token", "token-endpoint", "client-id", "client-secret", "scope"},
 	}
 
 	ConnectionSecretTypeMapping = map[string][]string{
-		"api-key":              {"openai", "azureml", "azureopenai", "googleai", "elastic", "pinecone"},
-		"aws-access-key":       {"bedrock", "sagemaker"},
-		"aws-secret-key":       {"bedrock", "sagemaker"},
-		"aws-session-token":    {"bedrock", "sagemaker"},
-		"service-key":          {"vertexai"},
-		"username":             {"mongodb", "couchbase", "confluent_jdbc", "rest"},
-		"password":             {"mongodb", "couchbase", "confluent_jdbc", "rest"},
-		"bearer-token":         {"rest"},
-		"oauth-token-endpoint": {"rest"},
-		"oauth-client-id":      {"rest"},
-		"oauth-client-secret":  {"rest"},
-		"oauth-scope":          {"rest"},
+		"api-key":           {"openai", "azureml", "azureopenai", "googleai", "elastic", "pinecone"},
+		"aws-access-key":    {"bedrock", "sagemaker"},
+		"aws-secret-key":    {"bedrock", "sagemaker"},
+		"aws-session-token": {"bedrock", "sagemaker"},
+		"service-key":       {"vertexai"},
+		"username":          {"mongodb", "couchbase", "confluent_jdbc", "rest"},
+		"password":          {"mongodb", "couchbase", "confluent_jdbc", "rest"},
+		"auth-type":         {"rest"},
+		"token":             {"rest"},
+		"token-endpoint":    {"rest"},
+		"client-id":         {"rest"},
+		"client-secret":     {"rest"},
+		"scope":             {"rest"},
 	}
 
 	ConnectionRequiredSecretMapping = map[string][]string{
@@ -46,20 +47,33 @@ var (
 		"pinecone":       {"api-key"},
 		"couchbase":      {"username", "password"},
 		"confluent_jdbc": {"username", "password"},
-		"rest":           {},
+		"rest":           {"auth-type"},
 	}
+
+	ConnectionDynamicSecretMapping = map[string]map[string]map[string][]string{
+		"rest": {
+			"auth-type": {
+				"no_auth": {},
+				"basic":   {"username", "password"},
+				"bearer":  {"token"},
+				"oauth2":  {"token-endpoint", "client-id", "client-secret", "scope"},
+			},
+		},
+	}
+
 	ConnectionSecretBackendKeyMapping = map[string]string{
-		"api-key":              "API_KEY",
-		"aws-access-key":       "AWS_ACCESS_KEY_ID",
-		"aws-secret-key":       "AWS_SECRET_ACCESS_KEY",
-		"aws-session-token":    "AWS_SESSION_TOKEN",
-		"service-key":          "SERVICE_KEY",
-		"username":             "USERNAME",
-		"password":             "PASSWORD",
-		"bearer-token":         "BEARER.TOKEN",
-		"oauth-token-endpoint": "OAUTH2.TOKEN_ENDPOINT",
-		"oauth-client-id":      "OAUTH2.CLIENT_ID",
-		"oauth-client-secret":  "OAUTH2.CLIENT_SECRET",
-		"oauth-scope":          "OAUTH2.SCOPE",
+		"api-key":           "API_KEY",
+		"aws-access-key":    "AWS_ACCESS_KEY_ID",
+		"aws-secret-key":    "AWS_SECRET_ACCESS_KEY",
+		"aws-session-token": "AWS_SESSION_TOKEN",
+		"service-key":       "SERVICE_KEY",
+		"username":          "USERNAME",
+		"password":          "PASSWORD",
+		"auth-type":         "AUTH_TYPE",
+		"token":             "BEARER_TOKEN",
+		"token-endpoint":    "OAUTH2_TOKEN_ENDPOINT",
+		"client-id":         "OAUTH2_CLIENT_ID",
+		"client-secret":     "OAUTH2_CLIENT_SECRET",
+		"scope":             "OAUTH2_SCOPE",
 	}
 )
