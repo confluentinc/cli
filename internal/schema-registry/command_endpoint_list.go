c96acf93fe52076b3a2158ece42d8f3665cbfea5
Steven Gagniere <sgagniere@confluent.io>
2025-07-22 12:15:41 -0700

    update error message again

--- internal/schema-registry/command_endpoint_list.go
+++ internal/schema-registry/command_endpoint_list.go
@@ -60,7 +60,7 @@ func (c *command) endpointList(cmd *cobra.Command, _ []string) error {
 	// Filter out non-PrivateLink networks for Azure
 	azureNetworks, err := c.V2Client.ListNetworks(environmentId, nil, []string{cloud.Azure}, []string{}, nil, []string{"READY"}, []string{"PRIVATELINK"})
 	if err != nil {
-		return fmt.Errorf("unable to list Schema Registry endpoints: failed to list Azure networks: %w", err)
+		return fmt.Errorf("unable to list Schema Registry endpoints: failed to list Azure PrivateLink networks: %w", err)
 	}
 
 	networks := append(awsNetworks, azureNetworks...)
