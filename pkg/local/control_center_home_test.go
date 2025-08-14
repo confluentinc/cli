667fc3a5460ce5fc12d1efdd44ae78336d69cadd
Tushar Malik <tmalik@confluent.io>
2025-08-13 19:26:35 -0500

    Lint

--- pkg/local/control_center_home_test.go
+++ pkg/local/control_center_home_test.go
@@ -1,11 +1,11 @@
 package local
 
 import (
-	"github.com/stretchr/testify/require"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
