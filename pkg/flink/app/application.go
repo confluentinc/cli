065eaccb8bbc2ce45e7f5e6ce158f8a178dbdebe
Hao Li <1127478+lihaosky@users.noreply.github.com>
2025-08-20 15:07:12 -0700

    [MATRIX-1102] use server returned statement text in history

--- pkg/flink/app/application.go
+++ pkg/flink/app/application.go
@@ -205,7 +205,8 @@ func (a *Application) readEvalPrint() {
 		return
 	}
 	if !executedStatement.IsSensitiveStatement {
-		a.history.Append(userInput)
+		// Append executedStatement text since sensitive information such as password can be masked in server side
+		a.history.Append(executedStatement.Statement)
 	}
 
 	if !executedStatement.IsDryRunStatement() {
