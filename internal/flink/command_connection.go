c969751da34d691eafdcca12fcf8f740df16808e
yanand0909 <yanand@confluent.io>
2025-09-11 15:09:19 -0400

    [MATRIX-1118] support transport type flag for MCP_SERVER

--- internal/flink/command_connection.go
+++ internal/flink/command_connection.go
@@ -96,6 +96,7 @@ func AddConnectionSecretFlags(cmd *cobra.Command) {
 	cmd.Flags().String("client-secret", "", fmt.Sprintf("Specify OAuth2 client secret for the type: %s.", utils.ArrayToCommaDelimitedString(flink.ConnectionSecretTypeMapping["client-secret"], "or")))
 	cmd.Flags().String("scope", "", fmt.Sprintf("Specify OAuth2 scope for the type: %s.", utils.ArrayToCommaDelimitedString(flink.ConnectionSecretTypeMapping["scope"], "or")))
 	cmd.Flags().String("sse-endpoint", "", fmt.Sprintf("Specify SSE endpoint for the type: %s.", utils.ArrayToCommaDelimitedString(flink.ConnectionSecretTypeMapping["sse-endpoint"], "or")))
+	cmd.Flags().String("transport-type", "", fmt.Sprintf("Specify transport type for the type: %s.", utils.ArrayToCommaDelimitedString(flink.ConnectionSecretTypeMapping["transport-type"], "or")))
 	cmd.MarkFlagsRequiredTogether("username", "password")
 	cmd.MarkFlagsRequiredTogether("aws-access-key", "aws-secret-key")
 	cmd.MarkFlagsRequiredTogether("token-endpoint", "client-id", "client-secret", "scope")
@@ -120,6 +121,16 @@ func validateConnectionSecrets(cmd *cobra.Command, connectionType string) (map[s
 		}
 		if secret != "" && !slices.Contains(connectionSecrets, key) {
 			return nil, errors.NewErrorWithSuggestions(fmt.Sprintf("%s is invalid for connection %s.", key, connectionType), fmt.Sprintf("Valid secret types are %s.", utils.ArrayToCommaDelimitedString(connectionSecrets, "or")))
+		}
+	}
+
+	for key, allowedValues := range flink.ConnectionSecretAllowedValues {
+		secretValue, err := cmd.Flags().GetString(key)
+		if err != nil {
+			return nil, err
+		}
+		if secretValue != "" && !slices.Contains(allowedValues, secretValue) {
+			return nil, errors.NewErrorWithSuggestions(fmt.Sprintf("%s is invalid value for flag %s.", secretValue, key), fmt.Sprintf("Valid values for flag %s are %s.", key, utils.ArrayToCommaDelimitedString(allowedValues, "or")))
 		}
 	}
 
