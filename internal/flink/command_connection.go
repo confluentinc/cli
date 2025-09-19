fe9e0320382c4e3ead9b40737c3cd101f1807789
yanand0909 <yanand@confluent.io>
2025-09-18 18:53:58 -0700

    [MATRIX-1118] Address comments

--- internal/flink/command_connection.go
+++ internal/flink/command_connection.go
@@ -3,6 +3,7 @@ package flink
 import (
 	"fmt"
 	"slices"
+	"strings"
 	"time"
 
 	"github.com/samber/lo"
@@ -110,39 +111,51 @@ func validateConnectionType(connectionType string) error {
 	return nil
 }
 
-func validateConnectionSecrets(cmd *cobra.Command, connectionType string) (map[string]string, error) {
-	var connectionSecrets []string
-	connectionSecrets = append(connectionSecrets, flink.ConnectionTypeSecretMapping[connectionType]...)
+func validateSecretCompatibility(cmd *cobra.Command, connectionType string) error {
+	connectionSecrets := flink.ConnectionTypeSecretMapping[connectionType]
 
 	for key := range flink.ConnectionSecretTypeMapping {
 		secret, err := cmd.Flags().GetString(key)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		if secret != "" && !slices.Contains(connectionSecrets, key) {
-			return nil, errors.NewErrorWithSuggestions(fmt.Sprintf("%s is invalid for connection %s.", key, connectionType), fmt.Sprintf("Valid secret types are %s.", utils.ArrayToCommaDelimitedString(connectionSecrets, "or")))
+			return errors.NewErrorWithSuggestions(
+				fmt.Sprintf("%s is invalid for connection %s.", key, connectionType),
+				fmt.Sprintf("Valid secret types are %s.", utils.ArrayToCommaDelimitedString(connectionSecrets, "or")))
 		}
 	}
+	return nil
+}
 
+func validateSecretValues(cmd *cobra.Command) error {
 	for key, allowedValues := range flink.ConnectionSecretAllowedValues {
 		secretValue, err := cmd.Flags().GetString(key)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		if secretValue != "" && !slices.Contains(allowedValues, secretValue) {
-			return nil, errors.NewErrorWithSuggestions(fmt.Sprintf("%s is invalid value for flag %s.", secretValue, key), fmt.Sprintf("Valid values for flag %s are %s.", key, utils.ArrayToCommaDelimitedString(allowedValues, "or")))
+		if secretValue != "" && !containsCaseInsensitive(allowedValues, secretValue) {
+			return errors.NewErrorWithSuggestions(
+				fmt.Sprintf("%s is invalid value for flag %s.", secretValue, key),
+				fmt.Sprintf("Valid values for flag %s are %s.", key, utils.ArrayToCommaDelimitedString(allowedValues, "or")))
 		}
 	}
+	return nil
+}
 
+func containsCaseInsensitive(slice []string, item string) bool {
+	for _, s := range slice {
+		if strings.EqualFold(s, item) {
+			return true
+		}
+	}
+	return false
+}
+
+func buildRequiredSecretsMap(cmd *cobra.Command, connectionType string) (map[string]string, error) {
 	requiredSecretKeys := flink.ConnectionRequiredSecretMapping[connectionType]
-	var optionalSecretKeys []string
-	for _, secretKey := range flink.ConnectionTypeSecretMapping[connectionType] {
-		if !slices.Contains(requiredSecretKeys, secretKey) {
-			optionalSecretKeys = append(optionalSecretKeys, secretKey)
-		}
-	}
-
 	secretMap := map[string]string{}
+
 	for _, requiredKey := range requiredSecretKeys {
 		secret, err := cmd.Flags().GetString(requiredKey)
 		if err != nil {
@@ -157,29 +170,47 @@ func validateConnectionSecrets(cmd *cobra.Command, connectionType string) (map[s
 		}
 		secretMap[backendKey] = secret
 	}
+	return secretMap, nil
+}
+
+func addOptionalSecretsToMap(cmd *cobra.Command, connectionType string, secretMap map[string]string) error {
+	requiredSecretKeys := flink.ConnectionRequiredSecretMapping[connectionType]
+	var optionalSecretKeys []string
+
+	for _, secretKey := range flink.ConnectionTypeSecretMapping[connectionType] {
+		if !slices.Contains(requiredSecretKeys, secretKey) {
+			optionalSecretKeys = append(optionalSecretKeys, secretKey)
+		}
+	}
 
 	for _, optionalSecretKey := range optionalSecretKeys {
 		secret, err := cmd.Flags().GetString(optionalSecretKey)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		backendKey, ok := flink.ConnectionSecretBackendKeyMapping[optionalSecretKey]
 		if !ok {
-			return nil, fmt.Errorf("backend key not found for %s", optionalSecretKey)
+			return fmt.Errorf("backend key not found for %s", optionalSecretKey)
 		}
 
 		if secret != "" {
 			secretMap[backendKey] = secret
 		}
 	}
+	return nil
+}
 
+func validateTransportTypeRules(secretMap map[string]string) error {
 	if transportType, ok := secretMap["TRANSPORT_TYPE"]; ok && transportType == "STREAMABLE_HTTP" {
 		if _, ok := secretMap["SSE_ENDPOINT"]; ok {
-			return nil, fmt.Errorf("sse-endpoint flag is not allowed for STREAMABLE_HTTP transport-type.")
+			return fmt.Errorf("sse-endpoint flag is not allowed for STREAMABLE_HTTP transport-type")
 		}
 	}
+	return nil
+}
 
+func determineAndSetAuthType(secretMap map[string]string) {
 	if _, ok := secretMap["API_KEY"]; ok {
 		secretMap[authType] = "API_KEY"
 	} else if _, ok := secretMap["USERNAME"]; ok {
@@ -189,12 +220,51 @@ func validateConnectionSecrets(cmd *cobra.Command, connectionType string) (map[s
 	} else if _, ok := secretMap["OAUTH2_CLIENT_ID"]; ok {
 		secretMap[authType] = "OAUTH2"
 	}
+}
 
+func validateRequiredAuthSecrets(connectionType string, secretMap map[string]string) error {
 	if secretMap[authType] == "" && slices.Contains(types.GetKeys(flink.ConnectionOneOfRequiredSecretsMapping), connectionType) {
-		return nil, fmt.Errorf("no secrets provided for type %s, one of the required secrets %s must be provided", connectionType,
+		return fmt.Errorf("no secrets provided for type %s, one of the required secrets %s must be provided", connectionType,
 			utils.ArrayToCommaDelimitedString(lo.Map(flink.ConnectionOneOfRequiredSecretsMapping[connectionType], func(item []string, _ int) string {
 				return fmt.Sprintf("%s", item)
 			}), "or"))
+	}
+	return nil
+}
+
+func validateConnectionSecrets(cmd *cobra.Command, connectionType string) (map[string]string, error) {
+	// Validate secret compatibility with connection type
+	if err := validateSecretCompatibility(cmd, connectionType); err != nil {
+		return nil, err
+	}
+
+	// Validate secret values against allowed values
+	if err := validateSecretValues(cmd); err != nil {
+		return nil, err
+	}
+
+	// Build secret map from required secrets
+	secretMap, err := buildRequiredSecretsMap(cmd, connectionType)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add optional secrets to the map
+	if err := addOptionalSecretsToMap(cmd, connectionType, secretMap); err != nil {
+		return nil, err
+	}
+
+	// Validate transport type specific rules
+	if err := validateTransportTypeRules(secretMap); err != nil {
+		return nil, err
+	}
+
+	// Determine and set authentication type
+	determineAndSetAuthType(secretMap)
+
+	// Validate required authentication secrets
+	if err := validateRequiredAuthSecrets(connectionType, secretMap); err != nil {
+		return nil, err
 	}
 
 	return secretMap, nil
