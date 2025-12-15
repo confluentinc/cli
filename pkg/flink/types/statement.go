654a596485efe89996dd1a9ed8bc8d8cc5de0b67
Santwana Verma <sverma@confluent.io>
2025-12-15 10:16:50 +0530

    Fixes statement name generation for CP Flink

--- pkg/flink/types/statement.go
+++ pkg/flink/types/statement.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"time"
 
@@ -13,4 +15,15 @@ func GenerateStatementName() string {
 	localTime := time.Now().Format("150405")
 	id := uuid.New().String()
 	return fmt.Sprintf("%s-%s-%s-%s", clientName, date, localTime, id)
+}
+
+func GenerateStatementNameForOnPrem() string {
+	clientName := "cli"
+	date := time.Now().Format("20060102")    // 8 chars
+	localTime := time.Now().Format("150405") // 6 chars
+	// 12 random bytes => 24 hex chars
+	b := make([]byte, 12)
+	_, _ = rand.Read(b)
+	randomHex := hex.EncodeToString(b)
+	return fmt.Sprintf("%s-%s-%s-%s", clientName, date, localTime, randomHex)
 }
