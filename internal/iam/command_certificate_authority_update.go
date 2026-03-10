8970c4cdf4c5dff987bc4de86420f8661a34ab93
Cynthia Qin <cqin@confluent.io>
2026-03-09 23:33:55 -0700

    Update flag name to resolve linter error

--- internal/iam/command_certificate_authority_update.go
+++ internal/iam/command_certificate_authority_update.go
@@ -30,7 +30,7 @@ func (c *certificateAuthorityCommand) newUpdateCommand() *cobra.Command {
 	cmd.Flags().String("certificate-chain-filename", "", "The name of the certificate file.")
 	cmd.Flags().String("crl-url", "", "The URL from which to fetch the CRL (Certificate Revocation List) for the certificate authority.")
 	cmd.Flags().String("crl-chain", "", "A base64 encoded string containing the CRL for this certificate authority.")
-	cmd.Flags().Bool("require-crl-on-client-cert", false, "Whether to require CRL validation on client certificates.")
+	cmd.Flags().Bool("require-client-crl", false, "Whether to require CRL validation on client certificates.")
 	pcmd.AddContextFlag(cmd, c.CLICommand)
 	pcmd.AddOutputFlag(cmd)
 
@@ -95,8 +95,8 @@ func (c *certificateAuthorityCommand) update(cmd *cobra.Command, args []string) 
 		}
 		update.CrlChain = certificateauthorityv2.PtrString(crlChain)
 	}
-	if cmd.Flags().Changed("require-crl-on-client-cert") {
-		requireCrlOnClientCertificate, err := cmd.Flags().GetBool("require-crl-on-client-cert")
+	if cmd.Flags().Changed("require-client-crl") {
+		requireCrlOnClientCertificate, err := cmd.Flags().GetBool("require-client-crl")
 		if err != nil {
 			return err
 		}
