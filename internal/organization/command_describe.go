44c023f96e5901e64a6c86ce41481ff264c4385c
Cynthia Qin <cqin@confluent.io>
2026-05-28 13:03:04 -0700

    Revert "Add scim_enabled flag to org CLI commands (#3236)"
    This reverts commit 6aa32ec20ed001861a17336c507a61659aaa8861.

--- internal/organization/command_describe.go
+++ internal/organization/command_describe.go
@@ -10,11 +10,10 @@ import (
 )
 
 type out struct {
-	IsCurrent   bool   `human:"Current" serialized:"is_current"`
-	Id          string `human:"ID" serialized:"id"`
-	Name        string `human:"Name" serialized:"name"`
-	JitEnabled  bool   `human:"JIT Enabled" serialized:"jit_enabled"`
-	ScimEnabled bool   `human:"SCIM Enabled" serialized:"scim_enabled"`
+	IsCurrent  bool   `human:"Current" serialized:"is_current"`
+	Id         string `human:"ID" serialized:"id"`
+	Name       string `human:"Name" serialized:"name"`
+	JitEnabled bool   `human:"JIT Enabled" serialized:"jit_enabled"`
 }
 
 func (c *command) newDescribeCommand() *cobra.Command {
@@ -38,11 +37,10 @@ func (c *command) describe(cmd *cobra.Command, _ []string) error {
 
 	table := output.NewTable(cmd)
 	table.Add(&out{
-		IsCurrent:   organization.GetId() == c.Context.GetCurrentOrganization(),
-		Id:          organization.GetId(),
-		Name:        organization.GetDisplayName(),
-		JitEnabled:  organization.GetJitEnabled(),
-		ScimEnabled: organization.GetScimEnabled(),
+		IsCurrent:  organization.GetId() == c.Context.GetCurrentOrganization(),
+		Id:         organization.GetId(),
+		Name:       organization.GetDisplayName(),
+		JitEnabled: organization.GetJitEnabled(),
 	})
 	return table.Print()
 }
