71a9a77bfd69fcfcd90bfefaf66c4d62c201c961
Channing Dong <165202380+channingdong@users.noreply.github.com>
2026-08-18 11:12:37 -0700

    Reapply "[CF-4208] Add `--page-size` to on-prem Flink list commands (#3450) (#3452)

--- internal/flink/command_secret_mapping_list.go
+++ internal/flink/command_secret_mapping_list.go
@@ -18,6 +18,7 @@ func (c *command) newSecretMappingListCommand() *cobra.Command {
 
 	cmd.Flags().String("environment", "", "Name of the Flink environment.")
 	cobra.CheckErr(cmd.MarkFlagRequired("environment"))
+	addPageSizeFlag(cmd)
 	addCmfFlagSet(cmd)
 	pcmd.AddOutputFlag(cmd)
 
@@ -30,12 +31,17 @@ func (c *command) secretMappingList(cmd *cobra.Command, _ []string) error {
 		return err
 	}
 
+	pageSize, err := getPageSize(cmd)
+	if err != nil {
+		return err
+	}
+
 	client, err := c.GetCmfClient(cmd)
 	if err != nil {
 		return err
 	}
 
-	sdkMappings, err := client.ListSecretMappings(c.createContext(), environment)
+	sdkMappings, err := client.ListSecretMappings(c.createContext(), environment, pageSize)
 	if err != nil {
 		return err
 	}
