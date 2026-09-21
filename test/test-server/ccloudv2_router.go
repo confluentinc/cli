1d8c4260b9f34883a2a6bb8431141caa7f3b20eb
Steven Gagniere <sgagniere@confluent.io>
2026-09-21 16:44:48 -0700

    Remove the manual implementation

--- test/test-server/ccloudv2_router.go
+++ test/test-server/ccloudv2_router.go
@@ -51,7 +51,6 @@ var ccloudV2Routes = []route{
 	{"/connect/v1/custom-connector-plugins", handleCustomConnectorPlugins},
 	{"/connect/v1/custom-connector-plugins/{id}", handleCustomConnectorPluginsId},
 	{"/connect/v1/presigned-upload-url", handleCustomPluginUploadUrl},
-	{"/connect/v1/custom-connector-runtimes", handleListCustomConnectorRuntimes},
 	{"/connect/v1/dummy-presigned-url", handleCustomPluginUploadFile},
 	{"/endpoint/v1/endpoints", handleEndpointV1Endpoints},
 	{"/fcpm/v2/compute-pools", handleFcpmComputePools},
